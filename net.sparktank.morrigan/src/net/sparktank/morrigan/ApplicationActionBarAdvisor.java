9e8689777b9ad0c85acf2a3a0ca6203c2e63dd1b
haku <haku@vaguehope.com>
2009-08-10 20:33:45 +0000

    sort of added a view menu.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@9 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan/src/net/sparktank/morrigan/ApplicationActionBarAdvisor.java
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/ApplicationActionBarAdvisor.java
@@ -1,10 +1,12 @@
 package net.sparktank.morrigan;
 
+import org.eclipse.jface.action.IContributionItem;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.MenuManager;
 import org.eclipse.ui.IWorkbenchActionConstants;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.actions.ActionFactory;
+import org.eclipse.ui.actions.ContributionItemFactory;
 import org.eclipse.ui.actions.ActionFactory.IWorkbenchAction;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
@@ -21,6 +23,8 @@ public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
 	// in the fill methods. This ensures that the actions aren't recreated
 	// when fillActionBars is called with FILL_PROXY.
 	private IWorkbenchAction exitAction;
+	private MenuManager showViewMenuMgr;
+	private IContributionItem showViewItemShortList;
 
 	public ApplicationActionBarAdvisor(IActionBarConfigurer configurer) {
 		super(configurer);
@@ -29,19 +33,25 @@ public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
 	protected void makeActions(final IWorkbenchWindow window) {
 		// Creates the actions and registers them.
 		// Registering is needed to ensure that key bindings work.
-		// The corresponding commands keybindings are defined in the plugin.xml
-		// file.
-		// Registering also provides automatic disposal of the actions when
-		// the window is closed.
+		// The corresponding commands keybindings are defined in the plugin.xml file.
+		// Registering also provides automatic disposal of the actions when the window is closed.
 
 		exitAction = ActionFactory.QUIT.create(window);
 		register(exitAction);
+		
+		showViewMenuMgr = new MenuManager("Show view", "showView");
+		showViewItemShortList = ContributionItemFactory.VIEWS_SHORTLIST.create(window);
 	}
-
+	
 	protected void fillMenuBar(IMenuManager menuBar) {
 		MenuManager fileMenu = new MenuManager("&File", IWorkbenchActionConstants.M_FILE);
 		menuBar.add(fileMenu);
 		fileMenu.add(exitAction);
+		
+		MenuManager windowMenu = new MenuManager("&Window", IWorkbenchActionConstants.M_WINDOW);
+		menuBar.add(windowMenu);
+		showViewMenuMgr.add(showViewItemShortList);
+		windowMenu.add(showViewMenuMgr);
 	}
 
 }
