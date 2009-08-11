97e75565f74f782fb7e415dded3cd670aafbad47
haku <haku@vaguehope.com>
2009-08-11 18:49:07 +0000

    beginnings of a tabular playlist editor.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@14 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan/src/net/sparktank/morrigan/editors/MediaListEditor.java
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/editors/MediaListEditor.java
@@ -1,11 +1,19 @@
 package net.sparktank.morrigan.editors;
 
 import net.sparktank.morrigan.model.media.MediaList;
+import net.sparktank.morrigan.model.media.MediaTrack;
 
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.viewers.ILabelProviderListener;
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.ITableLabelProvider;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.Table;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.PartInitException;
@@ -13,7 +21,9 @@ import org.eclipse.ui.part.EditorPart;
 
 public class MediaListEditor extends EditorPart {
 	public static final String ID = "net.sparktank.morrigan.editors.MediaListEditor";
+	
 	private MediaList editedMediaList;
+	private TableViewer editTable;
 	
 	public MediaListEditor() {
 	}
@@ -52,11 +62,76 @@ public class MediaListEditor extends EditorPart {
 	
 	@Override
 	public void createPartControl(Composite parent) {
+		editTable = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
 		
-		// TODO display this as a list.
+		// add columns.
+		String[] titles = { "file", "size" };
+		int[] bounds = { 400, 100 };
 		
-		Text text = new Text(parent, SWT.BORDER + SWT.MULTI + SWT.H_SCROLL + SWT.V_SCROLL);
-		text.setText(editedMediaList.toString());
+		for (int i = 0; i < titles.length; i++) {
+			TableViewerColumn column = new TableViewerColumn(editTable, SWT.NONE);
+			column.getColumn().setText(titles[i]);
+			column.getColumn().setWidth(bounds[i]);
+			column.getColumn().setResizable(true);
+			column.getColumn().setMoveable(true);
+		}
+		Table table = editTable.getTable();
+		table.setHeaderVisible(true);
+		table.setLinesVisible(true);
+		
+		// where the data is coming from.
+		editTable.setContentProvider(new IStructuredContentProvider() {
+			@Override
+			public Object[] getElements(Object inputElement) {
+				return editedMediaList.getMediaTracks().toArray();
+			}
+			@Override
+			public void dispose() {
+			}
+			@Override
+			public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+			}
+		});
+		
+		// how the data will appear.
+		editTable.setLabelProvider(new ITableLabelProvider() {
+			@Override
+			public Image getColumnImage(Object element, int columnIndex) {
+				return null;
+			}
+			@Override
+			public String getColumnText(Object element, int columnIndex) {
+				MediaTrack elm = (MediaTrack) element;
+				switch (columnIndex) {
+				case 0:
+					return elm.getFilepath();
+				case 1:
+					return "0";
+				default:
+					throw new RuntimeException("Invalid column; '" + columnIndex + "'.");
+				}
+			}
+			@Override
+			public void addListener(ILabelProviderListener listener) {
+				// not needed.
+			}
+			@Override
+			public void dispose() {
+				// not needed.
+			}
+			@Override
+			public boolean isLabelProperty(Object element, String property) {
+				return false;
+			}
+			@Override
+			public void removeListener(ILabelProviderListener listener) {
+				// not needed.
+			}
+		});
+		
+		// finishing off.
+		editTable.setInput(getEditorSite());
+		
 	}
 	
 	@Override
