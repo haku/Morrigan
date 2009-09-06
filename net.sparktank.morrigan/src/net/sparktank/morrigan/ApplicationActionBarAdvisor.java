4761e20d524aec5c442f56192e88855fea91e49c
haku <haku@vaguehope.com>
2009-09-06 00:22:10 +0000

    menu item for new playlist.  this is really NOT the way to do this.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@19 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan/src/net/sparktank/morrigan/ApplicationActionBarAdvisor.java
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/ApplicationActionBarAdvisor.java
@@ -1,8 +1,15 @@
 package net.sparktank.morrigan;
 
+import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.action.IContributionItem;
+import org.eclipse.jface.action.IMenuCreator;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.util.IPropertyChangeListener;
+import org.eclipse.swt.events.HelpListener;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.ui.IWorkbenchActionConstants;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.actions.ActionFactory;
@@ -46,6 +53,153 @@ public class ApplicationActionBarAdvisor extends ActionBarAdvisor {
 	protected void fillMenuBar(IMenuManager menuBar) {
 		MenuManager fileMenu = new MenuManager("&File", IWorkbenchActionConstants.M_FILE);
 		menuBar.add(fileMenu);
+		fileMenu.add(new IAction() {
+			
+			@Override
+			public void setToolTipText(String text) {
+			}
+			
+			@Override
+			public void setText(String text) {
+			}
+			
+			@Override
+			public void setMenuCreator(IMenuCreator creator) {
+			}
+			
+			@Override
+			public void setImageDescriptor(ImageDescriptor newImage) {
+			}
+			
+			@Override
+			public void setId(String id) {
+			}
+			
+			@Override
+			public void setHoverImageDescriptor(ImageDescriptor newImage) {
+			}
+			
+			@Override
+			public void setHelpListener(HelpListener listener) {
+			}
+			
+			@Override
+			public void setEnabled(boolean enabled) {
+			}
+			
+			@Override
+			public void setDisabledImageDescriptor(ImageDescriptor newImage) {
+			}
+			
+			@Override
+			public void setDescription(String text) {
+			}
+			
+			@Override
+			public void setChecked(boolean checked) {
+			}
+			
+			@Override
+			public void setActionDefinitionId(String id) {
+			}
+			
+			@Override
+			public void setAccelerator(int keycode) {
+			}
+			
+			@Override
+			public void runWithEvent(Event event) {
+				System.out.println("TODO: new playlist."); //TODO
+			}
+			
+			@Override
+			public void run() {
+				
+			}
+			
+			@Override
+			public void removePropertyChangeListener(IPropertyChangeListener listener) {
+			}
+			
+			@Override
+			public boolean isHandled() {
+				return false;
+			}
+			
+			@Override
+			public boolean isEnabled() {
+				return true;
+			}
+			
+			@Override
+			public boolean isChecked() {
+				return false;
+			}
+			
+			@Override
+			public String getToolTipText() {
+				return null;
+			}
+			
+			@Override
+			public String getText() {
+				return "New playlist...";
+			}
+			
+			@Override
+			public int getStyle() {
+				return 0;
+			}
+			
+			@Override
+			public IMenuCreator getMenuCreator() {
+				return null;
+			}
+			
+			@Override
+			public ImageDescriptor getImageDescriptor() {
+				return null;
+			}
+			
+			@Override
+			public String getId() {
+				return null;
+			}
+			
+			@Override
+			public ImageDescriptor getHoverImageDescriptor() {
+				return null;
+			}
+			
+			@Override
+			public HelpListener getHelpListener() {
+				return null;
+			}
+			
+			@Override
+			public ImageDescriptor getDisabledImageDescriptor() {
+				return null;
+			}
+			
+			@Override
+			public String getDescription() {
+				return null;
+			}
+			
+			@Override
+			public String getActionDefinitionId() {
+				return null;
+			}
+			
+			@Override
+			public int getAccelerator() {
+				return 0;
+			}
+			
+			@Override
+			public void addPropertyChangeListener(IPropertyChangeListener listener) {
+			}
+		});
 		fileMenu.add(exitAction);
 		
 		MenuManager windowMenu = new MenuManager("&Window", IWorkbenchActionConstants.M_WINDOW);
