e90a565bad9949efee0859db1cca7c7dcd4a5a31
haku <haku@vaguehope.com>
2009-09-06 00:04:49 +0000

    now gets list of PLs from file system.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@17 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan/src/net/sparktank/morrigan/helpers/PlaylistHelper.java
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/helpers/PlaylistHelper.java
@@ -3,9 +3,9 @@ package net.sparktank.morrigan.helpers;
 import java.io.File;
 import java.util.ArrayList;
 
+import net.sparktank.morrigan.config.Config;
 import net.sparktank.morrigan.model.media.MediaPlaylist;
 import net.sparktank.morrigan.model.ui.MediaExplorerItem;
-import net.sparktank.morrigan.model.ui.MenuItem;
 
 public class PlaylistHelper {
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -19,25 +19,39 @@ public class PlaylistHelper {
 		return null;
 	}
 	
+	public boolean isPlFile (String filePath) {
+		return (filePath.toLowerCase().endsWith(Config.PL_FILE_EXT));
+	}
+	
 	public ArrayList<MediaExplorerItem> getAllPlaylists () {
-		// TODO
-		// FIXME
-		
 		ArrayList<MediaExplorerItem> ret = new ArrayList<MediaExplorerItem>();
-		for (int i = 0; i < 10; i++) {
-			MediaExplorerItem newItem = new MediaExplorerItem(MediaExplorerItem.ItemType.PLAYLIST);
-			
-			newItem.identifier = "\\path\\playlists\\playlist" + i;
-			
-			int x = newItem.identifier.lastIndexOf(File.separator);
-			if (x > 0) {
-				newItem.title = newItem.identifier.substring(x+1);
-			} else {
-				newItem.title = newItem.identifier;
+		
+		// TODO better display name / show icons for PLs?
+		
+		File plDir = Config.getPlDir();
+		File [] plFiles = plDir.listFiles();
+		
+		// empty dir?
+		if (plFiles == null ) return ret;
+		
+		for (File file : plFiles) {
+			if (isPlFile(file.getAbsolutePath())) {
+				System.out.println("found pl = " + file);
+				
+				MediaExplorerItem newItem = new MediaExplorerItem(MediaExplorerItem.ItemType.PLAYLIST);
+				newItem.identifier = file.getAbsolutePath();
+				
+				int x = newItem.identifier.lastIndexOf(File.separator);
+				if (x > 0) {
+					newItem.title = newItem.identifier.substring(x+1);
+				} else {
+					newItem.title = newItem.identifier;
+				}
+				
+				ret.add(newItem);
 			}
-			
-			ret.add(newItem);
 		}
+		
 		return ret;
 	}
 	
