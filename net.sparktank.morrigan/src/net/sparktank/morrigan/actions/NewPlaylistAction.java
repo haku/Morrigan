7abe0079bcd6beefb9281680a66f02039f25d4c1
haku <haku@vaguehope.com>
2009-11-07 12:33:27 +0000

    Used abstract class to reduce the amound of boiler plate needed.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@29 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan/src/net/sparktank/morrigan/actions/NewPlaylistAction.java
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/actions/NewPlaylistAction.java
@@ -5,20 +5,15 @@ import net.sparktank.morrigan.exceptions.MorriganException;
 import net.sparktank.morrigan.helpers.PlaylistHelper;
 import net.sparktank.morrigan.views.ViewMediaExplorer;
 
-import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.action.IMenuCreator;
 import org.eclipse.jface.dialogs.InputDialog;
-import org.eclipse.jface.resource.ImageDescriptor;
-import org.eclipse.jface.util.IPropertyChangeListener;
 import org.eclipse.jface.window.Window;
-import org.eclipse.swt.events.HelpListener;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
 
 
-public class NewPlaylistAction implements IAction {
+public class NewPlaylistAction extends MorriganAction {
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	
 	private IWorkbenchWindow window;
@@ -60,97 +55,6 @@ public class NewPlaylistAction implements IAction {
 			view.refresh();
 		}
 	}
-	
-//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	
-	@Override
-	public boolean isHandled() {
-		return false;
-	}
-	@Override
-	public boolean isEnabled() {
-		return true;
-	}
-	@Override
-	public boolean isChecked() {
-		return false;
-	}
-	@Override
-	public String getToolTipText() {
-		return null;
-	}
-	@Override
-	public int getStyle() {
-		return 0;
-	}
-	@Override
-	public IMenuCreator getMenuCreator() {
-		return null;
-	}
-	@Override
-	public ImageDescriptor getImageDescriptor() {
-		return null;
-	}
-	@Override
-	public ImageDescriptor getHoverImageDescriptor() {
-		return null;
-	}
-	@Override
-	public HelpListener getHelpListener() {
-		return null;
-	}
-	@Override
-	public ImageDescriptor getDisabledImageDescriptor() {
-		return null;
-	}
-	@Override
-	public String getDescription() {
-		return null;
-	}
-	@Override
-	public String getActionDefinitionId() {
-		return null;
-	}
-	@Override
-	public int getAccelerator() {
-		return 0;
-	}
-	
-//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	
-	@Override
-	public void addPropertyChangeListener(IPropertyChangeListener listener) {}
-	
-	@Override
-	public void run() {}
-	@Override
-	public void removePropertyChangeListener(IPropertyChangeListener listener) {}
-	@Override
-	public void setToolTipText(String text) {}
-	@Override
-	public void setText(String text) {}
-	@Override
-	public void setMenuCreator(IMenuCreator creator) {}
-	@Override
-	public void setImageDescriptor(ImageDescriptor newImage) {}
-	@Override
-	public void setId(String id) {}
-	@Override
-	public void setHoverImageDescriptor(ImageDescriptor newImage) {}
-	@Override
-	public void setHelpListener(HelpListener listener) {}
-	@Override
-	public void setEnabled(boolean enabled) {}
-	@Override
-	public void setDisabledImageDescriptor(ImageDescriptor newImage) {}
-	@Override
-	public void setDescription(String text) {}
-	@Override
-	public void setChecked(boolean checked) {}
-	@Override
-	public void setActionDefinitionId(String id) {}
-	@Override
-	public void setAccelerator(int keycode) {}
 
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 }
