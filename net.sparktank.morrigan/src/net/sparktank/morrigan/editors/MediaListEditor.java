d2887cfe2191a4013f89e46c67c73b8f6e30bac9
haku <haku@vaguehope.com>
2010-02-02 15:30:01 +0000

    now ready to write save code.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@44 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan/src/net/sparktank/morrigan/editors/MediaListEditor.java
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/editors/MediaListEditor.java
@@ -1,5 +1,7 @@
 package net.sparktank.morrigan.editors;
 
+import java.io.File;
+
 import net.sparktank.morrigan.ApplicationActionBarAdvisor;
 import net.sparktank.morrigan.dialogs.MorriganMsgDlg;
 import net.sparktank.morrigan.model.media.MediaList;
@@ -17,21 +19,36 @@ import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.EditorPart;
 
 public class MediaListEditor extends EditorPart {
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
 	public static final String ID = "net.sparktank.morrigan.editors.MediaListEditor";
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	
 	private MediaList editedMediaList;
 	private TableViewer editTable;
 	
+	private boolean m_isDirty = false;
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//	Constructor.
+	
 	public MediaListEditor() {
 		super();
 	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//	EditorPart methods.
 	
 	@Override
 	public void init(IEditorSite site, IEditorInput input) throws PartInitException {
@@ -46,7 +63,7 @@ public class MediaListEditor extends EditorPart {
 		// Create table control.
 		editTable = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
 		
-		// add columns.
+		// add and configure columns.
 		String[] titles = { "file", "size" };
 		int[] bounds = { 400, 100 };
 		
@@ -61,65 +78,13 @@ public class MediaListEditor extends EditorPart {
 		table.setHeaderVisible(true);
 		table.setLinesVisible(true);
 		
-		// where the data is coming from.
-		editTable.setContentProvider(new IStructuredContentProvider() {
-			@Override
-			public Object[] getElements(Object inputElement) {
-				return editedMediaList.getMediaTracks().toArray();
-			}
-			@Override
-			public void dispose() {
-			}
-			@Override
-			public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
-			}
-		});
-		
-		// how the data will appear.
-		editTable.setLabelProvider(new ITableLabelProvider() {
-			@Override
-			public Image getColumnImage(Object element, int columnIndex) {
-				return null;
-			}
-			@Override
-			public String getColumnText(Object element, int columnIndex) {
-				MediaTrack elm = (MediaTrack) element;
-				switch (columnIndex) {
-				case 0:
-					return elm.getFilepath();
-				case 1:
-					return "0";
-				default:
-					throw new RuntimeException("Invalid column; '" + columnIndex + "'.");
-				}
-			}
-			@Override
-			public void addListener(ILabelProviderListener listener) {
-				// not needed.
-			}
-			@Override
-			public void dispose() {
-				// not needed.
-			}
-			@Override
-			public boolean isLabelProperty(Object element, String property) {
-				return false;
-			}
-			@Override
-			public void removeListener(ILabelProviderListener listener) {
-				// not needed.
-			}
-		});
+		// where the data and lables are coming from.
+		editTable.setContentProvider(contentProvider);
+		editTable.setLabelProvider(labelProvider);
 		
 		// finishing off.
 		editTable.setInput(getEditorSite());
 	}
-	
-	IAction addAction = new Action("add") {
-		public void run () {
-			new MorriganMsgDlg("TODO: process 'add' action on list '"+getTitle()+"'.").open();
-		}
-	};
 	
 	@Override
 	public void setFocus() {
@@ -128,23 +93,124 @@ public class MediaListEditor extends EditorPart {
 	
 	@Override
 	public boolean isDirty() {
-		// TODO isDirty
-		return false;
+		return m_isDirty;
 	}
 	
 	@Override
 	public boolean isSaveAsAllowed() {
-		return false;
+		return true;
 	}
 	
 	@Override
 	public void doSaveAs() {
+		new MorriganMsgDlg("TODO: do save as for '" + getTitle() + "'.\n\n(this should not happen.)").open();
 	}
 	
 	@Override
 	public void doSave(IProgressMonitor monitor) {
 		// TODO save changes.
-//		person.getAddress().setCountry(text2.getText());
+		new MorriganMsgDlg("TODO: do save " + getTitle()).open();
 	}
 	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//	Editor helper methods.
+	
+	private void setIsDirty (boolean dirty) {
+		m_isDirty = dirty;
+		firePropertyChange(PROP_DIRTY);
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//	Providers.
+	
+	IStructuredContentProvider contentProvider = new IStructuredContentProvider() {
+		
+		@Override
+		public Object[] getElements(Object inputElement) {
+			return editedMediaList.getMediaTracks().toArray();
+		}
+		
+		@Override
+		public void dispose() {}
+		@Override
+		public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {}
+		
+	};
+	
+	ITableLabelProvider labelProvider = new ITableLabelProvider() {
+		
+		@Override
+		public Image getColumnImage(Object element, int columnIndex) {
+			return null;
+		}
+		
+		@Override
+		public String getColumnText(Object element, int columnIndex) {
+			MediaTrack elm = (MediaTrack) element;
+			
+			switch (columnIndex) {
+				case 0:
+					return elm.getFilepath();
+				
+				case 1:
+					return "0";
+				
+				default:
+					throw new IllegalArgumentException("Invalid column; '" + columnIndex + "'.");
+				
+			}
+		}
+		
+		@Override
+		public boolean isLabelProperty(Object element, String property) {
+			return false;
+		}
+		
+		@Override
+		public void addListener(ILabelProviderListener listener) {}
+		@Override
+		public void dispose() {}
+		@Override
+		public void removeListener(ILabelProviderListener listener) {}
+		
+	};
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//	Methods for editing editedMediaList.
+	
+	private void addTrack (String file) {
+		editedMediaList.addTrack(file);
+		editTable.refresh();
+		setIsDirty(true);
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//	Actions.
+	
+	IAction addAction = new Action("add") {
+		public void run () {
+			Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+			
+			FileDialog dialog = new FileDialog(shell, SWT.MULTI);
+			dialog.setText("Add to " + getTitle());
+			dialog.setFilterExtensions(new String[] {"*.mp3", "*.*"}); // TODO refine file type list.
+			dialog.setFilterNames(new String[] {"mp3 Files", "All Files"});
+			
+			String firstSel = dialog.open();
+			if (firstSel != null) {
+				File firstSelFile = new File(firstSel);
+				String baseDir = firstSelFile.getAbsoluteFile().getParentFile().getAbsolutePath();
+				
+				String[] files = dialog.getFileNames();
+				for (String file : files) {
+					String toAdd = baseDir + File.separatorChar + file;
+					System.out.println("add f=" + toAdd);
+					addTrack(toAdd);
+				}
+			}
+			
+		}
+	};
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 }
