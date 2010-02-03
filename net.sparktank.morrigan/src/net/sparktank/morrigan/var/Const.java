77341ae2837d70e40caca916b45d45c28950cc6a
haku <haku@vaguehope.com>
2010-02-03 03:09:58 +0000

    fixed MP3SPI package name.  again.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@87 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan/src/net/sparktank/morrigan/var/Const.java
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/var/Const.java
@@ -2,6 +2,6 @@ package net.sparktank.morrigan.var;
 
 public class Const {
 	
-	public static final String PLAYBACK_ENGINE = "net.sparktank.morrigan.playbackimpl.mp3spi.PlaybackEngine";
+	public static final String PLAYBACK_ENGINE = "net.sparktank.morrigan.playbackimpl.spi.PlaybackEngine";
 	
 }
