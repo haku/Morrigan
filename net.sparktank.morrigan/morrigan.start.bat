741194315e7ff347800109d359a2071e393f910c
haku <haku@vaguehope.com>
2010-02-03 02:56:37 +0000

    updated.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@81 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan/morrigan.start.bat
+++ net.sparktank.morrigan/morrigan.start.bat
@@ -1,12 +1,10 @@
 "C:\Program Files\Java\jdk1.6.0_17\bin\javaw.exe" ^
 -Dosgi.requiredJavaVersion=1.5 -Xms40m -Xmx256m ^
--Djava.util.logging.config.file=logging.properties ^
 -Dfile.encoding=UTF-8 ^
 -classpath C:\Software\eclipsercpgalileo\plugins\org.eclipse.equinox.launcher_1.0.201.R35x_v20090715.jar org.eclipse.equinox.launcher.Main ^
--launcher C:\Software\eclipsercpgalileo\eclipse.exe ^
--name Eclipse ^
--showsplash 600 ^
--product net.sparktank.morrigan.product ^
--configuration file:D:/haku/development/eclipseWorkspace-java/.metadata/.plugins/org.eclipse.pde.core/net.sparktank.morrigan.application/ ^
--dev file:D:/haku/development/eclipseWorkspace-java/.metadata/.plugins/org.eclipse.pde.core/net.sparktank.morrigan.application/dev.properties ^
+-launcher C:\Software\eclipsercpgalileo\eclipse.exe -name Eclipse -showsplash 600 -product net.sparktank.morrigan.product ^
+-data D:\haku\development\eclipseWorkspace-java/../runtime-net.sparktank.morrigan.product ^
+-configuration file:D:/haku/development/eclipseWorkspace-java/.metadata/.plugins/org.eclipse.pde.core/net.sparktank.morrigan.product/ ^
+-dev file:D:/haku/development/eclipseWorkspace-java/.metadata/.plugins/org.eclipse.pde.core/net.sparktank.morrigan.product/dev.properties ^
 -os win32 -ws win32 -arch x86 -nl en_GB -consoleLog -console
+
