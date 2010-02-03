b57e64438ba9a0502a0fb55f5a0423c846cad927
haku <haku@vaguehope.com>
2010-02-03 00:16:53 +0000

    new constructors.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@75 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan/src/net/sparktank/morrigan/exceptions/MorriganException.java
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/exceptions/MorriganException.java
@@ -4,12 +4,18 @@ public class MorriganException extends Exception {
 	
 	private static final long serialVersionUID = -6138831951686942856L;
 
+	public MorriganException() {}
+	
 	public MorriganException (String s) {
 		super(s);
 	}
 	
 	public MorriganException (String s, Throwable t) {
 		super(s, t);
+	}
+
+	public MorriganException(Throwable t) {
+		super(t);
 	}
 	
 }
