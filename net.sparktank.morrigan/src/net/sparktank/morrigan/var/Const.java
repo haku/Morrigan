d267406b8be72be8986c1cd961f599cb0062cd48
haku <haku@vaguehope.com>
2010-02-04 02:19:05 +0000

    unused package
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@111 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan/src/net/sparktank/morrigan/var/Const.java
+++
@@ -1,9 +0,0 @@
-package net.sparktank.morrigan.var;
-
-public class Const {
-	
-	public static final String SQLITE_DBNAME = "library.db3";
-	
-	public static final String PLAYBACK_ENGINE = "net.sparktank.morrigan.playbackimpl.spi.PlaybackEngine";
-	
-}
