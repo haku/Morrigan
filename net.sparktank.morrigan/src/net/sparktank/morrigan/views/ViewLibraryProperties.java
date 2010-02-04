e1b4d3fea58b1af85c8a0ec7953dbe6b5512e13e
haku <haku@vaguehope.com>
2010-02-04 06:53:33 +0000

    can now edit a library's sources.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@130 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan/src/net/sparktank/morrigan/views/ViewLibraryProperties.java
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/views/ViewLibraryProperties.java
@@ -1,5 +1,6 @@
 package net.sparktank.morrigan.views;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import net.sparktank.morrigan.Activator;
@@ -9,15 +10,17 @@ import net.sparktank.morrigan.model.media.MediaLibrary;
 
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.swt.widgets.Label;
-import org.eclipse.swt.widgets.Table;
 import org.eclipse.ui.part.ViewPart;
 
 public class ViewLibraryProperties extends ViewPart {
@@ -31,7 +34,7 @@ public class ViewLibraryProperties extends ViewPart {
 	public void createPartControl(Composite parent) {
 		createLayout(parent);
 		addToolbar();
-		updateStatus();
+		updateGui();
 	}
 	
 	@Override
@@ -43,7 +46,7 @@ public class ViewLibraryProperties extends ViewPart {
 	
 	public void setContent (MediaLibrary library) {
 		this.library = library;
-		updateStatus();
+		updateGui();
 	}
 	
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -54,26 +57,23 @@ public class ViewLibraryProperties extends ViewPart {
 	
 	private void createLayout (Composite parent) {
 		GridData gridData;
-		
 		parent.setLayout(new GridLayout(1, false));
 		
-		mainLabel = new Label(parent, SWT.WRAP);
 		gridData = new GridData();
 		gridData.horizontalAlignment = SWT.FILL;
 		gridData.grabExcessHorizontalSpace = true;
+		mainLabel = new Label(parent, SWT.WRAP);
 		mainLabel.setLayoutData(gridData);
 		
-		Table table = new Table(parent, SWT.MULTI | SWT.V_SCROLL );
 		gridData = new GridData();
 		gridData.horizontalAlignment = SWT.FILL;
 		gridData.verticalAlignment = SWT.FILL;
 		gridData.grabExcessHorizontalSpace = true;
 		gridData.grabExcessVerticalSpace = true;
-		table.setLayoutData(gridData);
-		
-		tableViewer = new TableViewer(table);    
+		tableViewer = new TableViewer(parent);
+		tableViewer.getTable().setLayoutData(gridData);
 		tableViewer.setContentProvider(sourcesProvider);
-		
+		tableViewer.setInput(getViewSite()); // use content provider.
 	}
 	
 	private void addToolbar () {
@@ -81,9 +81,9 @@ public class ViewLibraryProperties extends ViewPart {
 		getViewSite().getActionBars().getToolBarManager().add(removeAction);
 	}
 	
-	private void updateStatus () {
+	private void updateGui () {
 		if (library!=null) {
-			mainLabel.setText("Sources for " + library.getListName() + ".");
+			mainLabel.setText( "Sources for " + library.getListName() + ".");
 			
 		} else {
 			mainLabel.setText("No library selected.");
@@ -93,44 +93,110 @@ public class ViewLibraryProperties extends ViewPart {
 	}
 	
 	private IStructuredContentProvider sourcesProvider = new IStructuredContentProvider() {
+		
+		@Override
+		public Object[] getElements(Object inputElement) {
+			System.out.println("getElements.");
 			
-			@Override
-			public Object[] getElements(Object inputElement) {
-				List<String> sources = null;
+			List<String> sources = null;
+			
+			if (library!=null) {
+				try {
+					sources = library.getSources();
+				} catch (DbException e) {
+					new MorriganMsgDlg(e, getSite().getShell().getDisplay());
+				}
 				
-				if (library!=null) {
-					try {
-						sources = library.getSources();
-					} catch (DbException e) {
-						new MorriganMsgDlg(e, getSite().getShell().getDisplay());
+				return sources.toArray();
+				
+			} else {
+				return new String[]{};
+			}
+		}
+		
+		@Override
+		public void dispose() {}
+		@Override
+		public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {}
+		
+	};
+	
+	private ArrayList<String> getSelectedSources () {
+		ISelection selection = tableViewer.getSelection();
+		
+		if (selection==null) return null;
+		if (selection.isEmpty()) return null;
+		
+		if (selection instanceof IStructuredSelection) {
+			IStructuredSelection iSel = (IStructuredSelection) selection;
+			
+			ArrayList<String> ret = new ArrayList<String>();
+			for (Object selectedObject : iSel.toList()) {
+				if (selectedObject != null) {
+					if (selectedObject instanceof String) {
+						String item = (String) selectedObject;
+						ret.add(item);
 					}
-					
-					return sources.toArray();
-					
-				} else {
-					return null;
 				}
 			}
-			
-			@Override
-			public void dispose() {}
-			@Override
-			public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {}
-			
-		};
+			return ret;
+		}
+		
+		return null;
+	}
 	
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 //	Actions.
 	
 	private IAction addAction = new Action("add", Activator.getImageDescriptor("icons/plus.gif")) {
 		public void run() {
-			new MorriganMsgDlg("TODO: implement add desu~.").open();
+			if (library==null) {
+				new MorriganMsgDlg("No library selected desu~.").open();
+				return;
+			}
+			
+			DirectoryDialog dlg = new DirectoryDialog(getSite().getShell());
+			dlg.setText("Add Source");
+			dlg.setMessage("Select a directory containing media.");
+			String dir = dlg.open();
+			if (dir != null) {
+				try {
+					library.addSource(dir);
+				} catch (DbException e) {
+					new MorriganMsgDlg(e, getSite().getShell().getDisplay()).open();
+				}
+				updateGui();
+			}
 		};
 	};
 	
 	private IAction removeAction = new Action("remove", Activator.getImageDescriptor("icons/minus.gif")) {
 		public void run() {
-			new MorriganMsgDlg("TODO: implement remove desu~.").open();
+			if (library==null) {
+				new MorriganMsgDlg("No library selected desu~.").open();
+				return;
+			}
+			
+			ArrayList<String> selectedSources = getSelectedSources();
+			if (selectedSources==null || selectedSources.isEmpty()) {
+				new MorriganMsgDlg("No items selected desu~.").open();
+				return;
+			}
+			
+			MorriganMsgDlg dlg = new MorriganMsgDlg("Remove selected from " + library.getListName() + "?", MorriganMsgDlg.YESNO);
+			dlg.open();
+			if (dlg.getReturnCode() == MorriganMsgDlg.OK) {
+				for (String item : selectedSources) {
+					try {
+						library.removeSource(item);
+					} catch (DbException e) {
+						new MorriganMsgDlg(e, getSite().getShell().getDisplay()).open();
+					}
+					updateGui();
+				}
+			}
+			
+			
 		};
 	};
 	
