76e460b2782a4018042d471ee235078291d6b67c
haku <haku@vaguehope.com>
2010-02-05 02:14:38 +0000

    discovered view label
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@170 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan/src/net/sparktank/morrigan/views/ViewLibraryProperties.java
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/views/ViewLibraryProperties.java
@@ -17,11 +17,8 @@ import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.DirectoryDialog;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.part.ViewPart;
 
 public class ViewLibraryProperties extends ViewPart {
@@ -53,26 +50,10 @@ public class ViewLibraryProperties extends ViewPart {
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 //	GUI stuff.
 	
-	private Label mainLabel;
 	TableViewer tableViewer;
 	
 	private void createLayout (Composite parent) {
-		GridData gridData;
-		parent.setLayout(new GridLayout(1, false));
-		
-		gridData = new GridData();
-		gridData.horizontalAlignment = SWT.FILL;
-		gridData.grabExcessHorizontalSpace = true;
-		mainLabel = new Label(parent, SWT.WRAP);
-		mainLabel.setLayoutData(gridData);
-		
-		gridData = new GridData();
-		gridData.horizontalAlignment = SWT.FILL;
-		gridData.verticalAlignment = SWT.FILL;
-		gridData.grabExcessHorizontalSpace = true;
-		gridData.grabExcessVerticalSpace = true;
-		tableViewer = new TableViewer(parent);
-		tableViewer.getTable().setLayoutData(gridData);
+		tableViewer = new TableViewer(parent, SWT.MULTI | SWT.V_SCROLL);
 		tableViewer.setContentProvider(sourcesProvider);
 		tableViewer.setInput(getViewSite()); // use content provider.
 	}
@@ -85,10 +66,10 @@ public class ViewLibraryProperties extends ViewPart {
 	
 	private void updateGui () {
 		if (library!=null) {
-			mainLabel.setText( "Sources for " + library.getListName() + ".");
+			setContentDescription( "Sources for " + library.getListName() + ".");
 			
 		} else {
-			mainLabel.setText("No library selected.");
+			setContentDescription("No library selected.");
 		}
 		
 		tableViewer.refresh();
