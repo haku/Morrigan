23cd8aa6b89c5bab9ec5a1ea9c223937edaf8ffc
haku <haku@vaguehope.com>
2010-02-05 14:50:23 +0000

    asethicics
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@197 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan/src/net/sparktank/morrigan/helpers/EqualHelper.java
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/helpers/EqualHelper.java
@@ -2,7 +2,7 @@ package net.sparktank.morrigan.helpers;
 
 public class EqualHelper {
 	
-	static public boolean areEqual(Object aThis, Object aThat){
+	public static boolean areEqual(Object aThis, Object aThat){
 		return aThis == null ? aThat == null : aThis.equals(aThat);
 	}
 	
