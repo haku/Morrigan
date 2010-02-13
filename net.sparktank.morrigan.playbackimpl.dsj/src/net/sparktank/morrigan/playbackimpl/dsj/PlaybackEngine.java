230294e48f2e61d8b90a63b145e00f4644e4a1b0
haku <haku@vaguehope.com>
2010-02-13 13:21:09 +0000

    init.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@279 2128fabd-03f8-4a31-9ba6-05665b9bae8a

---
+++ net.sparktank.morrigan.playbackimpl.dsj/src/net/sparktank/morrigan/playbackimpl/dsj/PlaybackEngine.java
@@ -0,0 +1,254 @@
+package net.sparktank.morrigan.playbackimpl.dsj;
+
+import java.awt.Component;
+import java.awt.Frame;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.net.MalformedURLException;
+
+import javax.media.CannotRealizeException;
+import javax.media.NoDataSourceException;
+import javax.media.NoPlayerException;
+import javax.media.Time;
+
+import net.sparktank.morrigan.playback.IPlaybackEngine;
+import net.sparktank.morrigan.playback.IPlaybackStatusListener;
+import net.sparktank.morrigan.playback.NotImplementedException;
+import net.sparktank.morrigan.playback.PlaybackException;
+import de.humatic.dsj.DSFiltergraph;
+import de.humatic.dsj.DSMovie;
+
+@SuppressWarnings("restriction")
+public class PlaybackEngine  implements IPlaybackEngine {
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	// TODO Any more?
+	private final static String[] SUPPORTED_FORMATS = {"wav", "mp3"};
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	private volatile boolean m_stopPlaying;
+	
+	private String filepath = null;
+	private Frame videoFrame = null;
+	private IPlaybackStatusListener listener = null;
+	private PlayState playbackState = PlayState.Stopped;
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//	Constructor.
+
+	public PlaybackEngine () {}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//	IPlaybackEngine methods.
+	
+	@Override
+	public String getAbout () {
+		return "net.sparktank.morrigan.playbackimpl.dsj version 0.01.";
+	}
+	
+	@Override
+	public String[] getSupportedFormats() {
+		return SUPPORTED_FORMATS;
+	}
+	
+	@Override
+	public void setFile(String filepath) {
+		this.filepath = filepath;
+	}
+	
+	@Override
+	public void setVideoFrame(Frame frame) {
+		this.videoFrame = frame;
+	}
+	
+	@Override
+	public void unloadFile() {
+		finalisePlayback();
+	}
+	
+	@Override
+	public void finalise() {}
+
+	@Override
+	public void startPlaying() throws PlaybackException {
+		m_stopPlaying = false;
+		
+		try {
+			loadTrack();
+		} catch (Exception e) {
+			throw new PlaybackException("Failed to load '"+filepath+"'.", e);
+		}
+		
+		playTrack();
+	}
+	
+	@Override
+	public void stopPlaying() throws PlaybackException {
+		m_stopPlaying = true;
+		stopTrack();
+	}
+	
+	@Override
+	public void pausePlaying() throws PlaybackException {
+		throw new NotImplementedException();
+	}
+	
+	@Override
+	public PlayState getPlaybackState() {
+		return playbackState;
+	}
+	
+	@Override
+	public int getDuration() throws PlaybackException {
+		if (mediaPlayer!=null) {
+			Time mediaTime = mediaPlayer.getDuration();
+			return (int) Math.round(mediaTime.getSeconds()); // FIXME return long.
+		} else {
+			return -1;
+		}
+	}
+	
+	@Override
+	public long getPlaybackProgress() throws PlaybackException {
+		if (mediaPlayer!=null) {
+			Time mediaTime = mediaPlayer.getMediaTime();
+			return Math.round(mediaTime.getSeconds());
+		} else {
+			return -1;
+		}
+	}
+	
+	@Override
+	public void setStatusListener(IPlaybackStatusListener listener) {
+		this.listener = listener;
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//	Local playback methods.
+	
+	private DSFiltergraph dsFiltergraph = null;
+	
+	private void finalisePlayback () {
+		if (dsFiltergraph!=null) {
+			
+		}
+	}
+	
+	private void loadTrack () {
+		dsFiltergraph = new DSMovie(filepath, DSFiltergraph.RENDER_NATIVE, propertyChangeLlistener);
+		dsFiltergraph.setVolume(1.0f);
+		
+		videoFrame.add(dsFiltergraph.asComponent());
+	}
+	
+	private static PropertyChangeListener propertyChangeLlistener = new PropertyChangeListener() {
+		@Override
+		public void propertyChange(PropertyChangeEvent evt) {
+			System.out.println("received event or callback from "+evt.getPropagationId());
+		}
+	};
+	
+	private void playTrack () {
+		if (dsFiltergraph!=null) {
+			dsFiltergraph.play();
+			startWatcherThread();
+		}
+	}
+	
+	private void stopTrack () {
+		stopWatcherThread();
+		if (dsFiltergraph!=null) {
+			dsFiltergraph.stop();
+		}
+	}
+	
+	private volatile boolean m_stopWatching = false;
+	private Thread watcherThread = null;
+	
+	private void startWatcherThread () {
+		m_stopWatching = false;
+		watcherThread = new WatcherThread();
+		watcherThread.setDaemon(true);
+		watcherThread.start();
+	}
+	
+	private void stopWatcherThread () {
+		m_stopWatching = true;
+		try {
+			if (watcherThread!=null
+					&& watcherThread.isAlive()
+					&& !Thread.currentThread().equals(watcherThread)) {
+				watcherThread.join();
+			}
+			
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private class WatcherThread extends Thread {
+		public void run() {
+			while (!m_stopWatching) {
+				
+				if (mediaPlayer!=null) {
+					Time mediaTime = mediaPlayer.getMediaTime();
+					callPositionListener(Math.round(mediaTime.getSeconds()));
+				}
+				
+				try {
+					Thread.sleep(500);
+				} catch (InterruptedException e) {}
+			}
+		};
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//	Local helper methods.
+	
+	private void callOnErrorHandler (Exception e) {
+		if (listener!=null) {
+			listener.onError(e);
+		}
+	}
+	
+	private void callOnEndOfTrackHandler () {
+		if (listener!=null) {
+			listener.onEndOfTrack();
+		}
+	}
+	
+	private void callStateListener (PlayState state) {
+		this.playbackState = state;
+		if (listener!=null) listener.statusChanged(state);
+	}
+	
+	private void callPositionListener (long position) {
+		if (listener!=null) {
+			listener.positionChanged(position);
+		}
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	private void shoeHorn () {
+		try {
+			Class clazz = ClassLoader.class;
+			Field field = clazz.getDeclaredField("sys_paths");
+			boolean accessible = field.isAccessible();
+			if (!accessible)
+			field.setAccessible(true);
+			Object original = field.get(clazz);
+			field.set(clazz, null);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.setProperty("java.library.path", "D:\\haku\\development\\eclipseWorkspace-java\\dsjtest\\lib");
+		System.load("D:\\haku\\development\\eclipseWorkspace-java\\dsjtest\\lib\\dsj.dll");
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+}
