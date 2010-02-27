c2803ea60b609ab03cd9139ea0b7f9b17e1c4a0d
haku <haku@vaguehope.com>
2010-02-27 19:04:50 +0000

    Interfaces for hotkeys
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@370 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan/src/net/sparktank/morrigan/engines/hotkey/IHotkeyEngine.java
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/engines/hotkey/IHotkeyEngine.java
@@ -2,8 +2,10 @@ package net.sparktank.morrigan.engines.hotkey;
 
 public interface IHotkeyEngine {
 	
-	public void registerHotkey ();
+	public void registerHotkey (int id, int key, boolean ctrl, boolean shift, boolean alt, boolean supr) throws HotkeyException;
 	
-	public void unregisterHotkey ();
+	public void unregisterHotkey (int id) throws HotkeyException;
+	
+	public void setListener (IHotkeyListener listener);
 	
 }
