fc42f1aac901fb5dfa6d9e6b59f43739ea056905
haku <haku@vaguehope.com>
2010-02-27 19:09:03 +0000

    init
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@372 2128fabd-03f8-4a31-9ba6-05665b9bae8a

---
+++ net.sparktank.morrigan.hotkeyimpl.jintellitype/src/net/sparktank/morrigan/hotkeyimpl/jintellitype/HotkeyEngine.java
@@ -0,0 +1,24 @@
+package net.sparktank.morrigan.hotkeyimpl.jintellitype;
+
+import net.sparktank.morrigan.engines.hotkey.HotkeyException;
+import net.sparktank.morrigan.engines.hotkey.IHotkeyEngine;
+import net.sparktank.morrigan.engines.hotkey.IHotkeyListener;
+
+public class HotkeyEngine implements IHotkeyEngine {
+	
+	@Override
+	public void registerHotkey(int id, int key, boolean ctrl, boolean shift, boolean alt, boolean supr) throws HotkeyException {
+		
+	}
+	
+	@Override
+	public void unregisterHotkey(int id) throws HotkeyException {
+		
+	}
+	
+	@Override
+	public void setListener(IHotkeyListener listener) {
+		
+	}
+	
+}
