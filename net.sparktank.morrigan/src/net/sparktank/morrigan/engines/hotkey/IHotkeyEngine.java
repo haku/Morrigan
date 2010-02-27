1a5329935db4de1244da62003e0491d6737c219a
haku <haku@vaguehope.com>
2010-02-27 19:19:37 +0000

    engine about and classpath methods
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@373 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan/src/net/sparktank/morrigan/engines/hotkey/IHotkeyEngine.java
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/engines/hotkey/IHotkeyEngine.java
@@ -1,6 +1,21 @@
 package net.sparktank.morrigan.engines.hotkey;
 
+import java.io.File;
+
 public interface IHotkeyEngine {
+	
+	/**
+	 * Returns the description of this engine.
+	 */
+	public String getAbout ();
+	
+	/**
+	 * This method will be called by the plugin loader shortly after
+	 * the instance of the engine is created.
+	 * @param classPath The array of File objects used by the
+	 * classloader to load the engine.
+	 */
+	public void setClassPath (File[] classPath);
 	
 	public void registerHotkey (int id, int key, boolean ctrl, boolean shift, boolean alt, boolean supr) throws HotkeyException;
 	
