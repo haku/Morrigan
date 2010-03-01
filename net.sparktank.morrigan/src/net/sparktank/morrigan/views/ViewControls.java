c3902f1b76fe065e03719fda3472cc7e954cdbb9
haku <haku@vaguehope.com>
2010-03-01 23:41:39 +0000

    First draft of new player.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@404 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan/src/net/sparktank/morrigan/views/ViewControls.java
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/views/ViewControls.java
@@ -2,27 +2,28 @@ package net.sparktank.morrigan.views;
 
 
 import net.sparktank.morrigan.Activator;
+import net.sparktank.morrigan.display.ScreenPainter;
 import net.sparktank.morrigan.helpers.OrderHelper.PlaybackOrder;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.FormAttachment;
 import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.ui.ISizeProvider;
-import org.eclipse.ui.part.ViewPart;
 
-public class ViewControls extends ViewPart implements ISizeProvider {
+public class ViewControls extends AbstractPlayerView implements ISizeProvider {
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	
 	public static final String ID = "net.sparktank.morrigan.views.ViewControls";
@@ -30,8 +31,12 @@ public class ViewControls extends ViewPart implements ISizeProvider {
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	
 	public void createPartControl(Composite parent) {
+		super.createPartControl(parent);
+		
 		makeIcons();
 		makeControls(parent);
+		
+		callUpdateStatus();
 	}
 	
 	@Override
@@ -51,11 +56,14 @@ public class ViewControls extends ViewPart implements ISizeProvider {
 	private Image iconStop;
 	private Image iconPrev;
 	private Image iconNext;
+	private Image iconScreen;
 	private int preferedHeight = -1;
 	
 	private Composite parentComposite;
 	private Menu orderModeMenu;
+	private Menu fullscreenMenu;
 	private Button btnOrderMode;
+	private Label lblStatus;
 	
 	private void makeIcons () {
 		iconPlay = Activator.getImageDescriptor("icons/play.gif").createImage();
@@ -63,12 +71,16 @@ public class ViewControls extends ViewPart implements ISizeProvider {
 		iconStop = Activator.getImageDescriptor("icons/stop.gif").createImage();
 		iconPrev = Activator.getImageDescriptor("icons/prev.gif").createImage();
 		iconNext = Activator.getImageDescriptor("icons/next.gif").createImage();
+		iconScreen = Activator.getImageDescriptor("icons/display.gif").createImage();
 	}
 	
 	private void disposeIcons () {
 		iconPlay.dispose();
 		iconPause.dispose();
 		iconStop.dispose();
+		iconPrev.dispose();
+		iconNext.dispose();
+		iconScreen.dispose();
 	}
 	
 	private void makeControls (Composite parent) {
@@ -77,11 +89,36 @@ public class ViewControls extends ViewPart implements ISizeProvider {
 		// Off-screen controls.
 		
 		orderModeMenu = new Menu(parent.getShell(), SWT.POP_UP);
-		for (PlaybackOrder o : PlaybackOrder.values()) {
+		for (final OrderSelectAction a : getOrderMenuActions()) {
 			MenuItem menuItem = new MenuItem(orderModeMenu, SWT.PUSH);
-			menuItem.setText(o.toString());
-			menuItem.addSelectionListener(menuOrderModeListener);
+			menuItem.setText(a.getText());
+			// TODO read-back checked state of action.
+			menuItem.addSelectionListener(new SelectionListener() {
+				@Override
+				public void widgetSelected(SelectionEvent e) {
+					a.run();
+				}
+				@Override
+				public void widgetDefaultSelected(SelectionEvent e) {}
+			});
 		}
+		
+		fullscreenMenu = new Menu(parent.getShell(), SWT.POP_UP);
+		for (final FullScreenAction a : getFullScreenActions()) {
+			MenuItem menuItem = new MenuItem(fullscreenMenu, SWT.PUSH);
+			menuItem.setText(a.getText());
+			// TODO read-back checked state of action.
+			menuItem.addSelectionListener(new SelectionListener() {
+				@Override
+				public void widgetSelected(SelectionEvent e) {
+					a.run();
+				}
+				@Override
+				public void widgetDefaultSelected(SelectionEvent e) {}
+			});
+		}
+		
+		
 		
 		// On-screen controls.
 		
@@ -92,47 +129,71 @@ public class ViewControls extends ViewPart implements ISizeProvider {
 		FormData formData;
 		
 		Button btnStop = new Button(parent, SWT.PUSH);
+		Button btnPlayPause = new Button(parent, SWT.PUSH);
+		Button btnPrev = new Button(parent, SWT.PUSH);
+		Button btnNext = new Button(parent, SWT.PUSH);
+		lblStatus = new Label(parent, SWT.NONE);
+		Canvas canvas = new Canvas(parent, SWT.NONE);
+		btnOrderMode = new Button(parent, SWT.PUSH);
+		Button btnFullscreen = new Button(parent, SWT.PUSH);
+		
 		btnStop.setImage(iconStop);
 		formData = new FormData();
 		formData.top = new FormAttachment(0, sep);
 		formData.left = new FormAttachment(0, sep);
 		btnStop.setLayoutData(formData);
+		btnStop.addSelectionListener(btnStopListener);
 		
-		Button btnPlayPause = new Button(parent, SWT.PUSH);
-		btnPlayPause.setImage(iconPlay);
+		btnPlayPause.setImage(iconPause);
 		formData = new FormData();
 		formData.top = new FormAttachment(0, sep);
 		formData.left = new FormAttachment(btnStop, sep);
 		btnPlayPause.setLayoutData(formData);
-
-		Button btnPrev = new Button(parent, SWT.PUSH);
+		btnPlayPause.addSelectionListener(btnPlayPauseListener);
+		
 		btnPrev.setImage(iconPrev);
 		formData = new FormData();
 		formData.top = new FormAttachment(0, sep);
 		formData.left = new FormAttachment(btnPlayPause, sep);
 		btnPrev.setLayoutData(formData);
+		btnPrev.addSelectionListener(btnPrevListener);
 		
-		Button btnNext = new Button(parent, SWT.PUSH);
 		btnNext.setImage(iconNext);
 		formData = new FormData();
 		formData.top = new FormAttachment(0, sep);
 		formData.left = new FormAttachment(btnPrev, sep);
 		btnNext.setLayoutData(formData);
+		btnNext.addSelectionListener(btnNextListener);
 		
-		Label label = new Label(parent, SWT.NONE);
-		label.setText("Playing 0:15 of 12:34");
 		formData = new FormData();
-		formData.top = new FormAttachment(50, -(label.computeSize(SWT.DEFAULT, SWT.DEFAULT).y)/2);
+		formData.top = new FormAttachment(50, -(lblStatus.computeSize(SWT.DEFAULT, SWT.DEFAULT).y)/2);
 		formData.left = new FormAttachment(btnNext, sep*2);
-		label.setLayoutData(formData);
+		formData.right = new FormAttachment(btnOrderMode, -sep);
+		lblStatus.setLayoutData(formData);
 		
-		btnOrderMode = new Button(parent, SWT.PUSH);
 		btnOrderMode.setText(PlaybackOrder.SEQUENTIAL.toString());
 		formData = new FormData();
 		formData.top = new FormAttachment(0, sep);
-		formData.right = new FormAttachment(100, -sep);
+		formData.right = new FormAttachment(canvas, -sep);
 		btnOrderMode.setLayoutData(formData);
 		btnOrderMode.addSelectionListener(btnOrderModeListener);
+		
+		formData = new FormData();
+		formData.top = new FormAttachment(0, sep);
+		formData.right = new FormAttachment(btnFullscreen, -sep);
+		formData.bottom = new FormAttachment(100, -sep);
+		formData.width = 80;
+		canvas.setLayoutData(formData);
+		canvas.setLayout(new FillLayout());
+		canvas.addPaintListener(new ScreenPainter(canvas)); // FIXME do we really want this?  Better to hide video space when not needed.
+		setMediaFrameParent(canvas);
+		
+		btnFullscreen.setImage(iconScreen);
+		formData = new FormData();
+		formData.top = new FormAttachment(0, sep);
+		formData.right = new FormAttachment(100, -sep);
+		btnFullscreen.setLayoutData(formData);
+		btnFullscreen.addSelectionListener(btnFullscreenListener);
 		
 		preferedHeight = sep + btnStop.computeSize(SWT.DEFAULT, SWT.DEFAULT).y + sep;
 	}
@@ -152,14 +213,82 @@ public class ViewControls extends ViewPart implements ISizeProvider {
 	}
 	
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//	Events.
+	
+	@Override
+	protected void updateStatus() {
+		if (isDisposed()) return;
+		
+		if (getCurrentItem() != null && getCurrentList() != null) {
+			switch (internal_getPlayState()) {
+				case Playing:
+//					setTitleImage(iconPlay);
+					break;
+					
+				case Paused:
+//					setTitleImage(iconPause);
+					break;
+					
+				case Loading:
+//					setTitleImage(iconPlay); // FIXME new icon?
+					break;
+					
+				case Stopped:
+//					setTitleImage(iconStop);
+					break;
+					
+			}
+			
+			lblStatus.setText(
+					"Playing: " + getCurrentPosition() + " : " + getCurrentItem().toString()
+			);
+			
+		} else {
+//			setTitleImage(iconStop);
+			lblStatus.setText("Idle.");
+		};
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 //	Control events.
 	
-	private SelectionAdapter menuOrderModeListener = new SelectionAdapter () {
+	private SelectionListener btnStopListener = new SelectionListener() {
 		@Override
 		public void widgetSelected(SelectionEvent e) {
-			btnOrderMode.setText(((MenuItem) e.widget).getText());
+			stopAction.run(); // FIXME don't call this like this!
 		}
+		@Override
+		public void widgetDefaultSelected(SelectionEvent e) {}
 	};
+	
+	private SelectionListener btnPlayPauseListener = new SelectionListener() {
+		@Override
+		public void widgetSelected(SelectionEvent e) {
+			pauseAction.run(); // FIXME don't call this like this!
+		}
+		@Override
+		public void widgetDefaultSelected(SelectionEvent e) {}
+	};
+	
+	private SelectionListener btnPrevListener = new SelectionListener() {
+		@Override
+		public void widgetSelected(SelectionEvent e) {
+			prevAction.run(); // FIXME don't call this like this!
+		}
+		@Override
+		public void widgetDefaultSelected(SelectionEvent e) {}
+	};
+	
+	private SelectionListener btnNextListener = new SelectionListener() {
+		@Override
+		public void widgetSelected(SelectionEvent e) {
+			nextAction.run(); // FIXME don't call this like this!
+		}
+		@Override
+		public void widgetDefaultSelected(SelectionEvent e) {}
+	};
+	
+	// TODO extract common code from these following two methods.
 	
 	private SelectionListener btnOrderModeListener = new SelectionListener() {
 		@Override
@@ -174,6 +303,18 @@ public class ViewControls extends ViewPart implements ISizeProvider {
 		public void widgetDefaultSelected(SelectionEvent e) {}
 	};
 	
+	private SelectionListener btnFullscreenListener = new SelectionListener() {
+		@Override
+		public void widgetSelected(SelectionEvent e) {
+			Rectangle rect = ((Button)e.widget).getBounds();
+			Point pt = new Point(rect.x, rect.y + rect.height);
+			pt = parentComposite.toDisplay(pt);
+			fullscreenMenu.setLocation(pt);
+			fullscreenMenu.setVisible(true);
+		}
+		@Override
+		public void widgetDefaultSelected(SelectionEvent e) {}
+	};
 	
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 }
