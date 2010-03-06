90a09c533621ac67c42ecc510387161203d5a779
haku <haku@vaguehope.com>
2010-03-06 10:54:41 +0000

    extra PlayItem to own class.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@458 2128fabd-03f8-4a31-9ba6-05665b9bae8a

---
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/model/media/PlayItem.java
@@ -0,0 +1,26 @@
+package net.sparktank.morrigan.model.media;
+
+import net.sparktank.morrigan.helpers.EqualHelper;
+
+public class PlayItem {
+	
+	public MediaList list;
+	public MediaItem item;
+	
+	public PlayItem (MediaList list, MediaItem item) {
+		this.list = list;
+		this.item = item;
+	}
+	
+	@Override
+	public boolean equals (Object obj) {
+		if ( obj == null ) return false;
+		if ( this == obj ) return true;
+		if ( !(obj instanceof PlayItem) ) return false;
+		PlayItem that = (PlayItem)obj;
+		
+		return EqualHelper.areEqual(list, that.list)
+		&& EqualHelper.areEqual(item, that.item);
+	}
+	
+}
