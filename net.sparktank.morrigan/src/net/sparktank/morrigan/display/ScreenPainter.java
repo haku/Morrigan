8bd27a9a007233816208226f519b322a4cb7fa19
haku <haku@vaguehope.com>
2010-03-12 20:13:00 +0000

    Improved title drawing.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@501 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan/src/net/sparktank/morrigan/display/ScreenPainter.java
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/display/ScreenPainter.java
@@ -7,6 +7,8 @@ import net.sparktank.morrigan.model.media.PlayItem;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.FontData;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Canvas;
@@ -25,12 +27,12 @@ public class ScreenPainter implements PaintListener {
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	
 	private final Canvas canvas;
-	private final ScreenType type;
+	private final ScreenType screenType;
 	private TitleProvider titleProvider;
 	
 	public ScreenPainter(Canvas canvas, ScreenType type) {
 		this.canvas = canvas;
-		this.type = type;
+		this.screenType = type;
 		
 		canvas.setBackground(canvas.getDisplay().getSystemColor(SWT.COLOR_BLACK));
 		canvas.setForeground(canvas.getDisplay().getSystemColor(SWT.COLOR_WHITE));
@@ -57,50 +59,98 @@ public class ScreenPainter implements PaintListener {
 	public void paintControl(PaintEvent e) {
 		Rectangle clientArea = canvas.getClientArea();
 		
-		if (type != ScreenType.TINY) {
+		if (screenType != ScreenType.TINY) {
 			PlayItem item = titleProvider.getItem();
 			Point centre = new Point(clientArea.width/2, clientArea.height/2);
 			
 			String text;
 			
 			if (item != null && item.item != null) {
+				Font font = e.gc.getFont();
+				Font font2;
+				Font font3;
+				
+				FontData fontData = e.gc.getFont().getFontData()[0];
+				if (screenType == ScreenType.LARGE) {
+					font2 = new Font(e.gc.getDevice(), fontData.getName(),
+							fontData.getHeight() * 2, fontData.getStyle());
+					font3 = new Font(e.gc.getDevice(), fontData.getName(),
+							fontData.getHeight() * 3, fontData.getStyle());
+					
+				} else {
+					font2 = new Font(e.gc.getDevice(), fontData);
+					font3 = new Font(e.gc.getDevice(), fontData.getName(),
+							fontData.getHeight() * 2, fontData.getStyle());
+				}
+				
 				text = item.item.getTitle();
 				text = text.substring(0, text.lastIndexOf("."));
 				text = text.replace(" - ", "\n");
+				e.gc.setFont(font3);
+				Rectangle rect = drawTextHVCen(e, centre.x, centre.y, text);
 				
-				text = text + "\n\n" + item.item.getStartCount() + " / " + item.item.getEndCount();
+				text = item.item.getStartCount() + " / " + item.item.getEndCount();
 				if (item.item.getDuration() > 0) {
 					text = text + "   " + TimeHelper.formatTime(item.item.getDuration());
 				}
+				e.gc.setFont(font2);
+				Rectangle rect2 = drawTextHCen(e, centre.x, rect.y + rect.height, text);
 				
 				if (item.list != null) {
-					text = "(" + item.list.getListName() + ")\n\n" + text;
+					text = "(" + item.list.getListName() + ")";
+					Point textSize = e.gc.textExtent(text);
+					drawTextHCen(e, centre.x, rect.y - textSize.y, text);
 				}
+				
+				e.gc.setFont(font);
+				
+				if (screenType == ScreenType.LARGE) {
+					text = item.item.getFilepath();
+					drawTextHCen(e, centre.x, rect2.y + rect2.height, text);
+				}
+				
+				font2.dispose();
+				font3.dispose();
 				
 			} else {
 				text = "[ Morrigan ]";
+				drawTextHVCen(e, centre.x, centre.y, text);
 			}
-			
-			drawTextCen(e, centre.x, centre.y, text);
 			
 		}
 	}
 	
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	
-	private void drawTextCen (PaintEvent e, int x, int y, String... text) {
-		for (int i=0; i<text.length; i++) {
-			Point textSize = e.gc.textExtent(text[i]);
-			e.gc.drawText(text[i],
-					x-(textSize.x/2),
-					y + (textSize.y) * i - (textSize.y * text.length)/2, 
-					SWT.TRANSPARENT);
-		}
+	private Rectangle drawTextHCen (PaintEvent e, int x, int top, String text) {
+		Point textSize = e.gc.textExtent(text);
+		int _left = x - (textSize.x / 2);
+		e.gc.drawText(text, _left, top, SWT.TRANSPARENT);
+		return new Rectangle(_left, top, textSize.x, textSize.y);
 	}
 	
-	private void drawTextCen (PaintEvent e, int x, int y, String text) {
+	private Rectangle drawTextHVCen (PaintEvent e, int x, int y, String... text) {
+		Rectangle ret = new Rectangle(x, y, 0, 0);
+		
+		for (int i=0; i < text.length; i++) {
+			Point textSize = e.gc.textExtent(text[i]);
+			int _left = x - (textSize.x / 2);
+			int _top = y + (textSize.y) * i - (textSize.y * text.length) / 2;
+			
+			e.gc.drawText(text[i], _left, _top, SWT.TRANSPARENT);
+			
+			if (ret.x > _left) ret.x = _left;
+			if (ret.y > _top) ret.y = _top;
+			if (ret.width < textSize.x) ret.width = textSize.x;
+			ret.height = ret.height + textSize.y;
+		}
+		
+		return ret;
+	}
+	
+	private Rectangle drawTextHVCen (PaintEvent e, int x, int y, String text) {
 		String[] split = text.split("\n");
-		drawTextCen(e, x, y, split);
+		return drawTextHVCen(e, x, y, split);
 	}
 	
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
