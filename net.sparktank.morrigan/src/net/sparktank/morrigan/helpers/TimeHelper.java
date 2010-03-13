a17094525777520a726b3c00f8ad0eec1681b7de
haku <haku@vaguehope.com>
2010-03-13 03:14:16 +0000

    time formatter knows about hours.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@524 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan/src/net/sparktank/morrigan/helpers/TimeHelper.java
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/helpers/TimeHelper.java
@@ -2,11 +2,17 @@ package net.sparktank.morrigan.helpers;
 
 public class TimeHelper {
 	
-	public static String formatTime (long seconds) {
-		String s = "0" + seconds % 60;
-		s = s.substring(s.length()-2);
-		int m = (int) Math.floor(seconds/60f);
-		return m + ":" + s;
+	public static String formatTime (long x) {
+//		String s = "0" + x % 60;
+//		s = s.substring(s.length()-2);
+//		int m = (int) Math.floor(x/60f);
+//		return m + ":" + s;
+		
+		if (x >= 3600) {
+			return String.format("%d:%02d:%02d", x / 3600, (x % 3600) / 60, (x % 60));
+		} else {
+			return String.format("%d:%02d", (x % 3600) / 60, (x % 60));
+		}
 	}
 	
 }
