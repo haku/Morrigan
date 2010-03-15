c52eec783a3b6b3cba9c3b1d887f84fe7247e4c5
haku <haku@vaguehope.com>
2010-03-15 22:21:19 +0000

    smarter hotkey logic when there are multiple windows open.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@537 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan/src/net/sparktank/morrigan/engines/hotkey/IHotkeyListener.java
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/engines/hotkey/IHotkeyListener.java
@@ -2,6 +2,10 @@ package net.sparktank.morrigan.engines.hotkey;
 
 public interface IHotkeyListener {
 	
+	public enum CanDo {YES, NO, MAYBE};
+	
+	public CanDo canDoKeyPress (int id);
+	
 	public void onKeyPress (int id);
 	
 }
