dff3140e2fc794e80bd083adcdc7f65c9e4d3140
haku <haku@vaguehope.com>
2010-03-19 00:04:06 +0000

    rough implementation of console view.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@552 2128fabd-03f8-4a31-9ba6-05665b9bae8a

---
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/helpers/Tracer.java
@@ -0,0 +1,42 @@
+package net.sparktank.morrigan.helpers;
+
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.MessageConsole;
+import org.eclipse.ui.console.MessageConsoleStream;
+
+public class Tracer {
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	static public void showConsole () {
+		ConsolePlugin.getDefault().getConsoleManager().showConsoleView(getMessageConsole());
+	}
+	
+	static public void appendToConsole (String s) {
+		getMessageConsoleStream().println(s);  
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	static private MessageConsoleStream messageConsoleStream = null;
+	
+	static private MessageConsoleStream getMessageConsoleStream () {
+		if (messageConsoleStream == null) {
+			messageConsole = getMessageConsole();
+			messageConsoleStream = messageConsole.newMessageStream();
+		}
+		return messageConsoleStream;
+	}
+	
+	static private MessageConsole messageConsole;
+	
+	static private MessageConsole getMessageConsole () {
+		if (messageConsole == null) {
+			messageConsole = new MessageConsole("Morrigan", null);
+			ConsolePlugin.getDefault().getConsoleManager().addConsoles(new IConsole[] { messageConsole });
+		}
+		return messageConsole;
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+}
