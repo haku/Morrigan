dfc21c81d4c22885447c1916a35d40d5b2b2fd5b
haku <haku@vaguehope.com>
2010-05-01 10:30:54 +0000

    better navigation and error handling.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@693 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan/src/net/sparktank/morrigan/server/MediaHandler.java
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/server/MediaHandler.java
@@ -9,6 +9,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import net.sparktank.morrigan.exceptions.MorriganException;
+import net.sparktank.morrigan.gui.helpers.ErrorHelper;
 import net.sparktank.morrigan.gui.model.MediaExplorerItem;
 import net.sparktank.morrigan.model.MediaItem;
 import net.sparktank.morrigan.model.MediaListFactory;
@@ -23,20 +25,30 @@ public class MediaHandler extends AbstractHandler {
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	
 	public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-		StringBuilder sb = null;
+		System.err.println("request:t=" + target);
 		
-		if (target.equals("/")) {
-			sb = getMediaLists();
-			
-		} else {
-			String d = target.substring(1,target.indexOf("/", 2));
-			String id = target.substring(d.length() + 2);
-			if (d.equals("library")) {
-				sb = getLibrary(id);
+		response.getWriter().println("<h1>Media desu~</h1>");
+		response.getWriter().println("<p><a href=\"/\">home</a></p>");
+		
+		StringBuilder sb = null;
+		try {
+			if (target.equals("/")) {
+				sb = getMediaLists();
 				
-			} else if (d.equals("playlist")) {
-				
+			} else {
+				String d = target.substring(1,target.indexOf("/", 2));
+				String id = target.substring(d.length() + 2);
+				if (d.equals("library")) {
+					sb = getLibrary(id);
+					
+				} else if (d.equals("playlist")) {
+					
+				}
 			}
+			
+		} catch (Throwable t) {
+			sb = new StringBuilder();
+			sb.append(ErrorHelper.getStackTrace(t));
 		}
 		
 		if (sb != null) {
@@ -52,8 +64,6 @@ public class MediaHandler extends AbstractHandler {
 	private StringBuilder getMediaLists () {
 		StringBuilder sb = new StringBuilder();
 		ArrayList<MediaExplorerItem> items = new ArrayList<MediaExplorerItem>();
-		
-		sb.append("<h1>Media desu~</h1>");
 		
 		sb.append("<h2>Libraries</h2>");
 		items.addAll(LibraryHelper.getAllLibraries());
@@ -74,28 +84,22 @@ public class MediaHandler extends AbstractHandler {
 		return sb;
 	}
 	
-	private StringBuilder getLibrary (String id) {
-		try {
-			StringBuilder sb = new StringBuilder();
-			sb.append("<h1>"+id+"</h1>");
-			
-			String f = LibraryHelper.getFullPathToLib(id);
-			MediaLibrary ml = MediaListFactory.makeMediaLibrary(f);
-			ml.read();
-			List<MediaItem> mediaTracks = ml.getMediaTracks();
-			
-			sb.append("<ul>");
-			for (MediaItem i : mediaTracks) {
-				sb.append("<li><a href=\"/media/library/" + id + "/00000\">" + i.getTitle() + "</a></li>");
-			}
-			sb.append("</ul>");
-			
-			return sb;
-			
-		} catch (Throwable t) {
-			t.printStackTrace();
-			return null;
+	private StringBuilder getLibrary (String id) throws MorriganException {
+		StringBuilder sb = new StringBuilder();
+		sb.append("<h2>"+id+"</h2>");
+		
+		String f = LibraryHelper.getFullPathToLib(id);
+		MediaLibrary ml = MediaListFactory.makeMediaLibrary(f);
+		ml.read();
+		List<MediaItem> mediaTracks = ml.getMediaTracks();
+		
+		sb.append("<ul>");
+		for (MediaItem i : mediaTracks) {
+			sb.append("<li><a href=\"/media/library/" + id + "/00000\">" + i.getTitle() + "</a></li>");
 		}
+		sb.append("</ul>");
+		
+		return sb;
 	}
 	
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
