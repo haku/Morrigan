6c3997d4827a4cfd6729f61ecacf0d3172752afa
haku <haku@vaguehope.com>
2010-05-04 20:27:01 +0000

    MediaFeed
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@717 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan/src/net/sparktank/morrigan/server/helpers/GenericFeed.java
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/server/helpers/GenericFeed.java
@@ -1,5 +1,6 @@
 package net.sparktank.morrigan.server.helpers;
 
+import java.io.File;
 import java.io.StringWriter;
 
 import javax.xml.transform.OutputKeys;
@@ -11,8 +12,10 @@ import javax.xml.transform.TransformerFactoryConfigurationError;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
+import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
 public class GenericFeed {
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -56,6 +59,48 @@ public class GenericFeed {
 		}
 		
 		return result.getWriter().toString();
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	static public void addElement (Document doc, Node element, String newElement, int i) {
+		addElement(doc, element, newElement, String.valueOf(i));
+	}
+	
+	static public void addElement (Document doc, Node element, String newElement, long l) {
+		addElement(doc, element, newElement, String.valueOf(l));
+	}
+	
+	static public void addElement (Document doc, Node element, String newElement, String textContent) {
+		Element e = doc.createElement(newElement);
+		e.setTextContent(textContent);
+		element.appendChild(e);
+	}
+	
+	static public void addLink (Document doc, Node element, String href) {
+		addLink(doc, element, href, null);
+	}
+	
+	static public void addLink (Document doc, Node element, String href, String rel) {
+		Element link = doc.createElement("link");
+		
+		if (rel != null) {
+			Attr attRel = doc.createAttribute("rel");
+			attRel.setValue(rel);
+			link.getAttributes().setNamedItem(attRel);
+		}
+		
+		Attr attHref = doc.createAttribute("href");
+		attHref.setValue(href);
+		link.getAttributes().setNamedItem(attHref);
+		
+		element.appendChild(link);
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	static public String filenameFromPath (String path) {
+		return path.substring(path.lastIndexOf(File.separator) + 1);
 	}
 	
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
---
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/server/helpers/MediaFeed.java
@@ -0,0 +1,70 @@
+package net.sparktank.morrigan.server.helpers;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import net.sparktank.morrigan.model.explorer.MediaExplorerItem;
+import net.sparktank.morrigan.model.library.LibraryHelper;
+import net.sparktank.morrigan.model.playlist.PlaylistHelper;
+import net.sparktank.morrigan.player.Player;
+import net.sparktank.morrigan.player.PlayerRegister;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+public class MediaFeed extends GenericFeed {
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	public MediaFeed () {
+		super();
+		mediaListsToFeed(getDoc());
+	}
+
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	private void mediaListsToFeed(Document doc) {
+		Node feed = doc.getFirstChild(); // This should get the "feed" element.
+		addElement(doc, feed, "title", "Morrigan media desu~");
+		addLink(doc, feed, "/media" , "self");
+		addLink(doc, feed, "/media/newlib", "newlib");
+			
+		List<Player> players = PlayerRegister.getPlayers();
+		
+		for (int n = 0; n < 2; n++) {
+			ArrayList<MediaExplorerItem> items = new ArrayList<MediaExplorerItem>();
+			String type = null;
+			
+			switch (n) {
+				case 0:
+					type="library";
+					items.addAll(LibraryHelper.getAllLibraries());
+					break;
+				
+				case 1:
+					type="playlist";
+					items.addAll(PlaylistHelper.getAllPlaylists());
+					break;
+				
+			}
+			
+			for (MediaExplorerItem i : items) {
+				String fileName = i.identifier.substring(i.identifier.lastIndexOf(File.separator) + 1);
+				
+				Element entry = doc.createElement("entry");
+				addElement(doc, entry, "title", i.title);
+				addLink(doc, entry, "/media/" + type + "/" + fileName);
+				
+				for (Player p : players) {
+					addLink(doc, entry, "/player/" + p.getId() + "/play/" + fileName, "play");
+				}
+				
+				feed.appendChild(entry);
+			}
+			
+		}
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+}
