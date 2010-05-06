8c08bc544ddd3cede331520af70bbc9e5c82c358
haku <haku@vaguehope.com>
2010-05-06 12:41:31 +0000

    Beginning of framework for linking to remote libs.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@722 2128fabd-03f8-4a31-9ba6-05665b9bae8a

---
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/gui/actions/NewRemoteLibraryAction.java
@@ -0,0 +1,72 @@
+package net.sparktank.morrigan.gui.actions;
+
+import net.sparktank.morrigan.exceptions.MorriganException;
+import net.sparktank.morrigan.gui.Activator;
+import net.sparktank.morrigan.gui.dialogs.MorriganMsgDlg;
+import net.sparktank.morrigan.gui.views.ViewMediaExplorer;
+import net.sparktank.morrigan.model.library.RemoteLibraryHelper;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.dialogs.InputDialog;
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.actions.ActionFactory.IWorkbenchAction;
+
+
+public class NewRemoteLibraryAction extends Action implements IWorkbenchAction {
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	private IWorkbenchWindow window;
+	
+	public NewRemoteLibraryAction (IWorkbenchWindow window) {
+		super();
+		this.window = window;
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	@Override
+	public String getText() { return "New remote library..."; }
+	
+	@Override
+	public String getId() { return "newremlib"; }
+	
+	@Override
+	public org.eclipse.jface.resource.ImageDescriptor getImageDescriptor() {
+		// TODO choose icon.
+		return Activator.getImageDescriptor("icons/library-remote.gif");
+	};
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	@Override
+	public void run () {
+		InputDialog dlg = new InputDialog(
+				Display.getCurrent().getActiveShell(),
+				"", "Enter library URL.", "http://", null);
+		if (dlg.open() == Window.OK) {
+			
+			String libUrl = dlg.getValue();
+			try {
+				RemoteLibraryHelper.createRemoteLib(libUrl);
+			} catch (MorriganException e) {
+				new MorriganMsgDlg(e).open();
+				return;
+			}
+			
+			// refresh explorer.
+			IWorkbenchPage page = window.getActivePage();
+			ViewMediaExplorer view = (ViewMediaExplorer) page.findView(ViewMediaExplorer.ID);
+			view.refresh();
+		}
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	@Override
+	public void dispose() {}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+}
