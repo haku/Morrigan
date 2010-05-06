2eba7ee32cc69889c983ec1146e0178ecb223c0d
haku <haku@vaguehope.com>
2010-05-06 16:50:24 +0000

    Start of feedreading.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@731 2128fabd-03f8-4a31-9ba6-05665b9bae8a

---
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/server/feedreader/GenericFeedReader.java
@@ -0,0 +1,63 @@
+package net.sparktank.morrigan.server.feedreader;
+
+import java.io.IOException;
+import java.util.List;
+
+import net.sparktank.morrigan.model.MediaItem;
+import net.sparktank.morrigan.server.feedwriters.XmlHelper;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public abstract class GenericFeedReader {
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	private final Document doc;
+	
+	private List<MediaItem> mediaItemList = null;
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	public GenericFeedReader (String xmlString) throws SAXException, IOException, FeedParseException {
+		doc = XmlHelper.xmlStringToDocument(xmlString);
+		Node feed = doc.getFirstChild(); // This should get the "feed" element.
+		if (feed.getNodeName().equals("feed")) {
+			NodeList entries = feed.getChildNodes();
+			if (entries.getLength() < 1) {
+				throw new FeedParseException("Feed element contains no entry elements.");
+			}
+			
+			for (int i = 0; i < entries.getLength(); i++) {
+				Node entry = entries.item(i);
+				if (entry.getNodeName().equals("entry")) {
+					parseEntry(entry);
+				}
+			}
+			
+		} else {
+			throw new IllegalArgumentException("First node '"+feed.getNodeName()+"' != feed.");
+		}
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	protected abstract void parseEntry (Node entry) throws FeedParseException;
+	
+	protected void addMediaItem (MediaItem mi) {
+		mediaItemList.add(mi);
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	protected Document getDoc () {
+		return doc;
+	}
+	
+	public List<MediaItem> getMediaItemList() {
+		return mediaItemList;
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+}
