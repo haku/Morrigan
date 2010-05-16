86a72e2a5b17f5be50cf6c055e38393edd4b6b80
haku <haku@vaguehope.com>
2010-05-16 18:12:50 +0000

    use keyTyped not keyReleased.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@758 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan.playbackimpl.dsj/src/net/sparktank/morrigan/playbackimpl/dsj/PlaybackEngine.java
+++ net.sparktank.morrigan.playbackimpl.dsj/src/net/sparktank/morrigan/playbackimpl/dsj/PlaybackEngine.java
@@ -297,13 +297,13 @@ public class PlaybackEngine implements IPlaybackEngine {
 	
 	private KeyListener keyListener = new KeyListener() {
 		@Override
-		public void keyReleased(KeyEvent key) {
+		public void keyTyped(KeyEvent key) {
 			callOnKeyPressListener(key.getKeyCode());
 		}
 		@Override
-		public void keyTyped(KeyEvent arg0) {}
+		public void keyReleased(KeyEvent key) {}
 		@Override
-		public void keyPressed(KeyEvent arg0) {}
+		public void keyPressed(KeyEvent key) {}
 	};
 	
 	private PropertyChangeListener propertyChangeLlistener = new PropertyChangeListener() {
