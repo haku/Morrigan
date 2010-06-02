3cf1340a6bd53bd3635d4728b021b51ade9bdd86
haku <haku@vaguehope.com>
2010-06-02 19:55:05 +0000

    @Deprecated old parsing code
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@782 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan/src/net/sparktank/morrigan/server/feedreader/FeedParseException.java
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/server/feedreader/FeedParseException.java
@@ -2,6 +2,7 @@ package net.sparktank.morrigan.server.feedreader;
 
 import net.sparktank.morrigan.exceptions.MorriganException;
 
+@Deprecated
 public class FeedParseException extends MorriganException {
 	
 	private static final long serialVersionUID = 8518574728577998800L;
--- net.sparktank.morrigan/src/net/sparktank/morrigan/server/feedreader/IEntryHandler.java
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/server/feedreader/IEntryHandler.java
@@ -2,6 +2,7 @@ package net.sparktank.morrigan.server.feedreader;
 
 import org.w3c.dom.Node;
 
+@Deprecated
 public interface IEntryHandler {
 	
 	public abstract void parseEntry (Node entry) throws FeedParseException;
