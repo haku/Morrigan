4f9f123052fea6818e4c7796efb4059997cee753
haku <haku@vaguehope.com>
2010-06-19 17:35:42 +0000

    initial impl of fetching remote files.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@842 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan/src/net/sparktank/morrigan/model/library/remote/RemoteMediaLibrary.java
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/model/library/remote/RemoteMediaLibrary.java
@@ -1,8 +1,15 @@
 package net.sparktank.morrigan.model.library.remote;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
+import java.net.SocketException;
 import java.net.URL;
+import java.net.UnknownHostException;
 import java.util.Date;
 
 import net.sparktank.morrigan.engines.playback.NotImplementedException;
@@ -12,7 +19,10 @@ import net.sparktank.morrigan.model.MediaTrack;
 import net.sparktank.morrigan.model.TaskEventListener;
 import net.sparktank.morrigan.model.library.AbstractMediaLibrary;
 import net.sparktank.morrigan.model.library.DbException;
+import net.sparktank.morrigan.model.library.MediaLibraryTrack;
 import net.sparktank.morrigan.model.library.SqliteLayer;
+import net.sparktank.morrigan.server.HttpClient;
+import net.sparktank.morrigan.server.HttpClient.IHttpStreamHandler;
 import net.sparktank.morrigan.server.feedreader.MediaListFeedParser2;
 
 public class RemoteMediaLibrary extends AbstractMediaLibrary {
@@ -144,8 +154,77 @@ public class RemoteMediaLibrary extends AbstractMediaLibrary {
 //	Actions.
 	
 	@Override
-	public void copyMediaItemFile(MediaItem item, File targetFile) throws MorriganException {
-		throw new NotImplementedException();
+	public void copyMediaItemFile(MediaItem mi, File targetDirectory) throws MorriganException {
+		if (mi instanceof MediaLibraryTrack) {
+			if (!targetDirectory.isDirectory()) {
+				throw new IllegalArgumentException("targetDirectory must be a directory.");
+			}
+			
+			final File targetFile = new File(targetDirectory.getAbsolutePath() + File.separatorChar
+					+ mi.getFilepath().substring(mi.getFilepath().lastIndexOf(File.separatorChar) + 1));
+			
+			if (!targetFile.exists()) {
+				MediaLibraryTrack mlt = (MediaLibraryTrack) mi;
+				
+				String serverUrlString = getDbLayer().getProp(DBKEY_SERVERURL);
+				URL serverUrl;
+				try {
+					serverUrl = new URL(serverUrlString);
+				} catch (MalformedURLException e) {
+					throw new MorriganException(e);
+				}
+				
+				String itemUrlString =
+					serverUrl.getProtocol() + "://" + serverUrl.getHost() + ":" + serverUrl.getPort()
+					+ mlt.getRemoteLocation();
+				
+				URL itemUrl;
+				try {
+					itemUrl = new URL(itemUrlString);
+				} catch (MalformedURLException e) {
+					throw new MorriganException(e);
+				}
+				
+				System.err.println("Fetching '"+itemUrlString+"' to '"+targetFile.getAbsolutePath()+"'...");
+				
+				IHttpStreamHandler httpStreamHandler = new IHttpStreamHandler () {
+					@Override
+					public void handleStream(InputStream is) throws IOException, MorriganException {
+						BufferedInputStream bis = new BufferedInputStream(is);
+						BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(targetFile));
+						
+						// FIXME this is ubertastically slow.
+						try {
+							int i;
+							while ((i = bis.read()) != -1) {
+								bos.write(i);
+							}
+						} finally {
+							bos.close();
+						}
+
+					}
+				};
+				
+				try {
+					HttpClient.getHttpClient().doHttpRequest(itemUrl, httpStreamHandler);
+				} catch (IOException e) {
+					if (e instanceof UnknownHostException) {
+						throw new MorriganException("Host unknown.", e);
+					} else if (e instanceof SocketException) {
+						throw new MorriganException("Host unreachable.", e);
+					} else {
+						throw new MorriganException(e);
+					}
+				}
+			}
+			else {
+				System.err.println("Skipping '"+targetFile.getAbsolutePath()+"' as it already exists.");
+			}
+		}
+		else {
+			super.copyMediaItemFile(mi, targetDirectory);
+		}
 	}
 	
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
