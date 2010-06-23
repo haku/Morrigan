1b35007a3d5c4fb3be5c36229fea9fc1224f9855
haku <haku@vaguehope.com>
2010-06-23 22:19:40 +0000

    Widgets for tag editor.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@855 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan/src/net/sparktank/morrigan/gui/views/ViewTagEditor.java
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/gui/views/ViewTagEditor.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import net.sparktank.morrigan.gui.editors.AbstractLibraryEditor;
+import net.sparktank.morrigan.gui.helpers.ImageCache;
 import net.sparktank.morrigan.model.MediaTrack;
 
 import org.eclipse.jface.viewers.ISelection;
@@ -12,7 +13,12 @@ import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.ISelectionListener;
 import org.eclipse.ui.ISelectionService;
 import org.eclipse.ui.IWorkbenchPart;
@@ -22,6 +28,10 @@ public class ViewTagEditor extends ViewPart {
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	
 	public static final String ID = "net.sparktank.morrigan.gui.views.ViewTagEditor";
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	private ImageCache imageCache = new ImageCache();
 	
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	
@@ -39,26 +49,76 @@ public class ViewTagEditor extends ViewPart {
 	@Override
 	public void dispose() {
 		removeSelectionListener();
+		imageCache.clearCache();
 		super.dispose();
 	}
 	
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 //	GUI stuff.
 	
+	protected final int sep = 3;
+	
+	private Text txtFilter;
+	private Button btnAddTag;
+	private Button btnRemoveTag;
 	private TableViewer tableViewer;
 	
 	private void createLayout (Composite parent) {
+		FormData formData;
+		
+		parent.setLayout(new FormLayout());
+		
+		Composite tbCom = new Composite(parent, SWT.NONE);
+		txtFilter = new Text(tbCom, SWT.SINGLE | SWT.BORDER);
+		btnAddTag = new Button(tbCom, SWT.PUSH);
+		btnRemoveTag = new Button(tbCom, SWT.PUSH);
+		
+		tbCom.setLayout(new FormLayout());
+		formData = new FormData();
+		formData.top = new FormAttachment(0, 0);
+		formData.left = new FormAttachment(0, 0);
+		formData.right = new FormAttachment(100, 0);
+		tbCom.setLayoutData(formData);
+		
+		txtFilter.setMessage("New tag");
+		formData = new FormData();
+		formData.left = new FormAttachment(0, sep);
+		formData.right = new FormAttachment(btnAddTag, -sep);
+		formData.top = new FormAttachment(0, sep);
+		formData.bottom = new FormAttachment(100, -sep);
+		txtFilter.setLayoutData(formData);
+		
+		btnAddTag.setImage(imageCache.readImage("icons/plus.gif"));
+		formData = new FormData();
+		formData.right = new FormAttachment(btnRemoveTag, -sep);
+		formData.top = new FormAttachment(0, sep);
+		formData.bottom = new FormAttachment(100, -sep);
+		btnAddTag.setLayoutData(formData);
+		
+		btnRemoveTag.setImage(imageCache.readImage("icons/minus.gif"));
+		formData = new FormData();
+		formData.right = new FormAttachment(100, -sep);
+		formData.top = new FormAttachment(0, sep);
+		formData.bottom = new FormAttachment(100, -sep);
+		btnRemoveTag.setLayoutData(formData);
+		
 		tableViewer = new TableViewer(parent, SWT.MULTI | SWT.V_SCROLL);
 		tableViewer.setContentProvider(sourcesProvider);
 		tableViewer.setInput(getViewSite()); // use content provider.
 		getSite().setSelectionProvider(tableViewer);
+		formData = new FormData();
+		formData.left = new FormAttachment(0, 0);
+		formData.right = new FormAttachment(100, 0);
+		formData.top = new FormAttachment(tbCom, 0);
+		formData.bottom = new FormAttachment(100, -sep);
+		tableViewer.getTable().setLayoutData(formData);
 	}
 	
 	private IStructuredContentProvider sourcesProvider = new IStructuredContentProvider() {
 		
 		@Override
 		public Object[] getElements(Object inputElement) {
-			return new String[]{};
+			return new String[]{"tag0", "tag1", "tag2"};
 		}
 		
 		@Override
