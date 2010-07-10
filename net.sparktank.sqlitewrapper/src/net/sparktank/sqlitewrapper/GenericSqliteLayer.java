b5f2265e54cc19b763e948a748f5e4894ff50131
haku <haku@vaguehope.com>
2010-07-10 15:34:28 +0000

    initial import of generic Sqlite wrapper class.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@911 2128fabd-03f8-4a31-9ba6-05665b9bae8a

---
+++ net.sparktank.sqlitewrapper/src/net/sparktank/sqlitewrapper/GenericSqliteLayer.java
@@ -0,0 +1,154 @@
+package net.sparktank.sqlitewrapper;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public abstract class GenericSqliteLayer {
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//	Instance properties.
+	
+	private final String dbFilePath;
+
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//	Constructors.
+	
+	protected GenericSqliteLayer (String dbFilePath) throws DbException {
+		this.dbFilePath = dbFilePath;
+		
+		try {
+			initDatabaseTables();
+		} catch (Exception e) {
+			throw new DbException(e);
+		}
+	}
+	
+	@Override
+	protected void finalize() throws Throwable {
+		dispose();
+		super.finalize();
+	}
+	
+	public void dispose () throws DbException {
+		try {
+			disposeDbCon();
+		} catch (Exception e) {
+			throw new DbException(e);
+		}
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//	Properties.
+	
+	public String getDbFilePath() {
+		return dbFilePath;
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//	DB connection.
+	
+	private Connection dbConnection = null;
+	
+	protected Connection getDbCon () throws ClassNotFoundException, SQLException {
+		if (dbConnection==null) {
+			Class.forName("org.sqlite.JDBC");
+			String url = "jdbc:sqlite:/" + dbFilePath;
+			dbConnection = DriverManager.getConnection(url);
+		}
+		
+		return dbConnection;
+	}
+	
+	private void disposeDbCon () throws SQLException {
+		if (dbConnection!=null) {
+			dbConnection.close();
+		}
+	}
+	
+	public void setAutoCommit (boolean b) throws DbException {
+		try {
+			getDbCon().setAutoCommit(b);
+		} catch (Exception e) {
+			throw new DbException(e);
+		}
+	}
+	
+	public void commit () throws DbException {
+		try {
+			getDbCon().commit();
+		} catch (Exception e) {
+			throw new DbException(e);
+		}
+	}
+	
+	public void rollback () throws DbException {
+		try {
+			getDbCon().rollback();
+		} catch (Exception e) {
+			throw new DbException(e);
+		}
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//	Init.
+	
+	private void initDatabaseTables () throws SQLException, ClassNotFoundException {
+		Statement stat = getDbCon().createStatement();
+		
+		SqlCreateCmd[] tblCreateCmds = getTblCreateCmds();
+		
+		for (SqlCreateCmd sqlCreateCmd : tblCreateCmds) {
+			try {
+				ResultSet rs;
+				rs = stat.executeQuery(sqlCreateCmd.getTblExistsSql());
+				try {
+					if (!rs.next()) { // True if there are rows in the result.
+						stat.executeUpdate(sqlCreateCmd.getTblCreateSql());
+					}
+				}
+				finally {
+					rs.close();
+				}
+			}
+			finally {
+				stat.close();
+			}
+		}
+		
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	static public class SqlCreateCmd {
+		
+		private String tblExistsSql;
+		private String tblCreateSql;
+		
+		public SqlCreateCmd (String tblExistsSql, String tblCreateSql) {
+			this.tblExistsSql = tblExistsSql;
+			this.tblCreateSql = tblCreateSql;
+		}
+		
+		public String getTblExistsSql() {
+			return tblExistsSql;
+		}
+		public void setTblExistsSql(String tblExistsSql) {
+			this.tblExistsSql = tblExistsSql;
+		}
+		
+		public String getTblCreateSql() {
+			return tblCreateSql;
+		}
+		public void setTblCreateSql(String tblCreateSql) {
+			this.tblCreateSql = tblCreateSql;
+		}
+		
+	}
+	
+	protected abstract SqlCreateCmd[] getTblCreateCmds ();
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+}
