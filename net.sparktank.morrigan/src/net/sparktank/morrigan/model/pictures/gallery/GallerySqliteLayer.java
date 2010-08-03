acd71a4df6cd83994255b42b9a98564ece9c74c6
haku <haku@vaguehope.com>
2010-08-03 19:16:10 +0000

    Framework for GallerySqliteLayer.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@939 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan/src/net/sparktank/morrigan/model/pictures/gallery/GallerySqliteLayer.java
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/model/pictures/gallery/GallerySqliteLayer.java
@@ -2,23 +2,57 @@ package net.sparktank.morrigan.model.pictures.gallery;
 
 import java.util.List;
 
+import net.sparktank.morrigan.model.MediaSqliteLayer;
 import net.sparktank.sqlitewrapper.DbException;
-import net.sparktank.sqlitewrapper.GenericSqliteLayer;
 
-public class GallerySqliteLayer extends GenericSqliteLayer {
+public class GallerySqliteLayer extends MediaSqliteLayer {
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//	Constructors.
 	
 	protected GallerySqliteLayer (String dbFilePath) throws DbException {
 		super(dbFilePath);
 	}
 	
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//	Public methods for ...
+	
+	// TODO
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//	Schema.
+	
+	/* - - - - - - - - - - - - - - - -
+	 * tbl_...
+	 */
+	
+	// TODO
+	
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//	SQL statements.
+	
+	/* - - - - - - - - - - - - - - - -
+	 * tbl_...
+	 */
+	
+	// TODO
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//	Superclass methods.
 	
 	@Override
 	protected List<SqlCreateCmd> getTblCreateCmds() {
-		// TODO Auto-generated method stub
-		return null;
+		List<SqlCreateCmd> l = super.getTblCreateCmds();
+		
+		// TODO create tables here.
+		
+		return l;
 	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//	Private methods for...
+	
+	// TODO
 	
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 }
