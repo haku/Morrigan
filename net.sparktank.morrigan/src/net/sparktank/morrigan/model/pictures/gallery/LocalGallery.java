a24d391a937ad92f58b4116bae41b361c630f800
haku <haku@vaguehope.com>
2010-08-07 17:04:59 +0000

    Make LocalGallery extend MediaItemDb.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@954 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan/src/net/sparktank/morrigan/model/pictures/gallery/LocalGallery.java
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/model/pictures/gallery/LocalGallery.java
@@ -1,111 +1,43 @@
 package net.sparktank.morrigan.model.pictures.gallery;
 
-import java.util.List;
-
-import net.sparktank.morrigan.exceptions.MorriganException;
-import net.sparktank.morrigan.model.MediaItemList;
-import net.sparktank.morrigan.model.MediaSqliteLayer2;
-import net.sparktank.morrigan.model.MediaSqliteLayer2.DbColumn;
-import net.sparktank.morrigan.model.MediaSqliteLayer2.SortDirection;
+import net.sparktank.morrigan.model.MediaItemDb;
 import net.sparktank.morrigan.model.pictures.MediaPicture;
-import net.sparktank.sqlitewrapper.DbException;
+import net.sparktank.morrigan.model.pictures.MediaPictureList;
 
 /*
  * TODO FIXME Extract common code between this and AbstractMediaLibrary.
  */
-public class LocalGallery extends MediaItemList<MediaPicture> {
+public class LocalGallery extends MediaItemDb<MediaPictureList<MediaPicture>, GallerySqliteLayer, MediaPicture> /* TODO implements IMediaPictureList */ {
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	
 	public static final String TYPE = "LOCALGALLERY";
 	
-	public static final boolean HIDEMISSING = true; // TODO link this to GUI?
-	
-	private GallerySqliteLayer dbLayer;
-	private DbColumn librarySort;
-	private SortDirection librarySortDirection;
-	
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	
 	protected LocalGallery (String libraryName, GallerySqliteLayer dbLayer) {
-		super(dbLayer.getDbFilePath(), libraryName);
-		this.dbLayer = dbLayer;
-		
-		this.librarySort = MediaSqliteLayer2.SQL_TBL_MEDIAFILES_COL_FILE;
-		this.librarySortDirection = SortDirection.ASC;
-	}
-	
-	@Override
-	protected void finalize() throws Throwable {
-		this.dbLayer.dispose();
-		super.finalize();
+		super(libraryName, dbLayer);
 	}
 	
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	@Override
+	protected MediaPicture getNewT(String filePath) {
+		return new MediaPicture(filePath);
+	}
 	
 	@Override
 	public String getType() {
 		return TYPE;
 	}
 	
-	@Override
-	public String getSerial() {
-		return this.dbLayer.getDbFilePath();
-	}
-	
-	protected GallerySqliteLayer getDbLayer() {
-		return this.dbLayer;
-	}
-	
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	
-	@Override
-	public boolean isCanBeDirty () {
-		return false;
+	/*
+	TODO
+	public void setPictureWidthAndHeight (MediaPicture mp, int width, int height) throws MorriganException {
+		
 	}
-	
-	@Override
-	public boolean allowDuplicateEntries () {
-		return false;
-	}
-	
-//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	
-	private boolean firstRead = true;
-	private long durationOfLastRead = -1;
-	
-	@Override
-	public void read () throws MorriganException {
-		if (!this.firstRead) return;
-		try {
-			doRead();
-		} catch (DbException e) {
-			throw new MorriganException(e);
-		}
-	}
-	
-	/**
-	 * @throws MorriganException  
 	 */
-	protected void doRead () throws MorriganException, DbException {
-		System.err.println("[?] reading... " + getType() + " " + getListName() + "...");
-		
-		long t0 = System.currentTimeMillis();
-		List<MediaPicture> allMedia = this.dbLayer.updateListOfAllMedia(getMediaTracks(), this.librarySort, this.librarySortDirection, HIDEMISSING);
-		long l0 = System.currentTimeMillis() - t0;
-		
-		long t1 = System.currentTimeMillis();
-		setMediaTracks(allMedia);
-		long l1 = System.currentTimeMillis() - t1;
-		
-		System.err.println("[" + l0 + "," + l1 + " ms] " + getType() + " " + getListName());
-		this.durationOfLastRead = l0+l1;
-		
-		this.firstRead = false;
-	}
-	
-	public long getDurationOfLastRead() {
-		return this.durationOfLastRead;
-	}
 	
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 }
