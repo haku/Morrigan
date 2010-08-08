c445ffd6c850816fca3469bc0c41c311740c6afe
haku <haku@vaguehope.com>
2010-08-08 22:17:47 +0000

    Added menu item to show props.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@1001 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan/src/net/sparktank/morrigan/gui/editors/pictures/LocalGalleryEditor.java
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/gui/editors/pictures/LocalGalleryEditor.java
@@ -1,8 +1,15 @@
 package net.sparktank.morrigan.gui.editors.pictures;
 
+import net.sparktank.morrigan.gui.actions.LibraryUpdateAction;
+import net.sparktank.morrigan.gui.dialogs.MorriganMsgDlg;
+import net.sparktank.morrigan.gui.views.ViewLibraryProperties;
 import net.sparktank.morrigan.model.pictures.gallery.LocalGallery;
 
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.action.Separator;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.IViewPart;
 
 public class LocalGalleryEditor extends AbstractGalleryEditor<LocalGallery> {
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -15,6 +22,10 @@ public class LocalGalleryEditor extends AbstractGalleryEditor<LocalGallery> {
 	@Override
 	protected void createControls(Composite parent) {
 		super.createControls(parent);
+		
+		getPrefMenuMgr().add(new Separator());
+		getPrefMenuMgr().add(new LibraryUpdateAction(getMediaList()));
+		getPrefMenuMgr().add(this.showPropertiesAction);
 	}
 	
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -23,6 +34,32 @@ public class LocalGalleryEditor extends AbstractGalleryEditor<LocalGallery> {
 	@Override
 	protected boolean handleReadError(Exception e) {
 		return false;
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//	Actions.
+	
+	protected IAction showPropertiesAction = new Action("Properties") {
+		@Override
+		public void run () {
+			showLibPropView();
+		}
+	};
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//	Action helpers.
+	
+	ViewLibraryProperties showLibPropView () {
+		try {
+			IViewPart showView = getSite().getPage().showView(ViewLibraryProperties.ID);
+			ViewLibraryProperties viewProp = (ViewLibraryProperties) showView;
+			viewProp.setContent(getMediaList());
+			return viewProp;
+		}
+		catch (Exception e) {
+			new MorriganMsgDlg(e).open();
+			return null;
+		}
 	}
 	
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
