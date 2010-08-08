886b09e1da712c9f65ed383c96f46276e447a5b1
haku <haku@vaguehope.com>
2010-08-08 13:07:19 +0000

    Migrate MediaPictureList to IMediaPictureList.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@958 2128fabd-03f8-4a31-9ba6-05665b9bae8a

---
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/model/pictures/IMediaPictureList.java
@@ -0,0 +1,12 @@
+package net.sparktank.morrigan.model.pictures;
+
+import net.sparktank.morrigan.exceptions.MorriganException;
+import net.sparktank.morrigan.model.IMediaItemList;
+
+public interface IMediaPictureList<T extends MediaPicture> extends IMediaItemList<T> {
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	public void setPictureWidthAndHeight (MediaPicture mp, int width, int height) throws MorriganException;
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+}
