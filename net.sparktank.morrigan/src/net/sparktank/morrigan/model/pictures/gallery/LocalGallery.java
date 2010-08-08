10cfd4d63acaecd13df3643b7afd3b9ba118b851
haku <haku@vaguehope.com>
2010-08-08 19:08:56 +0000

    Create AbstractGallery in same pattern as with track classes.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@988 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan/src/net/sparktank/morrigan/model/pictures/gallery/LocalGallery.java
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/model/pictures/gallery/LocalGallery.java
@@ -1,11 +1,7 @@
 package net.sparktank.morrigan.model.pictures.gallery;
 
-import net.sparktank.morrigan.model.MediaItemDb;
-import net.sparktank.morrigan.model.pictures.IMediaPictureList;
-import net.sparktank.morrigan.model.pictures.MediaPicture;
-import net.sparktank.morrigan.model.pictures.MediaPictureListHelper;
 
-public class LocalGallery extends MediaItemDb<IMediaPictureList<MediaPicture>, GallerySqliteLayer, MediaPicture> implements IMediaPictureList<MediaPicture> {
+public class LocalGallery extends AbstractGallery {
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	
 	public static final String TYPE = "LOCALGALLERY";
@@ -19,20 +15,8 @@ public class LocalGallery extends MediaItemDb<IMediaPictureList<MediaPicture>, G
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	
 	@Override
-	protected MediaPicture getNewT(String filePath) {
-		return new MediaPicture(filePath);
-	}
-	
-	@Override
 	public String getType() {
 		return TYPE;
-	}
-	
-//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	
-	@Override
-	public void setPictureWidthAndHeight (MediaPicture mp, int width, int height) {
-		MediaPictureListHelper.setPictureWidthAndHeight(this, mp, width, height);
 	}
 	
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
