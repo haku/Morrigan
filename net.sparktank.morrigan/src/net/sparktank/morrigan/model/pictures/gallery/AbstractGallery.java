283bbaf33c8c11e9835b150c9dff48f50303ec14
haku <haku@vaguehope.com>
2010-08-08 19:08:56 +0000

    Create AbstractGallery in same pattern as with track classes.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@988 2128fabd-03f8-4a31-9ba6-05665b9bae8a

---
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/model/pictures/gallery/AbstractGallery.java
@@ -0,0 +1,30 @@
+package net.sparktank.morrigan.model.pictures.gallery;
+
+import net.sparktank.morrigan.model.MediaItemDb;
+import net.sparktank.morrigan.model.pictures.IMediaPictureList;
+import net.sparktank.morrigan.model.pictures.MediaPicture;
+import net.sparktank.morrigan.model.pictures.MediaPictureListHelper;
+
+public abstract class AbstractGallery extends MediaItemDb<IMediaPictureList<MediaPicture>, GallerySqliteLayer, MediaPicture> implements IMediaPictureList<MediaPicture> {
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -	
+
+	protected AbstractGallery (String libraryName, GallerySqliteLayer dbLayer) {
+		super(libraryName, dbLayer);
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	@Override
+	protected MediaPicture getNewT(String filePath) {
+		return new MediaPicture(filePath);
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	@Override
+	public void setPictureWidthAndHeight (MediaPicture mp, int width, int height) {
+		MediaPictureListHelper.setPictureWidthAndHeight(this, mp, width, height);
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+}
