d3b8a719773d3a719ef7e9a01dff368c2fe77def
haku <haku@vaguehope.com>
2010-08-08 13:06:57 +0000

    Remove unneeded exception throwing.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@957 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan/src/net/sparktank/morrigan/model/tracks/MediaTrackListHelper.java
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/model/tracks/MediaTrackListHelper.java
@@ -3,59 +3,40 @@ package net.sparktank.morrigan.model.tracks;
 import java.util.Date;
 import java.util.List;
 
-import net.sparktank.morrigan.exceptions.MorriganException;
-import net.sparktank.morrigan.model.MediaItemList;
 import net.sparktank.morrigan.model.IMediaItemList.DirtyState;
+import net.sparktank.morrigan.model.MediaItemList;
 import net.sparktank.morrigan.model.tracks.IMediaTrackList.DurationData;
 
 public class MediaTrackListHelper {
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	
-	/**
-	 * @throws MorriganException  
-	 */
-	static public void incTrackStartCnt (MediaItemList<?> mtl, MediaTrack track, long n) throws MorriganException {
+	static public void incTrackStartCnt (MediaItemList<?> mtl, MediaTrack track, long n) {
 		track.setStartCount(track.getStartCount() + n);
 		mtl.setDirtyState(DirtyState.METADATA);
 	}
 	
-	/**
-	 * @throws MorriganException  
-	 */
-	static public void incTrackEndCnt (MediaItemList<?> mtl, MediaTrack track, long n) throws MorriganException {
+	static public void incTrackEndCnt (MediaItemList<?> mtl, MediaTrack track, long n) {
 		track.setEndCount(track.getEndCount() + n);
 		mtl.setDirtyState(DirtyState.METADATA);
 	}
 	
-	/**
-	 * @throws MorriganException  
-	 */
-	static public void incTrackStartCnt (MediaItemList<?> mtl, MediaTrack track) throws MorriganException {
+	static public void incTrackStartCnt (MediaItemList<?> mtl, MediaTrack track) {
 		track.setStartCount(track.getStartCount()+1);
 		track.setDateLastPlayed(new Date());
 		mtl.setDirtyState(DirtyState.METADATA);
 	}
 	
-	/**
-	 * @throws MorriganException  
-	 */
-	static public void incTrackEndCnt (MediaItemList<?> mtl, MediaTrack track) throws MorriganException {
+	static public void incTrackEndCnt (MediaItemList<?> mtl, MediaTrack track) {
 		track.setEndCount(track.getEndCount()+1);
 		mtl.setDirtyState(DirtyState.METADATA);
 	}
 	
-	/**
-	 * @throws MorriganException  
-	 */
-	static public void setTrackDuration (MediaItemList<?> mtl, MediaTrack track, int duration) throws MorriganException {
+	static public void setTrackDuration (MediaItemList<?> mtl, MediaTrack track, int duration) {
 		track.setDuration(duration);
 		mtl.setDirtyState(DirtyState.METADATA);
 	}
 	
-	/**
-	 * @throws MorriganException  
-	 */
-	static public void setDateLastPlayed (MediaItemList<?> mtl, MediaTrack track, Date date) throws MorriganException {
+	static public void setDateLastPlayed (MediaItemList<?> mtl, MediaTrack track, Date date) {
 		track.setDateLastPlayed(date);
 		mtl.setDirtyState(DirtyState.METADATA);
 	}
