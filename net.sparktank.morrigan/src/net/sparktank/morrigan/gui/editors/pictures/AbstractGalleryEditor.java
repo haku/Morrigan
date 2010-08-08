b573da945c736383b7eb57b9528028f43ad3d12a
haku <haku@vaguehope.com>
2010-08-08 19:29:03 +0000

    Initial stab at creating Gallery editor classes.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@990 2128fabd-03f8-4a31-9ba6-05665b9bae8a

---
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/gui/editors/pictures/AbstractGalleryEditor.java
@@ -0,0 +1,127 @@
+package net.sparktank.morrigan.gui.editors.pictures;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import net.sparktank.morrigan.gui.dialogs.MorriganMsgDlg;
+import net.sparktank.morrigan.gui.display.DropMenuListener;
+import net.sparktank.morrigan.helpers.TimeHelper;
+import net.sparktank.morrigan.model.DbColumn;
+import net.sparktank.morrigan.model.MediaSqliteLayer2.SortDirection;
+import net.sparktank.morrigan.model.pictures.MediaPicture;
+import net.sparktank.morrigan.model.pictures.gallery.AbstractGallery;
+import net.sparktank.morrigan.model.tracks.IMediaTrackList.DurationData;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.action.IContributionItem;
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Text;
+
+public abstract class AbstractGalleryEditor<T extends AbstractGallery> extends MediaPictureListEditor<AbstractGallery, MediaPicture> {
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	public AbstractGalleryEditor () {
+		super();
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	@Override
+	protected MediaPicture getNewS(String filePath) {
+		return new MediaPicture(filePath);
+	}
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//	EditorPart methods.
+	
+	@Override
+	public boolean isSaveAsAllowed() {
+		return false;
+	}
+	
+	@Override
+	public void doSaveAs() {
+		new MorriganMsgDlg("TODO: do save as for '" + getTitle() + "'.\n\n(this should not happen.)").open();
+	}
+	
+	@Override
+	public void doSave(IProgressMonitor monitor) {/* NOT USED */}
+	
+	/**
+	 * There is no need for the library to
+	 * ever require the user manually save.
+	 */
+	@Override
+	public boolean isDirty() {
+		return false;
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	@Override
+	public T getMediaList () {
+		return (T) this.getEditorInput().getMediaList();
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//	GUI components.
+	
+	@Override
+	protected void createControls(Composite parent) {
+		// TODO
+	}
+	
+	@Override
+	protected List<Control> populateToolbar (Composite parent) {
+		// TODO
+		return null;
+	}
+	
+	@Override
+	protected void populateContextMenu(List<IContributionItem> menu0, List<IContributionItem> menu1) {
+		// TODO
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//	Events.
+	
+	@Override
+	protected void listChanged () {
+		if (this.lblStatus.isDisposed()) return;
+		
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append(getMediaList().getCount());
+		sb.append(" items.");
+		
+		long queryTime = getMediaList().getDurationOfLastRead();
+		if (queryTime > 0) {
+			sb.append("  Query took ");
+			sb.append(TimeHelper.formatTimeMiliseconds(queryTime));
+			sb.append(" seconds.");
+		}
+		
+		this.lblStatus.setText(sb.toString());
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//	Sorting.
+	
+	@Override
+	protected boolean isSortable () {
+		return true;
+	}
+	
+	@Override
+	protected void onSort (TableViewer table, TableViewerColumn column, int direction) {
+		// TODO setSort()...
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+}
