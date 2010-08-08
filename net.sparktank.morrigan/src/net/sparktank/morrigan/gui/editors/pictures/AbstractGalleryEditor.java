c12c5825e16f72da9a7f2e9914e06f2c7869af5a
haku <haku@vaguehope.com>
2010-08-08 21:56:57 +0000

    Implement enough GUI to avoid NPE.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@996 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan/src/net/sparktank/morrigan/gui/editors/pictures/AbstractGalleryEditor.java
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/gui/editors/pictures/AbstractGalleryEditor.java
@@ -1,16 +1,22 @@
 package net.sparktank.morrigan.gui.editors.pictures;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import net.sparktank.morrigan.gui.dialogs.MorriganMsgDlg;
+import net.sparktank.morrigan.gui.display.DropMenuListener;
 import net.sparktank.morrigan.helpers.TimeHelper;
 import net.sparktank.morrigan.model.pictures.MediaPicture;
 import net.sparktank.morrigan.model.pictures.gallery.AbstractGallery;
 
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.action.ActionContributionItem;
 import org.eclipse.jface.action.IContributionItem;
+import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 
@@ -63,20 +69,36 @@ public abstract class AbstractGalleryEditor<T extends AbstractGallery> extends M
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 //	GUI components.
 	
+	private Button btnProperties = null;
+	private MenuManager prefMenuMgr = null;
+	
 	@Override
 	protected void createControls(Composite parent) {
 		// TODO
+		
+		// Pref menu.
+		this.prefMenuMgr = new MenuManager();
 	}
 	
 	@Override
 	protected List<Control> populateToolbar (Composite parent) {
-		// TODO
-		return null;
+		List<Control> ret = new LinkedList<Control>();
+		
+		this.btnProperties = new Button(parent, SWT.PUSH);
+		this.btnProperties.setImage(getImageCache().readImage("icons/pref.gif"));
+		this.btnProperties.addSelectionListener(new DropMenuListener(this.btnProperties, this.prefMenuMgr));
+		ret.add(this.btnProperties);
+		
+		return ret;
 	}
 	
 	@Override
 	protected void populateContextMenu(List<IContributionItem> menu0, List<IContributionItem> menu1) {
-		// TODO
+		menu0.add(new ActionContributionItem(this.copyToAction));
+	}
+	
+	protected MenuManager getPrefMenuMgr () {
+		return this.prefMenuMgr;
 	}
 	
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
