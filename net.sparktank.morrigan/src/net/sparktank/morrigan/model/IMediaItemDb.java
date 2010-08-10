9c5cde423aee325e3679248f70a84fd4d46058dd
haku <haku@vaguehope.com>
2010-08-10 19:38:43 +0000

    create IMediaItemDb and use it in MediaItemDb.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@1009 2128fabd-03f8-4a31-9ba6-05665b9bae8a

---
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/model/IMediaItemDb.java
@@ -0,0 +1,46 @@
+package net.sparktank.morrigan.model;
+
+import java.util.List;
+
+import net.sparktank.morrigan.exceptions.MorriganException;
+import net.sparktank.morrigan.model.MediaSqliteLayer2.SortDirection;
+import net.sparktank.morrigan.model.tags.MediaTag;
+import net.sparktank.morrigan.model.tags.MediaTagClassification;
+import net.sparktank.morrigan.model.tags.MediaTagType;
+import net.sparktank.sqlitewrapper.DbException;
+
+public interface IMediaItemDb<S extends MediaSqliteLayer2<T>, T extends MediaItem> extends IMediaItemList<T> {
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	// TODO add more methods?
+	
+	public interface SortChangeListener {
+		public void sortChanged (DbColumn sort, SortDirection direction);
+	}
+	
+	public S getDbLayer();
+	
+	public DbColumn getSort ();
+	public SortDirection getSortDirection ();
+	public void setSort (DbColumn sort, SortDirection direction) throws MorriganException;
+	public void registerSortChangeListener (SortChangeListener scl);
+	public void unregisterSortChangeListener (SortChangeListener scl);
+	
+	public List<T> simpleSearch (String term, String esc, int maxResults) throws DbException;
+	
+	public String getDbPath ();
+	public List<String> getSources () throws DbException;
+	public void addSource (String source) throws DbException;
+	public void removeSource (String source) throws DbException;
+	
+	public boolean hasTags (IDbItem item) throws MorriganException;
+	public boolean hasTag (IDbItem item, String tag, MediaTagType type, MediaTagClassification mtc) throws MorriganException;
+	public List<MediaTag> getTags (IDbItem item) throws MorriganException;
+	public void addTag (IDbItem item, String tag, MediaTagType type, MediaTagClassification mtc) throws MorriganException;
+	public void addTag (IDbItem item, String tag, MediaTagType type, String mtc) throws MorriganException;
+	public void moveTags (IDbItem from_item, IDbItem to_item) throws MorriganException;
+	public void removeTag (MediaTag mt) throws MorriganException;
+	public void clearTags (IDbItem item) throws MorriganException;
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+}
