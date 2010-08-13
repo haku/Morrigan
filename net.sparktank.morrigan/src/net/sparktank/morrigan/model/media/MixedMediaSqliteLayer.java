1b70c082dbf68eae8ac9c447db5b1756efccd5ed
haku <haku@vaguehope.com>
2010-08-13 18:25:37 +0000

    Implement added, remote, setter function.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@1022 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan/src/net/sparktank/morrigan/model/media/MixedMediaSqliteLayer.java
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/model/media/MixedMediaSqliteLayer.java
@@ -88,111 +88,187 @@ public class MixedMediaSqliteLayer extends MixedMediaSqliteLayerImpl implements 
 	}
 	
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//	Media adders and removers.
 	
 	@Override
-	public boolean addFile(MediaType mediaType, File file) throws DbException {
-		throw new RuntimeException("Not implemented yet.");
+	public boolean addFile (MediaType mediaType, File file) throws DbException {
+		try {
+			return local_addTrack(mediaType, file.getAbsolutePath(), file.lastModified());
+		} catch (Exception e) {
+			throw new DbException(e);
+		}
 	}
 	
 	@Override
-	public boolean addFile(MediaType mediaType, String filepath, long lastModified) throws DbException {
-		throw new RuntimeException("Not implemented yet.");
+	public boolean addFile (MediaType mediaType, String filepath, long lastModified) throws DbException {
+		try {
+			return local_addTrack(mediaType, filepath, lastModified);
+		} catch (Exception e) {
+			throw new DbException(e);
+		}
 	}
 	
 	@Override
-	public int removeFile(String sfile) throws DbException {
-		throw new RuntimeException("Not implemented yet.");
+	public int removeFile (String sfile) throws DbException {
+		try {
+			return local_removeTrack(sfile);
+		} catch (Exception e) {
+			throw new DbException(e);
+		}
 	}
 	
 	@Override
-	public int removeFile(IDbItem dbItem) throws DbException {
-		throw new RuntimeException("Not implemented yet.");
-	}
-	
-	@Override
-	public void setDateAdded(String sfile, Date date) throws DbException {
-		throw new RuntimeException("Not implemented yet.");
-	}
-	
-	@Override
-	public void setHashcode(String sfile, long hashcode) throws DbException {
-		throw new RuntimeException("Not implemented yet.");
-	}
-	
-	@Override
-	public void setDateLastModified(String sfile, Date date) throws DbException {
-		throw new RuntimeException("Not implemented yet.");
-	}
-	
-	@Override
-	public void setEnabled(String sfile, boolean value) throws DbException {
-		throw new RuntimeException("Not implemented yet.");
-		
-	}
-	
-	@Override
-	public void setMissing(String sfile, boolean value) throws DbException {
-		throw new RuntimeException("Not implemented yet.");
-		
-	}
-	
-	@Override
-	public void setRemoteLocation(String sfile, String remoteLocation) throws DbException {
-		throw new RuntimeException("Not implemented yet.");
+	public int removeFile (IDbItem dbItem) throws DbException {
+		try {
+			return local_removeTrack(dbItem);
+		} catch (Exception e) {
+			throw new DbException(e);
+		}
 	}
 	
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//	MediaItem setters.
 	
 	@Override
-	public void incTrackPlayed(String sfile) throws DbException {
-		throw new RuntimeException("Not implemented yet.");
-		
+	public void setDateAdded (String sfile, Date date) throws DbException {
+		try {
+			local_setDateAdded(sfile, date);
+		} catch (Exception e) {
+			throw new DbException(e);
+		}
 	}
 	
 	@Override
-	public void incTrackFinished(String sfile) throws DbException {
-		throw new RuntimeException("Not implemented yet.");
+	public void setHashcode (String sfile, long hashcode) throws DbException {
+		try {
+			local_setHashCode(sfile, hashcode);
+		} catch (Exception e) {
+			throw new DbException(e);
+		}
 	}
 	
 	@Override
-	public void incTrackStartCnt(String sfile, long n) throws DbException {
-		throw new RuntimeException("Not implemented yet.");
+	public void setDateLastModified (String sfile, Date date) throws DbException {
+		try {
+			local_setDateLastModified(sfile, date);
+		} catch (Exception e) {
+			throw new DbException(e);
+		}
 	}
 	
 	@Override
-	public void setTrackStartCnt(String sfile, long n) throws DbException {
-		throw new RuntimeException("Not implemented yet.");
-		
+	public void setEnabled (String sfile, boolean value) throws DbException {
+		try {
+			local_setEnabled(sfile, value);
+		} catch (Exception e) {
+			throw new DbException(e);
+		}
 	}
 	
 	@Override
-	public void incTrackEndCnt(String sfile, long n) throws DbException {
-		throw new RuntimeException("Not implemented yet.");
-		
+	public void setMissing (String sfile, boolean value) throws DbException {
+		try {
+			local_setMissing(sfile, value);
+		} catch (Exception e) {
+			throw new DbException(e);
+		}
 	}
 	
 	@Override
-	public void setTrackEndCnt(String sfile, long n) throws DbException {
-		throw new RuntimeException("Not implemented yet.");
-		
-	}
-	
-	@Override
-	public void setDateLastPlayed(String sfile, Date date) throws DbException {
-		throw new RuntimeException("Not implemented yet.");
-		
-	}
-	
-	@Override
-	public void setTrackDuration(String sfile, int duration) throws DbException {
-		throw new RuntimeException("Not implemented yet.");
+	public void setRemoteLocation (String sfile, String remoteLocation) throws DbException {
+		try {
+			local_setRemoteLocation(sfile, remoteLocation);
+		} catch (Exception e) {
+			throw new DbException(e);
+		}
 	}
 	
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//	MediaTrack setters.
 	
 	@Override
-	public void setDimensions(String sfile, int width, int height) throws DbException {
-		throw new RuntimeException("Not implemented yet.");
+	public void incTrackPlayed (String sfile) throws DbException {
+		try {
+			local_incTrackStartCnt(sfile, 1);
+			local_setDateLastPlayed(sfile, new Date());
+		} catch (Exception e) {
+			throw new DbException(e);
+		}
+	}
+	
+	@Override
+	public void incTrackFinished (String sfile) throws DbException {
+		try {
+			local_incTrackEndCnt(sfile, 1);
+		} catch (Exception e) {
+			throw new DbException(e);
+		}
+	}
+	
+	@Override
+	public void incTrackStartCnt (String sfile, long n) throws DbException {
+		try {
+			local_incTrackStartCnt(sfile, n);
+		} catch (Exception e) {
+			throw new DbException(e);
+		}
+	}
+	
+	@Override
+	public void setTrackStartCnt (String sfile, long n) throws DbException {
+		try {
+			local_setTrackStartCnt(sfile, n);
+		} catch (Exception e) {
+			throw new DbException(e);
+		}
+	}
+	
+	@Override
+	public void incTrackEndCnt (String sfile, long n) throws DbException {
+		try {
+			local_incTrackEndCnt(sfile, n);
+		} catch (Exception e) {
+			throw new DbException(e);
+		}
+	}
+	
+	@Override
+	public void setTrackEndCnt (String sfile, long n) throws DbException {
+		try {
+			local_setTrackEndCnt(sfile, n);
+		} catch (Exception e) {
+			throw new DbException(e);
+		}
+	}
+	
+	@Override
+	public void setDateLastPlayed (String sfile, Date date) throws DbException {
+		try {
+			local_setDateLastPlayed(sfile, date);
+		} catch (Exception e) {
+			throw new DbException(e);
+		}
+	}
+	
+	@Override
+	public void setTrackDuration (String sfile, int duration) throws DbException {
+		try {
+			local_setTrackDuration(sfile, duration);
+		} catch (Exception e) {
+			throw new DbException(e);
+		}
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//	MediaPic setters.
+	
+	@Override
+	public void setDimensions (String sfile, int width, int height) throws DbException {
+		try {
+			local_setDimensions(sfile, width, height);
+		} catch (Exception e) {
+			throw new DbException(e);
+		}
 	}
 	
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
