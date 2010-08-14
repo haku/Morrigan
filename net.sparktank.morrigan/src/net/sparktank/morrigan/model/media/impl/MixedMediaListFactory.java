6ea3ca9e050e09be52d1013eba3eb3e069919cdc
haku <haku@vaguehope.com>
2010-08-14 01:12:17 +0000

    Create LocalMixedMediaDb and related factory method.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@1056 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan/src/net/sparktank/morrigan/model/media/impl/MixedMediaListFactory.java
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/model/media/impl/MixedMediaListFactory.java
@@ -1,10 +1,37 @@
 package net.sparktank.morrigan.model.media.impl;
 
+import net.sparktank.morrigan.helpers.RecyclingFactory;
+import net.sparktank.morrigan.model.tracks.library.local.LocalLibraryHelper;
+import net.sparktank.sqlitewrapper.DbException;
+
 
 public class MixedMediaListFactory {
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	
+	public static class LocalMixedMediaDbFactory extends RecyclingFactory<LocalMixedMediaDb, String, Void, DbException> {
+		
+		protected LocalMixedMediaDbFactory() {
+			super(true);
+		}
+		
+		@Override
+		protected boolean isValidProduct(LocalMixedMediaDb product) {
+			System.out.println("Found '" + product.getDbPath() + "' in cache.");
+			return true;
+		}
+		
+		@Override
+		protected LocalMixedMediaDb makeNewProduct(String material) throws DbException {
+			System.out.println("Making object instance '" + material + "'...");
+			/*
+			 * TODO replace LocalLibraryHelper with MixedMediaDbHelper.
+			 */
+			return new LocalMixedMediaDb(LocalLibraryHelper.getLibraryTitle(material), MixedMediaSqliteLayer.FACTORY.manufacture(material));
+		}
+		
+	}
 	
+	public static final LocalMixedMediaDbFactory LOCAL_MMDB_FACTORY = new LocalMixedMediaDbFactory();
 	
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 }
