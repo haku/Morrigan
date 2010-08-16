9c036ccfb9c6e4d4ee8fe3758bc2c42fbd160d44
haku <haku@vaguehope.com>
2010-08-16 20:48:07 +0000

    Add column to show dimensions, complete with preferences entry.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@1066 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan/src/net/sparktank/morrigan/gui/preferences/MediaListPref.java
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/gui/preferences/MediaListPref.java
@@ -2,6 +2,7 @@ package net.sparktank.morrigan.gui.preferences;
 
 import net.sparktank.morrigan.gui.Activator;
 import net.sparktank.morrigan.gui.editors.MediaColumn;
+import net.sparktank.morrigan.gui.editors.mmdb.MixedMediaListEditor;
 import net.sparktank.morrigan.gui.editors.tracks.MediaTrackListEditor;
 
 import org.eclipse.jface.preference.BooleanFieldEditor;
@@ -20,6 +21,7 @@ public class MediaListPref extends FieldEditorPreferencePage implements IWorkben
 	public static final String PREF_COL_HASHCODE  = "PREF_COL_HASHCODE";
 	public static final String PREF_COL_DMODIFIED = "PREF_COL_DMODIFIED";
 	public static final String PREF_COL_DURATION  = "PREF_COL_DURATION";
+	public static final String PREF_COL_DIMENSIONS  = "PREF_COL_DIMENSIONS";
 	
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	
@@ -32,6 +34,7 @@ public class MediaListPref extends FieldEditorPreferencePage implements IWorkben
 		addField(new BooleanFieldEditor(PREF_COL_HASHCODE, "Show hashcode column", getFieldEditorParent()));
 		addField(new BooleanFieldEditor(PREF_COL_DMODIFIED, "Show date last modified column", getFieldEditorParent()));
 		addField(new BooleanFieldEditor(PREF_COL_DURATION, "Show duration column", getFieldEditorParent()));
+		addField(new BooleanFieldEditor(PREF_COL_DIMENSIONS, "Show dimensions column", getFieldEditorParent()));
 	}
 	
 	@Override
@@ -64,6 +67,33 @@ public class MediaListPref extends FieldEditorPreferencePage implements IWorkben
 		}
 		else if (column == editor.COL_DURATION) {
 			return Activator.getDefault().getPreferenceStore().getBoolean(PREF_COL_DURATION);
+		}
+		else {
+			return true;
+		}
+	}
+	
+	public static boolean getColPref (MixedMediaListEditor<?,?> editor, MediaColumn column) {
+		if (column == editor.COL_ADDED) {
+			return Activator.getDefault().getPreferenceStore().getBoolean(PREF_COL_DADDED);
+		}
+		else if (column == editor.COL_COUNTS) {
+			return Activator.getDefault().getPreferenceStore().getBoolean(PREF_COL_COUNTS);
+		}
+		else if (column == editor.COL_LASTPLAYED) {
+			return Activator.getDefault().getPreferenceStore().getBoolean(PREF_COL_DLASTPLAY);
+		}
+		else if (column == editor.COL_HASH) {
+			return Activator.getDefault().getPreferenceStore().getBoolean(PREF_COL_HASHCODE);
+		}
+		else if (column == editor.COL_MODIFIED) {
+			return Activator.getDefault().getPreferenceStore().getBoolean(PREF_COL_DMODIFIED);
+		}
+		else if (column == editor.COL_DURATION) {
+			return Activator.getDefault().getPreferenceStore().getBoolean(PREF_COL_DURATION);
+		}
+		else if (column == editor.COL_DIMENSIONS) {
+			return Activator.getDefault().getPreferenceStore().getBoolean(PREF_COL_DIMENSIONS);
 		}
 		else {
 			return true;
