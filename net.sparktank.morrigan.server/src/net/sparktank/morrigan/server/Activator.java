dd75c51365fb8af1a06d6889ac27d5946b5d78ed
haku <haku@vaguehope.com>
2010-08-24 22:05:52 +0000

    Initial framework for net.sparktank.morrigan.server project that will control the life-cycle of the server when in headless mode.  (and also in the GUI at some point).
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@1118 2128fabd-03f8-4a31-9ba6-05665b9bae8a

---
+++ net.sparktank.morrigan.server/src/net/sparktank/morrigan/server/Activator.java
@@ -0,0 +1,107 @@
+package net.sparktank.morrigan.server;
+
+import net.sparktank.morrigan.engines.playback.IPlaybackEngine.PlayState;
+import net.sparktank.morrigan.helpers.ErrorHelper;
+import net.sparktank.morrigan.model.media.interfaces.IMediaTrack;
+import net.sparktank.morrigan.model.media.interfaces.IMediaTrackList;
+import net.sparktank.morrigan.player.IPlayerEventHandler;
+import net.sparktank.morrigan.player.PlayItem;
+import net.sparktank.morrigan.player.Player;
+import net.sparktank.morrigan.player.PlayerRegister;
+import net.sparktank.morrigan.player.OrderHelper.PlaybackOrder;
+
+import org.eclipse.swt.widgets.Composite;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	@Override
+	public void start (BundleContext context) throws Exception {
+		System.err.println("Starting Morrigan server...");
+		
+		// Prep player.
+		setupPlayer();
+		
+		// Start server.
+		MorriganServer s = makeServer();
+		s.start();
+	}
+	
+	@Override
+	public void stop (BundleContext context) throws Exception {
+		System.err.println("Stopping Morrigan server...");
+		
+		// Clean up.
+		cleanupPlayer();
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	static public MorriganServer makeServer () throws Exception {
+		return new MorriganServer();
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	private static Player _player;
+	
+	static private void setupPlayer () {
+		_player = PlayerRegister.makePlayer(eventHandler);
+		_player.setPlaybackOrder(PlaybackOrder.RANDOM);
+	}
+	
+	static private void cleanupPlayer () {
+		_player.dispose();
+	}
+	
+	static private IPlayerEventHandler eventHandler = new IPlayerEventHandler() {
+		
+		@Override
+		public void updateStatus() {
+			outputStatus();
+		}
+		
+		@Override
+		public void asyncThrowable(Throwable t) {
+			System.err.println("Throwable=" + ErrorHelper.getStackTrace(t));
+		}
+		
+		@Override
+		public Composite getCurrentMediaFrameParent() {
+			return null;
+		}
+		@Override
+		public IMediaTrackList<IMediaTrack> getCurrentList() {
+			return null;
+		}
+		@Override
+		public void currentItemChanged() {/* UNUSED */}
+		@Override
+		public void historyChanged() {/* UNUSED */}
+		@Override
+		public void videoAreaSelected() {/* UNUSED */}
+		@Override
+		public void videoAreaClose() {/* UNUSED */}
+	};
+	
+	
+	static private PlayState prevPlayState = null;
+	
+	static void outputStatus () {
+		PlayState playState = _player.getPlayState();
+		if (playState != prevPlayState) {
+			prevPlayState = playState;
+			
+			PlayItem currentItem = _player.getCurrentItem();
+			if (currentItem.item != null) {
+				System.out.println(prevPlayState.toString() + " " + currentItem.item);
+			} else {
+				System.out.println(prevPlayState.toString());
+			}
+		}
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+}
