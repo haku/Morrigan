08a33e686d0cc01228ea6575ed0fb6b7bcd74638
haku <haku@vaguehope.com>
2010-09-04 11:08:14 +0000

    Initial impl of RemoteMixedMediaDb and friends.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@1157 2128fabd-03f8-4a31-9ba6-05665b9bae8a

---
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/model/media/impl/RemoteMixedMediaDbHelper.java
@@ -0,0 +1,81 @@
+package net.sparktank.morrigan.model.media.impl;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import net.sparktank.morrigan.config.Config;
+import net.sparktank.morrigan.exceptions.MorriganException;
+import net.sparktank.morrigan.model.explorer.MediaExplorerItem;
+
+public class RemoteMixedMediaDbHelper {
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	public static String getFullPathToMmdb (String fileName) {
+		File dir = Config.getMmdbDir();
+		String file = dir.getPath() + File.separator + fileName;
+		
+		if (!file.toLowerCase().endsWith(Config.MMDB_REMOTE_FILE_EXT)) {
+			file = file.concat(Config.MMDB_REMOTE_FILE_EXT);
+		}
+		
+		return file;
+	}
+	
+	public static RemoteMixedMediaDb createRemoteMmdb (String mmdbUrl) throws MorriganException, MalformedURLException {
+		URL url = new URL(mmdbUrl);
+		// FIXME better naming?
+		String name = mmdbUrl.substring(mmdbUrl.lastIndexOf("/")+1).replace(Config.MMDB_REMOTE_FILE_EXT, "").replace(Config.MMDB_LOCAL_FILE_EXT, "");
+		String file = getFullPathToMmdb(url.getHost() + "_" + url.getPort() + "_" + name);
+		RemoteMixedMediaDb db = RemoteMixedMediaDb.FACTORY.manufacture(file, url);
+		return db;
+	}
+	
+	public static boolean isRemoteMmdbFile (String filePath) {
+		return (filePath.toLowerCase().endsWith(Config.MMDB_REMOTE_FILE_EXT));
+	}
+	
+	public static ArrayList<MediaExplorerItem> getAllRemoteMmdb () {
+		ArrayList<MediaExplorerItem> ret = new ArrayList<MediaExplorerItem>();
+		
+		File dir = Config.getMmdbDir();
+		File [] files = dir.listFiles();
+		
+		// empty dir?
+		if (files == null || files.length < 1 ) return ret;
+		
+		for (File file : files) {
+			if (isRemoteMmdbFile(file.getAbsolutePath())) {
+				MediaExplorerItem newItem = new MediaExplorerItem(MediaExplorerItem.ItemType.REMOTEMMDB);
+				newItem.identifier = file.getAbsolutePath();
+				newItem.title = getRemoteMmdbTitle(newItem.identifier);
+				ret.add(newItem);
+			}
+		}
+		
+		Collections.sort(ret);
+		
+		return ret;
+	}
+	
+	public static String getRemoteMmdbTitle (String filePath) {
+		String ret = filePath;
+		int x;
+		
+		x = ret.lastIndexOf(File.separator);
+		if (x > 0) {
+			ret = ret.substring(x+1);
+		}
+		
+		x = ret.lastIndexOf(Config.MMDB_REMOTE_FILE_EXT);
+		if (x > 0) {
+			ret = ret.substring(0, x);
+		}
+		
+		return ret;
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+}
---
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/server/feedreader/MixedMediaDbFeedParser.java
@@ -0,0 +1,267 @@
+package net.sparktank.morrigan.server.feedreader;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.SocketException;
+import java.net.URLDecoder;
+import java.net.UnknownHostException;
+import java.util.Date;
+import java.util.Stack;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import net.sparktank.morrigan.exceptions.MorriganException;
+import net.sparktank.morrigan.model.media.impl.MixedMediaItem;
+import net.sparktank.morrigan.model.media.impl.RemoteMixedMediaDb;
+import net.sparktank.morrigan.model.media.interfaces.IMixedMediaItem;
+import net.sparktank.morrigan.model.media.interfaces.IMixedMediaItem.MediaType;
+import net.sparktank.morrigan.model.tasks.TaskEventListener;
+import net.sparktank.morrigan.server.HttpClient;
+import net.sparktank.morrigan.server.HttpClient.HttpResponse;
+import net.sparktank.morrigan.server.HttpClient.IHttpStreamHandler;
+import net.sparktank.morrigan.server.feedwriters.XmlHelper;
+import net.sparktank.sqlitewrapper.DbException;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class MixedMediaDbFeedParser extends DefaultHandler {
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	/**
+	 * TODO report progress to taskEventListener.
+	 * TODO support event cancellation.
+	 */
+	public static void parseFeed (final RemoteMixedMediaDb mmdb, final TaskEventListener taskEventListener) throws MorriganException {
+//		if (taskEventListener!=null) taskEventListener.onStart(); // TODO do this?
+		if (taskEventListener!=null) taskEventListener.beginTask("Reading feed...", 100);
+		
+		IHttpStreamHandler httpStreamHandler = new IHttpStreamHandler () {
+			@Override
+			public void handleStream(InputStream is) throws MorriganException {
+				boolean thereWereErrors = true;
+				RemoteMixedMediaDb transClone = null;
+				try {
+					transClone = mmdb.getTransactionalClone();
+					transClone.readFromCache();
+					transClone.beginBulkUpdate();
+			        try {
+			        	SAXParserFactory factory = SAXParserFactory.newInstance();
+			        	factory.setNamespaceAware(true);
+			        	factory.setValidating(true);
+			        	SAXParser parser = factory.newSAXParser();
+			        	parser.parse(is, new MixedMediaDbFeedParser(transClone, taskEventListener));
+					}
+			        catch (SAXException e) {
+						throw new MorriganException(e);
+					} catch (ParserConfigurationException e) {
+						throw new MorriganException(e);
+					} catch (IOException e) {
+						throw new MorriganException(e);
+					}
+					thereWereErrors = false;
+				}
+				catch (DbException e) {
+					throw new MorriganException(e);
+				}
+				finally {
+					if (transClone != null) {
+						try {
+							transClone.completeBulkUpdate(thereWereErrors);
+						} catch (DbException e) {
+							throw new MorriganException(e);
+						} finally {
+							try {
+								if (thereWereErrors) {
+									transClone.rollback();
+								}
+								else {
+									transClone.commitOrRollback();
+								}
+							} catch (DbException e) {
+								throw new MorriganException(e);
+							}
+							finally {
+								transClone.dispose();
+							}
+						}
+					}
+				}
+			}
+		};
+		
+		try {
+			HttpResponse response = HttpClient.getHttpClient().doHttpRequest(mmdb.getUrl(), httpStreamHandler);
+			if (response.getCode() != 200) {
+				throw new MorriganException("After fetching remote MMDB response code was " + response.getCode() + " (expected 200).");
+			}
+		}
+		catch (IOException e) {
+			if (e instanceof UnknownHostException) {
+				throw new MorriganException("Host unknown.", e);
+			} else if (e instanceof SocketException) {
+				throw new MorriganException("Host unreachable.", e);
+			} else {
+				throw new MorriganException(e);
+			}
+		}
+		
+		if (taskEventListener!=null) taskEventListener.done();
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	private final RemoteMixedMediaDb rmmdb;
+	private final TaskEventListener taskEventListener;
+	private final Stack<String> stack;
+	
+	public MixedMediaDbFeedParser(RemoteMixedMediaDb rmmdb, TaskEventListener taskEventListener) {
+		this.taskEventListener = taskEventListener;
+		this.stack = new Stack<String>();
+		this.rmmdb = rmmdb; 
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	private long entryCount = 0;
+	private long entriesProcessed = 0;
+	private int progress = 0;
+	private IMixedMediaItem currentItem;
+	private StringBuilder currentText;
+	
+	@Override
+	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+		this.stack.push(localName);
+		if (this.stack.size() == 2 && localName.equals("entry")) {
+			this.currentItem = new MixedMediaItem(MediaType.UNKNOWN);
+		}
+		else if (this.stack.size() == 3 && localName.equals("link")) {
+			String relVal = attributes.getValue("rel");
+			if (relVal != null && relVal.equals("self")) {
+				String hrefVal = attributes.getValue("href");
+				if (hrefVal != null && hrefVal.length() > 0) {
+					try {
+						String remotePath = URLDecoder.decode(hrefVal, "UTF-8");
+						this.currentItem.setFilepath(remotePath);
+						this.currentItem.setRemoteLocation(hrefVal);
+					} catch (UnsupportedEncodingException e) {
+						throw new SAXException(e);
+					}
+				}
+			}
+		}
+		
+		// If we need a new StringBuilder, make one.
+		if (this.currentText == null || this.currentText.length() > 0) {
+			this.currentText = new StringBuilder();
+		}
+	}
+	
+	@Override
+	public void endElement(String uri, String localName, String qName) throws SAXException {
+		if (this.stack.size() == 2 && localName.equals("count")) {
+			this.entryCount = Long.parseLong(this.currentText.toString());
+		}
+		else if (this.stack.size() == 2 && localName.equals("entry")) {
+			try {
+				this.rmmdb.updateItem(this.currentItem); 
+			}
+			catch (MorriganException e) {
+				throw new SAXException(e);
+			} catch (DbException e) {
+				throw new SAXException(e);
+			}
+			
+			if (this.taskEventListener!=null) {
+				this.entriesProcessed++;
+				int p = (int) ((this.entriesProcessed * 100) / this.entryCount);
+				if (p > this.progress) {
+					this.taskEventListener.worked(p - this.progress);
+					this.progress = p;
+				}
+			}
+		}
+		else if (this.stack.size() == 3 && localName.equals("type")) {
+			int v = Integer.parseInt(this.currentText.toString());
+			MediaType type = MediaType.parseInt(v);
+			this.currentItem.setMediaType(type);
+		}
+		else if (this.stack.size() == 3 && localName.equals("dateadded")) {
+			try {
+				Date d = XmlHelper.getIso8601UtcDateFormatter().parse(this.currentText.toString());
+				this.currentItem.setDateAdded(d);
+			}
+			catch (Exception e) {
+				throw new SAXException(flattenStack(this.stack) + " Exception parsing date '"+this.currentText.toString()+"' for item '"+this.currentItem.getFilepath()+"'.", e);
+			}
+		}
+		else if (this.stack.size() == 3 && localName.equals("datelastmodified")) {
+			try {
+				Date d = XmlHelper.getIso8601UtcDateFormatter().parse(this.currentText.toString());
+				this.currentItem.setDateLastModified(d);
+			}
+			catch (Exception e) {
+				throw new SAXException(flattenStack(this.stack) + " Exception parsing date '"+this.currentText.toString()+"' for item '"+this.currentItem.getFilepath()+"'.", e);
+			}
+		}
+		else if (this.stack.size() == 3 && localName.equals("duration")) {
+			int v = Integer.parseInt(this.currentText.toString());
+			this.currentItem.setDuration(v);
+		}
+		else if (this.stack.size() == 3 && localName.equals("hash")) {
+			long v = Long.parseLong(this.currentText.toString());
+			this.currentItem.setHashcode(v);
+		}
+		else if (this.stack.size() == 3 && localName.equals("startcount")) {
+			long v = Long.parseLong(this.currentText.toString());
+			this.currentItem.setStartCount(v);
+		}
+		else if (this.stack.size() == 3 && localName.equals("endcount")) {
+			long v = Long.parseLong(this.currentText.toString());
+			this.currentItem.setEndCount(v);
+		}
+		else if (this.stack.size() == 3 && localName.equals("datelastplayed")) {
+			try {
+				Date d = XmlHelper.getIso8601UtcDateFormatter().parse(this.currentText.toString());
+				this.currentItem.setDateLastPlayed(d);
+			}
+			catch (Exception e) {
+				throw new SAXException(flattenStack(this.stack) + " Exception parsing date '"+this.currentText.toString()+"' for item '"+this.currentItem.getFilepath()+"'.", e);
+			}
+		}
+		else if (this.stack.size() == 3 && localName.equals("width")) {
+			int v = Integer.parseInt(this.currentText.toString());
+			this.currentItem.setWidth(v);
+		}
+		else if (this.stack.size() == 3 && localName.equals("height")) {
+			int v = Integer.parseInt(this.currentText.toString());
+			this.currentItem.setHeight(v);
+		}
+		
+		this.stack.pop();
+	} // endElement().
+	
+	@Override
+	public void characters(char[] ch, int start, int length) {
+		this.currentText.append( ch, start, length );
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	private static String flattenStack (Stack<String> stack) {
+		StringBuilder sb = new StringBuilder();
+		
+		for (String s : stack) {
+			sb.append(s);
+			sb.append('/');
+		}
+		
+		return sb.toString();
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+}
