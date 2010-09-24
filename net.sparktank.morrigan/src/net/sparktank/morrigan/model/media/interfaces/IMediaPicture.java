c45687759a16250c205de4c68a5dfbee2aa8e800
haku <haku@vaguehope.com>
2010-09-24 20:30:51 +0000

    add isPicture to IMediaPicture
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@1215 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan/src/net/sparktank/morrigan/model/media/interfaces/IMediaPicture.java
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/model/media/interfaces/IMediaPicture.java
@@ -3,6 +3,10 @@ package net.sparktank.morrigan.model.media.interfaces;
 public interface IMediaPicture extends IMediaItem {
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	
+	public boolean isPicture ();
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
 	public int getWidth();
 	public boolean setWidth(int width);
 	
