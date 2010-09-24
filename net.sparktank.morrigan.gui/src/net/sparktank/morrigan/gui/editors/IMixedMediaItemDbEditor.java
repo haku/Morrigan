a4a910df810504d3e1dbdb0b69f033ab83f69665
haku <haku@vaguehope.com>
2010-09-24 20:31:41 +0000

    add interface IMixedMediaItemDbEditor
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@1216 2128fabd-03f8-4a31-9ba6-05665b9bae8a

---
+++ net.sparktank.morrigan.gui/src/net/sparktank/morrigan/gui/editors/IMixedMediaItemDbEditor.java
@@ -0,0 +1,13 @@
+package net.sparktank.morrigan.gui.editors;
+
+import net.sparktank.morrigan.model.media.interfaces.IMediaItemDb;
+import net.sparktank.morrigan.model.media.interfaces.IMixedMediaItem;
+import net.sparktank.morrigan.model.media.interfaces.IMixedMediaStorageLayer;
+
+public interface IMixedMediaItemDbEditor<H extends IMediaItemDb<H,S,T>, S extends IMixedMediaStorageLayer<T>, T extends IMixedMediaItem>
+		extends IMediaItemDbEditor<H,S,T> {
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+}
