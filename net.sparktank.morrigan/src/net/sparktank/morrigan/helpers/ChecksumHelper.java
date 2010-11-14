96ad59b23481b8766bfa7e644427644fb7feb7ad
haku <haku@vaguehope.com>
2010-11-14 18:51:46 +0000

    Create method for MD5ing a file.  Also a quick test / demo class.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@1234 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan/src/net/sparktank/morrigan/helpers/ChecksumHelper.java
+++ net.sparktank.morrigan/src/net/sparktank/morrigan/helpers/ChecksumHelper.java
@@ -1,8 +1,11 @@
 package net.sparktank.morrigan.helpers;
 
 import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.zip.CRC32;
@@ -33,7 +36,47 @@ public class ChecksumHelper {
 		} finally {
 			fis.close();
 		}
+	}
+	
+	/**
+	 * MessageDigest.getInstance("MD5") can take up to a second,
+	 * so using this to cache it and improve performance.
+	 * Not sure if MessageDigest is thread-safe, so using ThreadLocal
+	 * just in case.
+	 * TODO find out if MessageDigest is thread-safe.
+	 */
+	static public ThreadLocal<MessageDigest> mdMd5Factory = new ThreadLocal<MessageDigest>() {
+		@Override
+		protected MessageDigest initialValue() {
+			try {
+				return MessageDigest.getInstance("MD5");
+			}
+			catch (NoSuchAlgorithmException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	};
+	
+	static public BigInteger generateMd5Checksum (File file) throws IOException {
+		InputStream is = null;
+		byte[] buffer = new byte[1024]; // TODO is this the right buffer size to use?
 		
+		try {
+			is = new FileInputStream(file);
+			MessageDigest md = mdMd5Factory.get();
+			
+			int n;
+			do {
+				n = is.read(buffer);
+				if (n > 0) md.update(buffer, 0, n);
+			} while (n != -1);
+			
+			BigInteger bi = new BigInteger(1, md.digest());
+			return bi;
+		}
+		finally {
+			if (is != null) is.close();
+		}
 	}
 	
 	public static String Md5String(String text) {
