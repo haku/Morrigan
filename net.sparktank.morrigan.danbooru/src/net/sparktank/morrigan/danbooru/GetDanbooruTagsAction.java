1738a84024b56e1643884ab56bf9a66eb3e3c3a5
haku <haku@vaguehope.com>
2010-11-25 23:20:14 +0000

    Improve danbooru plugin to work over selections of multiple items.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@1247 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan.danbooru/src/net/sparktank/morrigan/danbooru/GetDanbooruTagsAction.java
+++ net.sparktank.morrigan.danbooru/src/net/sparktank/morrigan/danbooru/GetDanbooruTagsAction.java
@@ -2,6 +2,8 @@ package net.sparktank.morrigan.danbooru;
 
 import java.io.File;
 import java.math.BigInteger;
+import java.util.LinkedList;
+import java.util.List;
 
 import net.sparktank.morrigan.exceptions.MorriganException;
 import net.sparktank.morrigan.gui.dialogs.RunnableDialog;
@@ -9,6 +11,7 @@ import net.sparktank.morrigan.gui.views.ViewTagEditor;
 import net.sparktank.morrigan.helpers.ChecksumHelper;
 import net.sparktank.morrigan.model.media.interfaces.IMediaItem;
 import net.sparktank.morrigan.model.media.interfaces.IMediaItemDb;
+import net.sparktank.morrigan.model.media.interfaces.IMixedMediaItem;
 import net.sparktank.morrigan.model.tags.MediaTagClassification;
 import net.sparktank.morrigan.model.tags.MediaTagType;
 
@@ -44,59 +47,81 @@ public class GetDanbooruTagsAction extends Action {
 	@Override
 	public void run() {
 		IMediaItemDb<?,?,?> editedItemDb = this.viewTagEd.getEditedItemDb();
-		IMediaItem editedItem = this.viewTagEd.getEditedItem();
+		List<IMediaItem> editedItems = this.viewTagEd.getEditedItems();
 		
-		if (editedItemDb != null && editedItem != null) {
-			FetchTagsJob job = new FetchTagsJob(editedItemDb, editedItem, this.viewTagEd);
-			job.schedule();
+		if (editedItemDb != null && editedItems != null && editedItems.size() > 0) {
+			List<IMixedMediaItem> items = new LinkedList<IMixedMediaItem>();
+			
+			for (IMediaItem item : editedItems) {
+				if (item instanceof IMixedMediaItem) {
+					IMixedMediaItem mmItem = (IMixedMediaItem) item;
+					items.add(mmItem);
+				}
+			}
+			
+			if (items.size() > 0) {
+				FetchTagsJob job = new FetchTagsJob(editedItemDb, items, this.viewTagEd);
+				job.schedule();
+			}
+			else {
+				Display.getDefault().asyncExec(new RunnableDialog("No pictures in selection."));
+			}
 		}
 	}
 	
 	static class FetchTagsJob extends Job {
 
 		private final IMediaItemDb<?, ?, ?> editedItemDb;
-		private final IMediaItem editedItem;
+		private final List<IMixedMediaItem> editedItems;
 		private final ViewTagEditor viewTagEd;
 
-		public FetchTagsJob (IMediaItemDb<?, ?, ?> editedItemDb, IMediaItem editedItem, ViewTagEditor viewTagEd) {
+		public FetchTagsJob (IMediaItemDb<?, ?, ?> editedItemDb, List<IMixedMediaItem> editedItems, ViewTagEditor viewTagEd) {
 			super("Fetching tags from Danbooru");
 			this.editedItemDb = editedItemDb;
-			this.editedItem = editedItem;
+			this.editedItems = editedItems;
 			this.viewTagEd = viewTagEd;
 		}
 		
 		@Override
 		protected IStatus run(IProgressMonitor monitor) {
-			File file = new File(this.editedItem.getFilepath());
 			try {
-				BigInteger checksum = ChecksumHelper.generateMd5Checksum(file); // TODO update model to track MD5.
-				String md5 = checksum.toString(16);
-				String[] tags = Danbooru.getTags(md5);
-				if (tags != null) {
-					MediaTagClassification cls = this.editedItemDb.getTagClassification(CATEGORY);
-					if (cls == null) {
-						this.editedItemDb.addTagClassification(CATEGORY);
-						cls = this.editedItemDb.getTagClassification(CATEGORY);
-						if (cls == null) throw new MorriganException("Failed to add tag category '"+CATEGORY+"'.");
-					}
-					
-					int n = 0;
-					for (String tag : tags) {
-						if (!this.editedItemDb.hasTag(this.editedItem, tag, MediaTagType.AUTOMATIC, cls)) {
-							this.editedItemDb.addTag(this.editedItem, tag, MediaTagType.AUTOMATIC, cls);
-							n++;
+				long nItems = 0;
+				long nTags = 0;
+				
+				for (IMixedMediaItem item : this.editedItems) {
+					if (item.isPicture()) {
+						File file = new File(item.getFilepath());
+						BigInteger checksum = ChecksumHelper.generateMd5Checksum(file); // TODO update model to track MD5.
+						String md5 = checksum.toString(16);
+						
+						String[] tags = Danbooru.getTags(md5);
+						if (tags != null) {
+							MediaTagClassification cls = this.editedItemDb.getTagClassification(CATEGORY);
+							if (cls == null) {
+								this.editedItemDb.addTagClassification(CATEGORY);
+								cls = this.editedItemDb.getTagClassification(CATEGORY);
+								if (cls == null) throw new MorriganException("Failed to add tag category '"+CATEGORY+"'.");
+							}
+							
+							boolean added = false;
+							for (String tag : tags) {
+								if (!this.editedItemDb.hasTag(item, tag, MediaTagType.AUTOMATIC, cls)) {
+									this.editedItemDb.addTag(item, tag, MediaTagType.AUTOMATIC, cls);
+									added = true;
+									nTags++;
+								}
+							}
+							
+							if (added) nItems++;
 						}
 					}
-					
-					if (n > 0) {
-						this.viewTagEd.refreshContent();
-					}
-					
-					Display.getDefault().asyncExec(new RunnableDialog("Found " + tags.length + " tags, added " + n + " new ones."));
 				}
-				else {
-					Display.getDefault().asyncExec(new RunnableDialog("No tags found on Danbooru."));
+				
+				if (this.editedItems.size() == 1 && nTags > 0) {  // TODO improve this by checking the selected item was updated.
+					this.viewTagEd.refreshContent();
 				}
+				
+				Display.getDefault().asyncExec(new RunnableDialog("Found " + nTags + " new tags for " + nItems + " items."));
 				
 				return Status.OK_STATUS;
 			}
