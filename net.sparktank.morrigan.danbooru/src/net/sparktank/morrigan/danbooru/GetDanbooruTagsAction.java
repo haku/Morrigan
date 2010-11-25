2b8f1dc1eb9b6059a9c8fe22e3a7fa308f89fcca
haku <haku@vaguehope.com>
2010-11-25 00:21:35 +0000

    do web request.  make background task.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@1242 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan.danbooru/src/net/sparktank/morrigan/danbooru/GetDanbooruTagsAction.java
+++ net.sparktank.morrigan.danbooru/src/net/sparktank/morrigan/danbooru/GetDanbooruTagsAction.java
@@ -1,9 +1,20 @@
 package net.sparktank.morrigan.danbooru;
 
+import java.io.File;
+import java.math.BigInteger;
+import java.net.URL;
+
 import net.sparktank.morrigan.gui.dialogs.MorriganMsgDlg;
 import net.sparktank.morrigan.gui.views.ViewTagEditor;
+import net.sparktank.morrigan.helpers.ChecksumHelper;
 import net.sparktank.morrigan.model.media.interfaces.IMediaItem;
+import net.sparktank.morrigan.server.HttpClient;
+import net.sparktank.morrigan.server.HttpClient.HttpResponse;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.jface.action.Action;
 
 public class GetDanbooruTagsAction extends Action {
@@ -30,13 +41,48 @@ public class GetDanbooruTagsAction extends Action {
 	public void run() {
 		IMediaItem editedItem = this.viewTagEd.getEditedItem();
 		
-		new MorriganMsgDlg("TODO: get tags from Danbooru for img '" + editedItem.getTitle() + "'.").open();
-		
-		// TODO get tags.
-		// TODO add tags to image.
+		if (editedItem != null) {
+			FetchTagsJob job = new FetchTagsJob(editedItem);
+			job.schedule();
+		}
 		
 //		IMediaItemDb<?, ?, ?> editedItemDb = this.viewTagEd.getEditedItemDb();
 //		editedItemDb.addTag(this.editedItem, text, MediaTagType.MANUAL, (MediaTagClassification)null);
+	}
+	
+	static class FetchTagsJob extends Job {
+
+		private final IMediaItem editedItem;
+
+		public FetchTagsJob (IMediaItem editedItem) {
+			super("Fetching tags from Danbooru");
+			this.editedItem = editedItem;
+		}
+		
+		@Override
+		protected IStatus run(IProgressMonitor monitor) {
+			File file = new File(this.editedItem.getFilepath());
+			try {
+				BigInteger checksum = ChecksumHelper.generateMd5Checksum(file); // TODO update model to track MD5.
+				String md5 = checksum.toString(16);
+				
+				String surl = "http://danbooru.donmai.us/post/index.xml?tags=md5:" + md5;
+				URL url = new URL(surl);
+				HttpResponse response = HttpClient.getHttpClient().doHttpRequest(url);
+				
+				// TODO parse out tags.
+				// TODO add tags to image.
+				
+				new MorriganMsgDlg("TODO: parse:\n" + response).open();
+				
+				return Status.OK_STATUS;
+			}
+			catch (Exception e) {
+				new MorriganMsgDlg(e).open();
+				return new Status(IStatus.ERROR, Activator.PLUGIN_ID, e.getMessage(), e);
+			}
+		}
+		
 	}
 	
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
