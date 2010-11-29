fb48013b05fd21781bcabda946f4a8f5f6ae2ee3
haku <haku@vaguehope.com>
2010-11-29 11:26:36 +0000

    Share project "net.sparktank.morrigan.model" into "https://aomushi.ath.cx:43443/svn/morrigan"
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@1274 2128fabd-03f8-4a31-9ba6-05665b9bae8a

---
+++ net.sparktank.morrigan.model/src/net/sparktank/morrigan/model/tasks/TaskEventListener.java
@@ -0,0 +1,15 @@
+package net.sparktank.morrigan.model.tasks;
+
+
+// TODO move this class somewhere more sensible.
+public interface TaskEventListener {
+	public void onStart ();
+	public void logMsg (String topic, String s);
+	public void logError (String topic, String s, Throwable t);
+	
+	public void beginTask(String name, int totalWork);
+	public void subTask(String name);
+	public void done();
+	public boolean isCanceled();
+	public void worked(int work);
+}
