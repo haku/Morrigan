9005a8708e48ff2672134d90285e18272d504694
haku <haku@vaguehope.com>
2010-11-29 15:16:47 +0000

    Share project "net.sparktank.morrigan.util.httpclient" into "https://aomushi.ath.cx:43443/svn/morrigan"
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@1299 2128fabd-03f8-4a31-9ba6-05665b9bae8a

---
+++ net.sparktank.morrigan.util.httpclient/src/net/sparktank/morrigan/util/httpclient/HttpStreamHandlerException.java
@@ -0,0 +1,19 @@
+package net.sparktank.morrigan.util.httpclient;
+
+public class HttpStreamHandlerException extends Exception {
+	
+	private static final long serialVersionUID = 5212664459071370278L;
+	
+	public HttpStreamHandlerException (String s) {
+		super(s);
+	}
+	
+	public HttpStreamHandlerException (String s, Throwable t) {
+		super(s, t);
+	}
+	
+	public HttpStreamHandlerException(Throwable t) {
+		super(t);
+	}
+	
+}
