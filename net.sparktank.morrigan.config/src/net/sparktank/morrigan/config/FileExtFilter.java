784791dd30a770155691f9a59b56b3e58006fa00
haku <haku@vaguehope.com>
2010-11-29 14:34:24 +0000

    Share project "net.sparktank.morrigan.config" into "https://aomushi.ath.cx:43443/svn/morrigan"
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@1287 2128fabd-03f8-4a31-9ba6-05665b9bae8a

---
+++ net.sparktank.morrigan.config/src/net/sparktank/morrigan/config/FileExtFilter.java
@@ -0,0 +1,24 @@
+package net.sparktank.morrigan.config;
+
+import java.io.File;
+import java.io.FileFilter;
+
+public class FileExtFilter implements FileFilter {
+	
+	private final String[] ext;
+	
+	public FileExtFilter (String... ext) {
+		this.ext = ext;
+	}
+	
+	@Override
+	public boolean accept (File file) {
+		for (String e : this.ext) {
+			if (file.getName().toLowerCase().endsWith(e)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+}
