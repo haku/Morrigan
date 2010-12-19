bc6ad0adbd2f9223bceab3e03e0c885c8b8af809
はく <haku@vaguehope.com>
2010-12-19 10:36:22 +0000

    Create net.sparktank.morrigan.android .

---
+++ net.sparktank.morrigan.android/src/net/sparktank/morrigan/android/PlayerActivity.java
@@ -0,0 +1,15 @@
+package net.sparktank.morrigan.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PlayerActivity extends Activity {
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    
+}
