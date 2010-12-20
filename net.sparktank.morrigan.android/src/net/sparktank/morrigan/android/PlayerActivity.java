1af5dcaf5b7f70c641b15393038dbf7c31e7cefd
はく <haku@vaguehope.com>
2010-12-20 12:42:10 +0000

    Refactor to SetPlaystateTask and enable 'NEXT' action.

--- net.sparktank.morrigan.android/src/net/sparktank/morrigan/android/PlayerActivity.java
+++ net.sparktank.morrigan.android/src/net/sparktank/morrigan/android/PlayerActivity.java
@@ -2,7 +2,8 @@ package net.sparktank.morrigan.android;
 
 import net.sparktank.morrigan.android.model.ServerReference;
 import net.sparktank.morrigan.android.model.impl.ServerReferenceImpl;
-import net.sparktank.morrigan.android.tasks.PlaypauseTask;
+import net.sparktank.morrigan.android.tasks.SetPlaystateTask;
+import net.sparktank.morrigan.android.tasks.SetPlaystateTask.TargetPlayState;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -20,47 +21,48 @@ public class PlayerActivity extends Activity {
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 //	Activity methods.
 	
-    @Override
-    public void onCreate (Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.player);
-        
-        hookUpButtons();
-    }
-    
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.player);
+		
+		hookUpButtons();
+	}
+	
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 //	Buttons.
     
-    private void hookUpButtons () {
-    	ImageButton cmd;
-    	
-    	cmd = (ImageButton) findViewById(R.id.btnPlaypause);
-    	cmd.setOnClickListener(new BtnPlaypause_OnClick());
-    	
-    	cmd = (ImageButton) findViewById(R.id.btnNext);
-    	cmd.setOnClickListener(new BtnNext_OnClick());
-    }
-    
-    class BtnPlaypause_OnClick implements OnClickListener {
+	private void hookUpButtons () {
+		ImageButton cmd;
+		
+		cmd = (ImageButton) findViewById(R.id.btnPlaypause);
+		cmd.setOnClickListener(new BtnPlaypause_OnClick());
+		
+		cmd = (ImageButton) findViewById(R.id.btnNext);
+		cmd.setOnClickListener(new BtnNext_OnClick());
+	}
+	
+	class BtnPlaypause_OnClick implements OnClickListener {
 		@Override
 		public void onClick(View v) {
-			PlaypauseTask playpauseTask = new PlaypauseTask(PlayerActivity.this, PlayerActivity.this.serverReference);
+			SetPlaystateTask playpauseTask = new SetPlaystateTask(PlayerActivity.this, PlayerActivity.this.serverReference, TargetPlayState.PLAYPAUSE);
 			playpauseTask.execute();
 		}
-    }
-    
-    class BtnNext_OnClick implements OnClickListener {
-    	@Override
-    	public void onClick(View v) {
-    		Toast.makeText(getApplicationContext(), "TODO: Next.", Toast.LENGTH_LONG).show();
-    	}
-    }
-    
+	}
+	
+	class BtnNext_OnClick implements OnClickListener {
+		@Override
+		public void onClick(View v) {
+			SetPlaystateTask playpauseTask = new SetPlaystateTask(PlayerActivity.this, PlayerActivity.this.serverReference, TargetPlayState.NEXT);
+			playpauseTask.execute();
+		}
+	}
+
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 //	Menu items.
-    
-    private static final int MENU_REFRESH = 1;
-    
+	
+	private static final int MENU_REFRESH = 1;
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		boolean result = super.onCreateOptionsMenu(menu);
