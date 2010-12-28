5657b67b1a1fc264692415d65e3f51f6a9418cd0
はく <haku@vaguehope.com>
2010-12-28 21:23:45 +0000

    Initial impl for adding servers.

--- net.sparktank.morrigan.android/src/net/sparktank/morrigan/android/ServersActivity.java
+++ net.sparktank.morrigan.android/src/net/sparktank/morrigan/android/ServersActivity.java
@@ -24,11 +24,16 @@ import net.sparktank.morrigan.android.model.ServerReferenceListAdapter;
 import net.sparktank.morrigan.android.model.impl.ServerReferenceImpl;
 import net.sparktank.morrigan.android.model.impl.ServerReferenceListAdapterImpl;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
 
 public class ServersActivity extends Activity {
@@ -61,6 +66,10 @@ public class ServersActivity extends Activity {
 		ListView lstServers = (ListView) findViewById(R.id.lstServers);
 		lstServers.setAdapter(this.serversListAdapter);
 		lstServers.setOnItemClickListener(this.serversListCickListener);
+		
+		ImageButton cmd;
+		cmd = (ImageButton) findViewById(R.id.btnAddServer);
+		cmd.setOnClickListener(new BtnAddServer_OnClick());
 	}
 	
 	private OnItemClickListener serversListCickListener = new OnItemClickListener() {
@@ -71,7 +80,51 @@ public class ServersActivity extends Activity {
 		}
 	};
 	
+	private class BtnAddServer_OnClick implements OnClickListener {
+		@Override
+		public void onClick(View v) {
+			addServer();
+		}
+	}
+	
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//	Commands - to be called on the UI thread.
+	
+	protected void addServer () {
+		final AlertDialog.Builder dlgBuilder = new AlertDialog.Builder(this);
+		dlgBuilder.setTitle("Add server");
+		
+		final EditText editText = new EditText(this);
+		editText.setText("http://host:8080");
+		dlgBuilder.setView(editText);
+		
+		dlgBuilder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int whichButton) {
+				String inputString = editText.getText().toString().trim();
+				
+				if (inputString == null || inputString.length() < 1) {
+					dialog.cancel();
+					return;
+				}
+				
+				dialog.dismiss();
+				// TODO validate inputString!
+				
+				serversListAdapter.getInputData().add(new ServerReferenceImpl(inputString));
+				serversListAdapter.notifyDataSetChanged();
+			}
+		});
+		
+		dlgBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int whichButton) {
+				dialog.cancel();
+			}
+		});
+		
+		dlgBuilder.show();
+	}
 	
 	protected void showServerActivity (ServerReference item) {
 		Intent intent = new Intent(getApplicationContext(), ServerActivity.class);
