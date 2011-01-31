1be0988f5563883950e4b4aebf93b4312868afea
haku <haku@vaguehope.com>
2011-01-31 18:10:31 +0000

    Cleanup Activator class.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@1401 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan.gui/src/net/sparktank/morrigan/gui/Activator.java
+++ net.sparktank.morrigan.gui/src/net/sparktank/morrigan/gui/Activator.java
@@ -6,48 +6,52 @@ import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
-/**
- * The activator class controls the plug-in life cycle
- */
 public class Activator extends AbstractUIPlugin {
-
-	// The plug-in ID
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
 	public static final String PLUGIN_ID = "net.sparktank.morrigan.gui";
-
-	// The shared instance
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	private static BundleContext context;
 	private static Activator plugin;
 	
-	/**
-	 * The constructor
-	 */
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
 	public Activator() {/* UNUSED */}
-
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
 	@Override
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		plugin = this;
+	public void start(BundleContext bundleContext) throws Exception {
+		super.start(bundleContext);
+		Activator.plugin = this;
+		Activator.context = bundleContext;
 	}
-
+	
 	@Override
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
+	public void stop(BundleContext bundleContext) throws Exception {
+		Activator.plugin = null;
+		Activator.context = null;
 		
 		try {
 			MediaFactoryImpl.get().disposeAllPlaylists();
-		} finally {
-			super.stop(context);
+		}
+		finally {
+			super.stop(bundleContext);
 		}
 	}
-
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
-	public static Activator getDefault() {
-		return plugin;
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	public static BundleContext getContext() {
+		return Activator.context;
 	}
-
+	
+	public static Activator getDefault() {
+		return Activator.plugin;
+	}
+	
 	/**
 	 * Returns an image descriptor for the image file at the given
 	 * plug-in relative path
@@ -58,4 +62,6 @@ public class Activator extends AbstractUIPlugin {
 	public static ImageDescriptor getImageDescriptor(String path) {
 		return imageDescriptorFromPlugin(PLUGIN_ID, path);
 	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 }
