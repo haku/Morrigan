6760694af3083f5a40d618b7e5b982d7b33ff6d0
haku <haku@vaguehope.com>
2011-04-25 15:50:21 +0000

    Bugfix: full screen after screen resolution change.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@1437 2128fabd-03f8-4a31-9ba6-05665b9bae8a

---
+++ net.sparktank.morrigan.gui/src/net/sparktank/morrigan/gui/helpers/MonitorHelper.java
@@ -0,0 +1,25 @@
+package net.sparktank.morrigan.gui.helpers;
+
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Monitor;
+
+public class MonitorHelper {
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	/**
+	 * Horrifying method to 'refresh' a cached monitor object.
+	 * This is useful in case the display configuration has been changed,
+	 * for example after screen's resolution has been changed.
+	 */
+	public static Monitor refreshMonitor (Display d, Monitor m) {
+		for (int i = 0; i < d.getMonitors().length; i++) {
+			Monitor mon = d.getMonitors()[i];
+			if (mon.equals(m)) {
+				return mon;
+			}
+		}
+		throw new IllegalArgumentException("Monitor " + m + " does not exist.");
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+}
