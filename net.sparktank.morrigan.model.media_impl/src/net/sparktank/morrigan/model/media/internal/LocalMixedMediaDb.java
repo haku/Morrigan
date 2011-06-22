6a207e75422fd39538b13da654b3fe9bd5bb2f49
haku <haku@vaguehope.com>
2011-06-22 00:18:13 +0000

    Fix whitespace.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@1445 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan.model.media_impl/src/net/sparktank/morrigan/model/media/internal/LocalMixedMediaDb.java
+++ net.sparktank.morrigan.model.media_impl/src/net/sparktank/morrigan/model/media/internal/LocalMixedMediaDb.java
@@ -12,24 +12,24 @@ public class LocalMixedMediaDb extends AbstractMixedMediaDb<ILocalMixedMediaDb> 
 	
 	public static class LocalMixedMediaDbFactory extends RecyclingFactory<ILocalMixedMediaDb, String, String, DbException> {
 		
-		protected LocalMixedMediaDbFactory() {
+		protected LocalMixedMediaDbFactory () {
 			super(true);
 		}
 		
 		@Override
-		protected boolean isValidProduct(ILocalMixedMediaDb product) {
+		protected boolean isValidProduct (ILocalMixedMediaDb product) {
 //			System.out.println("Found '" + product.getDbPath() + "' in cache.");
 			return true;
 		}
 		
 		@Override
-		protected ILocalMixedMediaDb makeNewProduct(String material) throws DbException {
+		protected ILocalMixedMediaDb makeNewProduct (String material) throws DbException {
 //			System.out.println("Making object instance '" + material + "'...");
 			return new LocalMixedMediaDb(LocalMixedMediaDbHelper.getMmdbTitle(material), MixedMediaSqliteLayer.FACTORY.manufacture(material), null);
 		}
 		
 		@Override
-		protected ILocalMixedMediaDb makeNewProduct(String material, String config) throws DbException {
+		protected ILocalMixedMediaDb makeNewProduct (String material, String config) throws DbException {
 			return new LocalMixedMediaDb(LocalMixedMediaDbHelper.getMmdbTitle(material), MixedMediaSqliteLayer.FACTORY.manufacture(material), config);
 		}
 		
@@ -44,15 +44,15 @@ public class LocalMixedMediaDb extends AbstractMixedMediaDb<ILocalMixedMediaDb> 
 	}
 
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
+	
 	@Override
-	public String getType() {
+	public String getType () {
 		return TYPE;
 	}
 	
 	@SuppressWarnings("boxing")
 	@Override
-	public LocalMixedMediaDb getTransactionalClone() throws DbException {
+	public LocalMixedMediaDb getTransactionalClone () throws DbException {
 		return new LocalMixedMediaDb(LocalMixedMediaDbHelper.getMmdbTitle(getDbPath()),
 				MixedMediaSqliteLayer.FACTORY.manufacture(getDbPath(), false, true),
 				this.getEscapedSearchTerm());
