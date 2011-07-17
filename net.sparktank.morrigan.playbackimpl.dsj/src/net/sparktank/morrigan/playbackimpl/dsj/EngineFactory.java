cf4b1aa0dfe5a9f8a5d4845b25766945a0db0537
haku <haku@vaguehope.com>
2011-07-17 14:54:37 +0000

    Remove unused commented out code from OSGi-ifying win32 engines.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@1465 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- net.sparktank.morrigan.playbackimpl.dsj/src/net/sparktank/morrigan/playbackimpl/dsj/EngineFactory.java
+++ net.sparktank.morrigan.playbackimpl.dsj/src/net/sparktank/morrigan/playbackimpl/dsj/EngineFactory.java
@@ -4,10 +4,12 @@ import net.sparktank.morrigan.engines.playback.IPlaybackEngine;
 import net.sparktank.morrigan.engines.playback.PlaybackEngineFactory;
 
 public class EngineFactory implements PlaybackEngineFactory {
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	
 	@Override
 	public IPlaybackEngine getNewPlaybackEngine() {
 		return new PlaybackEngine();
 	}
 	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 }
