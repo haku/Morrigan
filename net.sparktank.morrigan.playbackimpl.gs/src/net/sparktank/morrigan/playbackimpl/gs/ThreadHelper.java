4841a498ca793703bfaca7b696eaa883eb95177b
haku <haku@vaguehope.com>
2011-07-18 20:10:23 +0000

    Yet more massive restructure.  Brake out nested classes to try and decomplexify PlaybackEngine.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@1476 2128fabd-03f8-4a31-9ba6-05665b9bae8a

---
+++ net.sparktank.morrigan.playbackimpl.gs/src/net/sparktank/morrigan/playbackimpl/gs/ThreadHelper.java
@@ -0,0 +1,23 @@
+package net.sparktank.morrigan.playbackimpl.gs;
+
+import org.eclipse.swt.widgets.Widget;
+
+public class ThreadHelper {
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	private ThreadHelper () { /* Unused */ }
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//	UI Threading.
+	
+	static public void runInUiThread (Widget w, Runnable r) {
+		if (w.getDisplay().getThread().equals(Thread.currentThread())) {
+			r.run();
+		}
+		else {
+			w.getDisplay().syncExec(r);
+		}
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+}
