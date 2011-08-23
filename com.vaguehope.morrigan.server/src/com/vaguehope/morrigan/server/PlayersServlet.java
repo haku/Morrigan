979a0390ceb4fae237a17224204bbc991ea4db98
haku <haku@vaguehope.com>
2011-08-23 19:11:32 +0000

    Handlers for moving queue items via HTTP interface.  Updates to Player to allow such manipulation.
    git-svn-id: https://aomushi.ath.cx:43443/svn/morrigan/trunk@1639 2128fabd-03f8-4a31-9ba6-05665b9bae8a

--- com.vaguehope.morrigan.server/src/com/vaguehope/morrigan/server/PlayersServlet.java
+++ com.vaguehope.morrigan.server/src/com/vaguehope/morrigan/server/PlayersServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 import java.net.URLEncoder;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -13,7 +14,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 
 import org.xml.sax.SAXException;
 
@@ -34,13 +34,18 @@ import com.vaguehope.morrigan.util.TimeHelper;
  * Valid URLs:
  * <pre>
  *  GET /players
- *  GET /players/0
- *  GET /players/0/queue
  * 
+ *  GET /players/0
  * POST /players/0 action=playpause
  * POST /players/0 action=next
  * POST /players/0 action=stop
  * POST /players/0 action=fullscreen&monitor=0
+ * 
+ *  GET /players/0/queue
+ * POST /players/0/queue/0 action=top
+ * POST /players/0/queue/0 action=up
+ * POST /players/0/queue/0 action=down
+ * POST /players/0/queue/0 action=bottom
  * </pre>
  */
 public class PlayersServlet extends HttpServlet {
@@ -54,6 +59,14 @@ public class PlayersServlet extends HttpServlet {
 	private static final String CMD_NEXT = "next";
 	private static final String CMD_STOP = "stop";
 	private static final String CMD_FULLSCREEN = "fullscreen";
+	
+	private static final String CMD_CLEAR = "clear";
+	private static final String CMD_SHUFFLE = "shuffle";
+	private static final String CMD_TOP = "top";
+	private static final String CMD_UP = "up";
+	private static final String CMD_REMOVE = "remove";
+	private static final String CMD_DOWN = "down";
+	private static final String CMD_BOTTOM = "bottom";
 	
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	
@@ -71,9 +84,49 @@ public class PlayersServlet extends HttpServlet {
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		int n = Integer.parseInt(req.getPathInfo().substring(ROOTPATH.length()));
-		IPlayerAbstract player = PlayerRegister.getLocalPlayer(n);
-		
+		try {
+			String requestURI = req.getRequestURI();
+			String reqPath = requestURI.startsWith(CONTEXTPATH) ? requestURI.substring(CONTEXTPATH.length()) : requestURI;
+			
+			if (reqPath == null || reqPath.length() < 1 || reqPath.equals(ROOTPATH)) { // POST to root.
+				resp.setContentType("text/plain");
+				resp.getWriter().println("Invalid POST request desu~");
+			}
+			else {
+				String path = reqPath.startsWith(ROOTPATH) ? reqPath.substring(ROOTPATH.length()) : reqPath;
+				if (path.length() > 0) {
+					String[] pathParts = path.split("/");
+					int playerId = Integer.parseInt(pathParts[0]);
+					IPlayerAbstract player = PlayerRegister.getLocalPlayer(playerId);
+					if (pathParts.length == 1) {
+						postToPlayer(req, resp, player);
+					}
+					else if (pathParts.length >= 2 && PATH_QUEUE.equals(pathParts[1])) {
+						if (pathParts.length >= 3) {
+							int item = Integer.parseInt(pathParts[2]);
+							postToQueue(req, resp, player, item);
+						}
+						else {
+							postToQueue(req, resp, player);
+						}
+					}
+					else {
+						resp.setContentType("text/plain");
+						resp.getWriter().println("Invalid POST request to player "+player.getId()+": '"+path+"' desu~");
+					}
+				}
+				else {
+					resp.setContentType("text/plain");
+					resp.getWriter().println("Invalid POST request to '"+path+"' desu~");
+				}
+			}
+		}
+		catch (SAXException e) {
+			throw new ServletException(e);
+		}
+	}
+	
+	private static void postToPlayer (HttpServletRequest req, HttpServletResponse resp, IPlayerAbstract player) throws IOException, ServletException {
 		String act = req.getParameter("action");
 		if (act == null) {
 			resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
@@ -104,6 +157,64 @@ public class PlayersServlet extends HttpServlet {
 				resp.setContentType("text/plain");
 				resp.getWriter().println("HTTP Error 400 'fullscreen' parameter not set desu~");
 			}
+		}
+		else {
+			resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			resp.setContentType("text/plain");
+			resp.getWriter().println("HTTP Error 400 invalid 'action' parameter '"+act+"' desu~");
+		}
+	}
+	
+	private static void postToQueue (HttpServletRequest req, HttpServletResponse resp, IPlayerAbstract player) throws IOException, SAXException {
+		String act = req.getParameter("action");
+		if (act == null) {
+			resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			resp.setContentType("text/plain");
+			resp.getWriter().println("HTTP Error 400 'action' parameter not set desu~");
+		}
+		else if (act.equals(CMD_CLEAR)) {
+			player.clearQueue();
+			printPlayerQueue(resp, player);
+		}
+		else if (act.equals(CMD_SHUFFLE)) {
+			player.shuffleQueue();
+			printPlayerQueue(resp, player);
+		}
+		else {
+			resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			resp.setContentType("text/plain");
+			resp.getWriter().println("HTTP Error 400 invalid 'action' parameter '"+act+"' desu~");
+		}
+	}
+	
+	private static void postToQueue (HttpServletRequest req, HttpServletResponse resp, IPlayerAbstract player, int item) throws IOException, SAXException {
+		String act = req.getParameter("action");
+		if (act == null) {
+			resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			resp.setContentType("text/plain");
+			resp.getWriter().println("HTTP Error 400 'action' parameter not set desu~");
+		}
+		else if (act.equals(CMD_UP) || act.equals(CMD_DOWN) || act.equals(CMD_REMOVE)) {
+			PlayItem queueItem = player.getQueueItemById(item);
+			if (queueItem != null) {
+				if (act.equals(CMD_REMOVE)) {
+					player.removeFromQueue(queueItem);
+				}
+				else {
+					player.moveInQueue(Arrays.asList(queueItem), act.equals(CMD_DOWN));
+				}
+				printPlayerQueue(resp, player);
+			}
+			else {
+				resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+				resp.setContentType("text/plain");
+				resp.getWriter().println("HTTP Error 400 item '"+item+"' not found desu~");
+			}
+		}
+		else if (act.equals(CMD_TOP) || act.equals(CMD_BOTTOM)) {
+			resp.setStatus(HttpServletResponse.SC_SERVICE_UNAVAILABLE);
+			resp.setContentType("text/plain");
+			resp.getWriter().println("top/bottom not implemented desu~");
 		}
 		else {
 			resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
@@ -207,7 +318,7 @@ public class PlayersServlet extends HttpServlet {
 		AbstractFeed.endDocument(dw, "player");
 	}
 	
-	static private void printPlayerQueue (HttpServletResponse resp, IPlayerLocal player) throws IOException, SAXException {
+	static private void printPlayerQueue (HttpServletResponse resp, IPlayerAbstract player) throws IOException, SAXException {
 		resp.setContentType("text/xml;charset=utf-8");
 		DataWriter dw = AbstractFeed.startDocument(resp.getWriter(), "queue");
 		
@@ -267,44 +378,31 @@ public class PlayersServlet extends HttpServlet {
 			String trackLink = null;
 			String filename;
 			String filepath;
-			String hashcode;
-			String enabled;
-			String missing;
-			String startcount;
-			String endcount;
 			if (currentItem != null && currentItem.item != null) {
 				trackLink = URLEncoder.encode(currentItem.item.getFilepath(), "UTF-8");
-				
 				filename = currentItem.item.getTitle(); // FIXME This is a hack :s .
 				filepath = currentItem.item.getFilepath();
-				hashcode = currentItem.item.getHashcode() != null ? currentItem.item.getHashcode().toString(16) : NULL;
-				enabled = Boolean.toString(currentItem.item.isEnabled());
-				missing = Boolean.toString(currentItem.item.isMissing());
-				startcount = String.valueOf(currentItem.item.getStartCount());
-				endcount = String.valueOf(currentItem.item.getEndCount());
 			}
 			else {
 				filename = NULL;
 				filepath = NULL;
-				hashcode = NULL;
-				enabled = NULL;
-				missing = NULL;
-				startcount = NULL;
-				endcount = NULL;
 			}
 			
 			if (trackLink != null) AbstractFeed.addLink(dw, trackLink, "track");
 			
-			AbstractFeed.addElement(dw, "playposition", p.getCurrentPosition());
 			AbstractFeed.addElement(dw, "trackfile", filepath);
 			AbstractFeed.addElement(dw, "trackfilename", filename);
+			AbstractFeed.addElement(dw, "playposition", p.getCurrentPosition());
 			AbstractFeed.addElement(dw, "trackduration", p.getCurrentTrackDuration());
 			
-			AbstractFeed.addElement(dw, "trackhash", hashcode);
-			AbstractFeed.addElement(dw, "trackenabled", enabled);
-			AbstractFeed.addElement(dw, "trackmissing", missing);
-			AbstractFeed.addElement(dw, "trackstartcount", startcount);
-			AbstractFeed.addElement(dw, "trackendcount", endcount);
+			if (currentItem != null && currentItem.item != null) {
+				IMediaTrack item = currentItem.item;
+				if (item.getHashcode() != null) AbstractFeed.addElement(dw, "trackhash", item.getHashcode().toString(16));
+				AbstractFeed.addElement(dw, "trackenabled", Boolean.toString(currentItem.item.isEnabled()));
+				AbstractFeed.addElement(dw, "trackmissing", Boolean.toString(currentItem.item.isMissing()));
+				AbstractFeed.addElement(dw, "trackstartcount", String.valueOf(currentItem.item.getStartCount()));
+				AbstractFeed.addElement(dw, "trackendcount", String.valueOf(currentItem.item.getEndCount()));
+			}
 			
 			Map<Integer, String> mons = p.getMonitors();
 			if (mons != null) {
