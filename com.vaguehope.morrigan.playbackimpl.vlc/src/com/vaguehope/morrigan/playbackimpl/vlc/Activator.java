830798107627a269c595e57bb99abaa79b0e1dcd
はく <haku@vaguehope.com>
2011-10-01 15:50:27 +0100

    Migrate to vlcj 1.2.1 and make good.

--- com.vaguehope.morrigan.playbackimpl.vlc/src/com/vaguehope/morrigan/playbackimpl/vlc/Activator.java
+++ com.vaguehope.morrigan.playbackimpl.vlc/src/com/vaguehope/morrigan/playbackimpl/vlc/Activator.java
@@ -1,30 +1,74 @@
 package com.vaguehope.morrigan.playbackimpl.vlc;
 
+import java.io.File;
+import java.util.logging.Logger;
+
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
 import uk.co.caprica.vlcj.player.MediaPlayerFactory;
 
+import com.sun.jna.NativeLibrary;
 import com.vaguehope.morrigan.engines.playback.PlaybackEngineRegister;
 
 public class Activator implements BundleActivator {
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	public static final String PROP_VLCLIB = "morrigan.vlclib";
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	protected final static Logger logger = Logger.getLogger(Activator.class.getName());
+	
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 	
 	static private MediaPlayerFactory factory;
 	
 	@Override
 	public void start (BundleContext context) throws Exception {
-		factory = new MediaPlayerFactory();
+		createFactory();
 		PlaybackEngineRegister.registerFactory(context.getBundle().getSymbolicName(), new EngineFactory());
 	}
 	
 	@Override
 	public void stop (BundleContext context) throws Exception {
 		PlaybackEngineRegister.unregisterFactory(context.getBundle().getSymbolicName());
+		destroyFactory();
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	/* Possible switches:
+	 * -vvv (for debugging)
+	 * --no-video-title-show
+	 */
+	
+	private final static String[] FACTORY_ARGS = {
+		"--no-video-title-show"
+		};
+	
+	public static void createFactory () {
+		String vlclib = System.getProperty(PROP_VLCLIB);
+		if (vlclib != null) {
+			File f = new File(vlclib);
+			if (f.exists()) {
+				NativeLibrary.addSearchPath("vlc", vlclib);
+				logger.info("Native library 'vlc' search path added: " + f.getAbsolutePath());
+			}
+			else {
+				logger.warning("Native library 'vlc' specified via system property '"+PROP_VLCLIB+"' but not found: " + f.getAbsolutePath());
+			}
+		}
+		
+		factory = new MediaPlayerFactory(FACTORY_ARGS);
+	}
+	
+	public static void destroyFactory () {
 		factory.release();
 	}
 	
 	public static MediaPlayerFactory getFactory () {
+		if (factory == null) throw new IllegalStateException("Bundle is not active so factory does not exist.");
 		return factory;
 	}
 	
