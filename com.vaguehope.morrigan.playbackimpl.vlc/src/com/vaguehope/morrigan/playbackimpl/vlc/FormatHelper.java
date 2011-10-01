3702007bbe36b1a0febabe7fdda8062a57d5f802
はく <haku@vaguehope.com>
2011-10-01 01:47:42 +0100

    First attempt at VLC based playback engine.

---
+++ com.vaguehope.morrigan.playbackimpl.vlc/src/com/vaguehope/morrigan/playbackimpl/vlc/FormatHelper.java
@@ -0,0 +1,21 @@
+package com.vaguehope.morrigan.playbackimpl.vlc;
+
+public class FormatHelper {
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	private FormatHelper () { /* Unused */ }
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+	
+	static public boolean mightFileHaveVideo (String f) {
+		String ext = f.substring(f.lastIndexOf('.') + 1).toLowerCase();
+		for (String e : Constants.AUDIO_ONLY_FORMATS) {
+			if (e.equals(ext)) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+//	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+}
