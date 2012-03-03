a317d375e25d34b1418f48d5513ab45692ad2cc3
はく <haku@vaguehope.com>
2012-03-03 21:42:28 +0000

    Format.  Use ConcurrentHashMap.

--- com.vaguehope.morrigan.model/src/com/vaguehope/morrigan/model/factory/RecyclingFactory.java
+++ com.vaguehope.morrigan.model/src/com/vaguehope/morrigan/model/factory/RecyclingFactory.java
@@ -1,11 +1,11 @@
 package com.vaguehope.morrigan.model.factory;
 
 import java.lang.ref.WeakReference;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * T = what to make (product).
@@ -15,31 +15,31 @@ import java.util.Set;
  */
 public abstract class RecyclingFactory<T extends Object, K extends Object, P extends Object, S extends Throwable> {
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	
-	private Map<K, WeakReference<T>> cache = new HashMap<K, WeakReference<T>>();
+
+	private Map<K, WeakReference<T>> cache = new ConcurrentHashMap<K, WeakReference<T>>();
 	private final boolean allowRecycle;
-	
+
 	protected RecyclingFactory (boolean allowReuse) {
 		this.allowRecycle = allowReuse;
 	}
-	
+
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	
+
 	public synchronized T manufacture (K material) throws S {
 		return manufacture(material, null, false);
 	}
-	
+
 	public synchronized T manufacture (K material, boolean forceCompletlyNew) throws S {
 		return manufacture(material, null, forceCompletlyNew);
 	}
-	
+
 	public synchronized T manufacture (K material, P config) throws S {
 		return manufacture(material, config, false);
 	}
-	
+
 	public synchronized T manufacture (K material, P config, boolean forceCompletlyNew) throws S {
 		T ret = null;
-		
+
 		if (forceCompletlyNew) {
 			if (config == null) {
 				ret = makeNewProduct(material);
@@ -56,13 +56,13 @@ public abstract class RecyclingFactory<T extends Object, K extends Object, P ext
 					this.cache.remove(material);
 				}
 			}
-			
+
 			// If an object is found, check it is still valid.
 			if (ret != null && !isValidProduct(ret)) {
 				this.cache.remove(material);
 				ret = null;
 			}
-			
+
 			// If no reusable product found, make one.
 			// If we found one, but are not allowed to use it, return null.
 			if (ret == null) {
@@ -77,15 +77,15 @@ public abstract class RecyclingFactory<T extends Object, K extends Object, P ext
 				ret = null;
 			}
 		}
-		
+
 		return ret;
 	}
-	
+
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	
+
 	public synchronized void disposeAll () {
 		Set<Entry<K,WeakReference<T>>> products = new HashSet<Entry<K, WeakReference<T>>>(this.cache.entrySet());
-		
+
 		for (Entry<K, WeakReference<T>> entry : products) {
 			T product = entry.getValue().get();
 			if (product != null) {
@@ -94,28 +94,28 @@ public abstract class RecyclingFactory<T extends Object, K extends Object, P ext
 			}
 		}
 	}
-	
+
 	protected void disposeProduct (T product) {
 		throw new IllegalArgumentException("Not implemented.");
 	}
-	
+
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	
+
 	/**
-	 * @throws S  
+	 * @throws S
 	 */
 	protected T makeNewProduct (K material) throws S {
 		throw new IllegalArgumentException("Not implemented.");
 	}
-	
+
 	/**
-	 * @throws S  
+	 * @throws S
 	 */
 	protected T makeNewProduct (K material, P config) throws S {
 		throw new IllegalArgumentException("Not implemented.");
 	}
-	
+
 	protected abstract boolean isValidProduct (T product);
-	
+
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 }
