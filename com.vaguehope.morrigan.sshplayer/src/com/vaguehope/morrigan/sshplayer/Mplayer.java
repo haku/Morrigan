e223864ddc91153f2b357e950136e2cc50f662d9
はく <haku@vaguehope.com>
2012-05-24 21:50:42 +0100

    Use port-forwarding/jetty instead of ssh pipe for more reliable playback.

--- com.vaguehope.morrigan.sshplayer/src/com/vaguehope/morrigan/sshplayer/Mplayer.java
+++ com.vaguehope.morrigan.sshplayer/src/com/vaguehope/morrigan/sshplayer/Mplayer.java
@@ -1,8 +1,8 @@
 package com.vaguehope.morrigan.sshplayer;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Properties;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -16,21 +16,12 @@ import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 
 /**
- * References:
- * http://www.jcraft.com/jsch/examples/
+ * References: http://www.jcraft.com/jsch/examples/
  * http://www.mplayerhq.hu/DOCS/tech/slave.txt
- *
  */
 public class Mplayer extends Thread {
 
 	protected static final Logger LOG = Logger.getLogger(Mplayer.class.getName());
-
-	private static final String MPLAYER_CMD =
-			"export DISPLAY=:0 ;" +
-					" [ -e ~/.mnmpcmd ] || mkfifo ~/.mnmpcmd ;" +
-					" mplayer -input file=~/.mnmpcmd -cache 8192 -identify -fs - ;" +
-					" killall mplayer > /dev/null 2>&1 ;" +
-					" echo " + MplayerStatusReader.MORRIGAN_EOF;
 
 	private enum Commands {
 		TOGGLE_PAUSED
@@ -55,7 +46,7 @@ public class Mplayer extends Thread {
 	public void run () {
 		if (!this.running.compareAndSet(false, true)) throw new IllegalStateException("Already started.");
 		try {
-			runPlayback();
+			runPlaybackSession();
 		}
 		catch (Exception e) {
 			// TODO log failure to some form of player status.
@@ -94,63 +85,91 @@ public class Mplayer extends Thread {
 		return s == null ? -1 : s.getDuration();
 	}
 
-	protected void runPlayback () throws JSchException, IOException {
+	protected void runPlaybackSession () throws JSchException, IOException {
 		JSch jsch = new JSch();
 		Session session = jsch.getSession(this.mplayerHost.getUser(), this.mplayerHost.getHost(), this.mplayerHost.getPort());
-		java.util.Properties config = new java.util.Properties();
+		Properties config = new Properties();
 		config.put("StrictHostKeyChecking", "no");
 		session.setConfig(config);
 		session.setUserInfo(this.mplayerHost.getUserinfo());
 		session.connect();
 		try {
-			ChannelExec execCh = (ChannelExec) session.openChannel("exec");
-			execCh.setCommand(MPLAYER_CMD);
-			execCh.setInputStream(new FileInputStream(this.media)); // Let JSch copy the stream.
-			execCh.connect(CONNECT_TIMEOUT);
-
-			MplayerStatusReader statusReader = new MplayerStatusReader(execCh.getInputStream());
-			statusReader.start();
-			this.status.set(statusReader);
-
-			new StreamReader(execCh.getErrStream(), System.err).start();
-
-			while (!this.canceled.get()) {
-				if (statusReader.isFinished() || execCh.isEOF() || execCh.isClosed() || !execCh.isConnected()) {
-					break;
-				}
-				procCmd(session);
-				try {
-					Thread.sleep(1000);
-				}
-				catch (Exception ee) {/* Ignore. */}
-			}
-
-			try {
-				execCh.sendSignal("KILL");
-			}
-			catch (Exception e) {
-				LOG.log(Level.WARNING, "Exception trying to KILL remote process playing: " + this.media.getAbsolutePath() , e);
-			}
-			execCh.disconnect();
+			runStream(session);
 		}
 		finally {
 			session.disconnect();
 		}
 	}
 
-	public void procCmd (Session session) throws JSchException {
+	private void runStream (Session session) throws JSchException, IOException {
+		session.setPortForwardingR(34400, "127.0.0.1", 34400); // TODO auto find free port.
+		FileServer fs = new FileServer(this.media, 34400);
+		fs.start();
+		try {
+			runMplayer(session);
+		}
+		finally {
+			fs.stop();
+		}
+	}
+
+	private void runMplayer (Session session) throws JSchException, IOException {
+		ChannelExec execCh = (ChannelExec) session.openChannel("exec");
+		execCh.setCommand(makeMplayerCommand());
+		execCh.connect(CONNECT_TIMEOUT);
+
+		MplayerStatusReader statusReader = new MplayerStatusReader(execCh.getInputStream());
+		statusReader.start();
+		this.status.set(statusReader);
+
+		new StreamReader(execCh.getErrStream(), System.err).start();
+
+		while (!this.canceled.get()) {
+			if (statusReader.isFinished() || execCh.isEOF() || execCh.isClosed() || !execCh.isConnected()) {
+				break;
+			}
+			procCmd(session);
+			try {
+				Thread.sleep(1000);
+			}
+			catch (Exception e) {/* Ignore. */}
+		}
+
+		exec(session, "killall mplayer > /dev/null 2>&1"); // Very blunt.
+		execCh.disconnect();
+	}
+
+	private void procCmd (Session session) throws JSchException {
 		Commands c = this.cmd.poll();
 		if (c == null) return;
 		if (c == Commands.TOGGLE_PAUSED) {
-			// Must be a nicer way to do this.
-			ChannelExec cmdExCh = (ChannelExec) session.openChannel("exec");
-			cmdExCh.setCommand("echo pause > ~/.mnmpcmd");
-			cmdExCh.connect();
-			cmdExCh.disconnect();
+			exec(session, "echo pause > ~/.mnmpcmd"); // Must be a nicer way to do this.
 		}
 		else {
 			LOG.warning("Unknown command: " + c);
 		}
+	}
+
+	private String makeMplayerCommand () {
+		StringBuilder s = new StringBuilder("cd"); // Start in home directory.
+		s.append(" ; export DISPLAY=:0");
+		s.append(" ; if [[ ! -e .mnmpcmd ]] ; then mkfifo .mnmpcmd ; fi");
+		s.append(" ; mplayer -input file=.mnmpcmd -cache 131072 -identify -fs 'http://localhost:34400/")
+				.append(genericFileName(this.media)).append("'");
+		s.append(" ; echo ").append(MplayerStatusReader.MORRIGAN_EOF);
+		return s.toString();
+	}
+
+	private static void exec (Session session, String command) throws JSchException {
+		ChannelExec cmdExCh = (ChannelExec) session.openChannel("exec");
+		cmdExCh.setCommand(command);
+		cmdExCh.connect();
+		cmdExCh.disconnect();
+	}
+
+	private static String genericFileName (File file) {
+		String n = file.getName();
+		return "file" + n.substring(n.lastIndexOf("."));
 	}
 
 }
