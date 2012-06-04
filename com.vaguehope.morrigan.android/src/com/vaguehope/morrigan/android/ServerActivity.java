54dd4655be6f7c440d830d5ed5e79e55d5ec7075
はく <haku@vaguehope.com>
2012-06-04 13:26:39 +0100

    add servers list side bar to server activity.

--- com.vaguehope.morrigan.android/src/com/vaguehope/morrigan/android/ServerActivity.java
+++ com.vaguehope.morrigan.android/src/com/vaguehope/morrigan/android/ServerActivity.java
@@ -28,6 +28,8 @@ import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.vaguehope.morrigan.android.layouts.SidebarLayout;
+import com.vaguehope.morrigan.android.layouts.SidebarLayout.SidebarListener;
 import com.vaguehope.morrigan.android.model.Artifact;
 import com.vaguehope.morrigan.android.model.ArtifactList;
 import com.vaguehope.morrigan.android.model.ArtifactListAdaptor;
@@ -38,6 +40,7 @@ import com.vaguehope.morrigan.android.model.PlayerReference;
 import com.vaguehope.morrigan.android.model.PlayerStateList;
 import com.vaguehope.morrigan.android.model.PlayerStateListChangeListener;
 import com.vaguehope.morrigan.android.model.ServerReference;
+import com.vaguehope.morrigan.android.model.ServerReferenceList;
 import com.vaguehope.morrigan.android.modelimpl.ArtifactListAdaptorImpl;
 import com.vaguehope.morrigan.android.modelimpl.ArtifactListGroupImpl;
 import com.vaguehope.morrigan.android.state.ConfigDb;
@@ -46,24 +49,25 @@ import com.vaguehope.morrigan.android.tasks.GetPlayersTask;
 
 public class ServerActivity extends Activity implements PlayerStateListChangeListener, MlistStateListChangeListener {
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	
+
 	public static final String SERVER_ID = "serverId"; // int.
-	
+
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	
+
 	ConfigDb configDb;
 	ServerReference serverReference = null;
+	private SidebarLayout sidebarLayout;
 	ArtifactListAdaptor<ArtifactList> artifactListAdaptor;
 	private ArtifactListGroupImpl artifactListImpl;
-	
+
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	
+
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	public void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		this.configDb = new ConfigDb(this);
-		
+
 		Bundle extras = getIntent().getExtras();
 		int serverId = extras.getInt(SERVER_ID, -1);
 		if (serverId >= 0) {
@@ -73,57 +77,88 @@ public class ServerActivity extends Activity implements PlayerStateListChangeLis
 			finish();
 		}
 		this.setTitle(this.serverReference.getName());
-		
+
 		// TODO check return value.
 		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
-		
+
 		setContentView(R.layout.server);
 		wireGui();
 	}
-	
+
 	@Override
-	protected void onStart() {
+	protected void onStart () {
 		super.onStart();
-		
+
 		refresh();
 	}
-	
+
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 //	GUI methods.
-	
+
 	private void wireGui () {
+		this.sidebarLayout = (SidebarLayout) findViewById(R.id.serverLayout);
+		this.sidebarLayout.setListener(this.sidebarListener);
+
 		this.artifactListAdaptor = new ArtifactListAdaptorImpl<ArtifactList>(this, R.layout.simplelistrow);
 		this.artifactListImpl = new ArtifactListGroupImpl();
 		this.artifactListAdaptor.setInputData(this.artifactListImpl);
-		
+
 		ListView lstServers = (ListView) findViewById(R.id.lstServer);
 		lstServers.setAdapter(this.artifactListAdaptor);
 		lstServers.setOnItemClickListener(this.artifactsListCickListener);
-		
-		ImageButton cmd;
-		
-		cmd = (ImageButton) findViewById(R.id.btnRefresh);
-		cmd.setOnClickListener(new BtnRefresh_OnClick());
+
+		ImageButton btnRefresh = (ImageButton) findViewById(R.id.btnRefresh);
+		btnRefresh.setOnClickListener(new BtnRefresh_OnClick());
+
+		ImageButton btnSidebar = (ImageButton) findViewById(R.id.btnSidebar);
+		btnSidebar.setOnClickListener(new SidebarLayout.ToggleSidebarListener(this.sidebarLayout));
+
+		ListView lstSidebar = (ListView) findViewById(R.id.lstSidebar);
+		ArtifactListAdaptorImpl<ServerReferenceList> sidebarListAdapter = new ArtifactListAdaptorImpl<ServerReferenceList>(this, R.layout.simplelistrow);
+		sidebarListAdapter.setInputData(this.configDb);
+		lstSidebar.setAdapter(sidebarListAdapter);
+//		lstServers.setOnItemClickListener(this.serversListCickListener);
+//		lstServers.setOnCreateContextMenuListener(this.serversContextMenuListener);
 	}
-	
+
 	private OnItemClickListener artifactsListCickListener = new OnItemClickListener() {
 		@Override
-		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+		public void onItemClick (AdapterView<?> parent, View view, int position, long id) {
 			Artifact item = ServerActivity.this.artifactListAdaptor.getInputData().getArtifactList().get(position);
 			showArtifactActivity(item);
 		}
 	};
-	
+
 	class BtnRefresh_OnClick implements OnClickListener {
 		@Override
-		public void onClick(View v) {
+		public void onClick (View v) {
 			refresh();
 		}
 	}
-	
+
+	@Override
+	public void onBackPressed () {
+		if (!this.sidebarLayout.closeSidebar()) finish();
+	}
+
+	private final SidebarListener sidebarListener = new SidebarListener() {
+
+		@Override
+		public boolean onContentTouchedWhenOpening (SidebarLayout sidebar) {
+			sidebar.closeSidebar();
+			return true;
+		}
+
+		@Override
+		public void onSidebarOpened (SidebarLayout sidebar) {/* Unused. */}
+
+		@Override
+		public void onSidebarClosed (SidebarLayout sidebar) {/* Unused. */}
+	};
+
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 //	Commands - to be called on the UI thread.
-	
+
 	protected void showArtifactActivity (Artifact item) {
 		if (item instanceof PlayerReference) {
 			PlayerReference pr = (PlayerReference) item;
@@ -134,17 +169,17 @@ public class ServerActivity extends Activity implements PlayerStateListChangeLis
 			showArtifactActivity(mr);
 		}
 		else {
-			Toast.makeText(this, "TODO: open type '"+item.getClass().getName()+"' desu~", Toast.LENGTH_LONG).show();
+			Toast.makeText(this, "TODO: open type '" + item.getClass().getName() + "' desu~", Toast.LENGTH_LONG).show();
 		}
 	}
-	
+
 	protected void showArtifactActivity (PlayerReference item) {
 		Intent intent = new Intent(getApplicationContext(), PlayerActivity.class);
 		intent.putExtra(MlistActivity.SERVER_ID, this.serverReference.getId());
 		intent.putExtra(PlayerActivity.PLAYER_ID, item.getPlayerId());
 		startActivity(intent);
 	}
-	
+
 	protected void showArtifactActivity (MlistReference item) {
 		Intent intent = new Intent(getApplicationContext(), MlistActivity.class);
 		intent.putExtra(MlistActivity.SERVER_ID, this.serverReference.getId());
@@ -152,17 +187,17 @@ public class ServerActivity extends Activity implements PlayerStateListChangeLis
 		intent.putExtra(MlistActivity.QUERY, "*"); // Default to showing results of wild-card search.
 		startActivity(intent);
 	}
-	
+
 	protected void refresh () {
 		new GetPlayersTask(this, this.serverReference, this).execute();
 		new GetMlistsTask(this, this.serverReference, this).execute();
 	}
-	
+
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 //	UI updating.
-	
+
 	@Override
-	public void onPlayersChange(PlayerStateList playersState) {
+	public void onPlayersChange (PlayerStateList playersState) {
 		if (playersState == null) {
 			finish(); // TODO show a msg here? Retry / Fail dlg?
 		}
@@ -171,9 +206,9 @@ public class ServerActivity extends Activity implements PlayerStateListChangeLis
 			this.artifactListAdaptor.notifyDataSetChanged();
 		}
 	}
-	
+
 	@Override
-	public void onMlistsChange(MlistStateList mlistsState) {
+	public void onMlistsChange (MlistStateList mlistsState) {
 		if (mlistsState == null) {
 			finish(); // TODO show a msg here? Retry / Fail dlg?
 		}
@@ -182,6 +217,6 @@ public class ServerActivity extends Activity implements PlayerStateListChangeLis
 			this.artifactListAdaptor.notifyDataSetChanged();
 		}
 	}
-	
+
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 }
