267e2c22794ba0758153bcfb0a85d8105e27ba84
はく <haku@vaguehope.com>
2013-01-15 21:40:03 +0000

    redundant modifier.

--- com.vaguehope.morrigan.util.httpclient/src/com/vaguehope/morrigan/util/httpclient/HttpStreamHandler.java
+++ com.vaguehope.morrigan.util.httpclient/src/com/vaguehope/morrigan/util/httpclient/HttpStreamHandler.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public interface HttpStreamHandler {
-	
-	public void handleStream (InputStream is) throws IOException, HttpStreamHandlerException;
-	
+
+	void handleStream (InputStream is) throws IOException, HttpStreamHandlerException;
+
 }
