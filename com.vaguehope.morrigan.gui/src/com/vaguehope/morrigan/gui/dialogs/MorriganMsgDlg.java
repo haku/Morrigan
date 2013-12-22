fd2ba380686374fecd93d4f2ce71a0db17ae43e8
はく <haku@vaguehope.com>
2013-12-22 10:13:50 +0000

    whitespace/autoformat.

--- com.vaguehope.morrigan.gui/src/com/vaguehope/morrigan/gui/dialogs/MorriganMsgDlg.java
+++ com.vaguehope.morrigan.gui/src/com/vaguehope/morrigan/gui/dialogs/MorriganMsgDlg.java
@@ -9,38 +9,38 @@ import com.vaguehope.morrigan.util.ErrorHelper;
 
 public class MorriganMsgDlg extends MessageDialog {
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	
+
 	public static final String[] YESNO = {"Yes", "No"};
 	public static final String[] COPYCONTINUE = {"Copy", "Continue"};
-	
+
 	private Throwable throwable = null;
-	
-	public MorriganMsgDlg(String dialogMessage) {
+
+	public MorriganMsgDlg(final String dialogMessage) {
 		super(
-				Display.getCurrent().getActiveShell(), 
-				"Morrigan", null, 
-				dialogMessage, 
+				Display.getCurrent().getActiveShell(),
+				"Morrigan", null,
+				dialogMessage,
 				MessageDialog.INFORMATION, new String[] {"Continue"}, 0);
 	}
-	
-	public MorriganMsgDlg(String dialogMessage, String[] answers) {
+
+	public MorriganMsgDlg(final String dialogMessage, final String[] answers) {
 		super(
-				Display.getCurrent().getActiveShell(), 
-				"Morrigan", null, 
-				dialogMessage, 
+				Display.getCurrent().getActiveShell(),
+				"Morrigan", null,
+				dialogMessage,
 				MessageDialog.INFORMATION, answers, 0);
 	}
-	
-	public MorriganMsgDlg(Throwable t) {
+
+	public MorriganMsgDlg(final Throwable t) {
 		super(
-				Display.getCurrent().getActiveShell(), 
-				"Morrigan", null, 
-				ErrorHelper.getCauseTrace(t), 
+				Display.getCurrent().getActiveShell(),
+				"Morrigan", null,
+				ErrorHelper.getCauseTrace(t),
 				MessageDialog.ERROR, COPYCONTINUE, 0);
-		
+
 		this.throwable = t;
 	}
-	
+
 	@Override
 	public int open() {
 		int open;
@@ -50,14 +50,14 @@ public class MorriganMsgDlg extends MessageDialog {
 			e.printStackTrace();
 			return -1;
 		}
-		
+
 		if (this.throwable!=null && open==OK) {
 			this.throwable.printStackTrace();
 			ClipboardHelper.setText(ErrorHelper.getStackTrace(this.throwable));
 		}
-		
+
 		return open;
 	}
-	
+
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 }
