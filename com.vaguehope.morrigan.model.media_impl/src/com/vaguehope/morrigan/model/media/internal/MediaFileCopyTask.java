af7058018de1847dd88b66cee8a09152c5a256ad
haku <git@haku.me>
2014-07-24 17:43:37 +0100

    Fix MediaFileCopyTask not correctly reporting that the task had started.

--- com.vaguehope.morrigan.model.media_impl/src/com/vaguehope/morrigan/model/media/internal/MediaFileCopyTask.java
+++ com.vaguehope.morrigan.model.media_impl/src/com/vaguehope/morrigan/model/media/internal/MediaFileCopyTask.java
@@ -10,60 +10,56 @@ import com.vaguehope.morrigan.tasks.TaskEventListener;
 import com.vaguehope.morrigan.tasks.TaskResult;
 import com.vaguehope.morrigan.tasks.TaskResult.TaskOutcome;
 
-
-
 public class MediaFileCopyTask<T extends IMediaItem> implements MorriganTask {
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	
+
 	private final IMediaItemList<T> mediaItemList;
 	private final List<T> mediaSelection;
 	private final File targetDirectory;
-	
-	public MediaFileCopyTask (IMediaItemList<T> mediaItemList, List<T> mediaSelection, File targetDirectory) {
+
+	public MediaFileCopyTask (final IMediaItemList<T> mediaItemList, final List<T> mediaSelection, final File targetDirectory) {
 		this.mediaItemList = mediaItemList;
 		this.mediaSelection = mediaSelection;
 		this.targetDirectory = targetDirectory;
 	}
-	
+
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	
+
 	@Override
-	public String getTitle() {
+	public String getTitle () {
 		return "Copying " + this.mediaSelection.size() + " files";
 	}
 
 	@Override
-	public TaskResult run(TaskEventListener taskEventListener) {
+	public TaskResult run (final TaskEventListener taskEventListener) {
 		TaskResult ret;
-		
+
+		taskEventListener.onStart();
 		try {
 			taskEventListener.beginTask("Copying", this.mediaSelection.size());
-			
-			for (T mi : this.mediaSelection) {
+
+			for (final T mi : this.mediaSelection) {
 				taskEventListener.subTask(mi.getTitle());
 				this.mediaItemList.copyItemFile(mi, this.targetDirectory);
 				taskEventListener.worked(1);
-				
-				if (taskEventListener.isCanceled()) {
-					break;
-				}
+
+				if (taskEventListener.isCanceled()) break;
 			}
-			
+
 			if (taskEventListener.isCanceled()) {
 				ret = new TaskResult(TaskOutcome.CANCELED);
-			} else {
+			}
+			else {
 				ret = new TaskResult(TaskOutcome.SUCCESS);
 			}
-			
-			taskEventListener.done();
 		}
-		catch (Exception e) {
+		catch (final Exception e) {
 			ret = new TaskResult(TaskOutcome.FAILED, "Failed to copy all files.", e);
 		}
-		
+
+		taskEventListener.done();
 		return ret;
-		
 	}
-	
+
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 }
