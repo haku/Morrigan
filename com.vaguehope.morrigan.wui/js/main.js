30ae2bde48c047478bbed73be982ae13a62bcdd3
haku <git@haku.me>
2015-12-29 09:31:25 +0000

    Hostname in WUI title.

---
+++ com.vaguehope.morrigan.wui/js/main.js
@@ -0,0 +1,22 @@
+(function() {
+
+  $(document).ready(function() {
+    updatePageTitle();
+  });
+
+  function updatePageTitle() {
+    $.ajax({
+      type: 'GET',
+      cache: false,
+      url: '/hostinfo',
+      dataType: 'xml',
+      success: function(xml) {
+        var hostName = $(xml).find('hostname').text();
+        if (hostName && hostName.length > 0) {
+          document.title = hostName + "'s Morrigan";
+        }
+      }
+    });
+  }
+
+})();
