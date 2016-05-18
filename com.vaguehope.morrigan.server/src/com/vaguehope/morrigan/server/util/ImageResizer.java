28fa8412ac68164a4a2ac2a84ad7936552b45c2a
haku <git@haku.me>
2016-05-18 15:26:09 +0100

    Fixes in ImageResizer: not working for PNGs, leaving tmp files on error.

--- com.vaguehope.morrigan.server/src/com/vaguehope/morrigan/server/util/ImageResizer.java
+++ com.vaguehope.morrigan.server/src/com/vaguehope/morrigan/server/util/ImageResizer.java
@@ -1,6 +1,7 @@
 package com.vaguehope.morrigan.server.util;
 
 import java.awt.AlphaComposite;
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
@@ -23,6 +24,8 @@ public class ImageResizer {
 	 */
 	private static final float QUALITY = 0.9f;
 
+	private static final Object[] LOCK = new Object[0];
+
 	public static File resizeFile (final File inF, final Integer size) throws IOException {
 		if (!inF.exists()) throw new IllegalArgumentException("File does not exist: " + inF.getAbsolutePath());
 		if (size < 16 || size > 1000) throw new IllegalArgumentException("Invalid size: " + size);
@@ -30,6 +33,12 @@ public class ImageResizer {
 		final File outF = new File(Config.getResizedDir(), ChecksumHelper.md5String(inF.getAbsolutePath()) + "_" + size);
 		if (outF.exists() && outF.lastModified() > inF.lastModified()) return outF;
 
+		synchronized (LOCK) {
+			return scaleImageToFile(inF, size, outF);
+		}
+	}
+
+	private static File scaleImageToFile (final File inF, final Integer size, final File outF) throws IOException {
 		final BufferedImage inImg = ImageIO.read(inF);
 
 		if (inImg.getWidth() < 1 || inImg.getHeight() < 1) throw new IllegalArgumentException("Image too small: " + inF.getAbsolutePath());
@@ -54,15 +63,14 @@ public class ImageResizer {
 	}
 
 	private static BufferedImage scaleImage (final BufferedImage inImg, final int width, final int height) {
-		final int inType = inImg.getType() == 0 ? BufferedImage.TYPE_INT_ARGB : inImg.getType();
-		final BufferedImage outImg = new BufferedImage(width, height, inType);
+		final BufferedImage outImg = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 		final Graphics2D g = outImg.createGraphics();
 		try {
 			g.setComposite(AlphaComposite.Src);
 			g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
 			g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
 			g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-			g.drawImage(inImg, 0, 0, width, height, null);
+			g.drawImage(inImg, 0, 0, width, height, Color.BLACK, null);
 		}
 		finally {
 			g.dispose();
@@ -72,8 +80,13 @@ public class ImageResizer {
 
 	private static void writeImageViaTmpFile (final BufferedImage outImg, final File f) throws IOException {
 		final File ftmp = new File(f.getAbsolutePath() + ".tmp");
-		writeImageToFile(outImg, ftmp);
-		if (!ftmp.renameTo(f)) throw new IOException("Failed to rename '" + ftmp.getAbsolutePath() + "' to '" + f.getAbsolutePath() + "'.");
+		try {
+			writeImageToFile(outImg, ftmp);
+			if (!ftmp.renameTo(f)) throw new IOException("Failed to rename '" + ftmp.getAbsolutePath() + "' to '" + f.getAbsolutePath() + "'.");
+		}
+		finally {
+			if (ftmp.exists()) ftmp.delete();
+		}
 	}
 
 	private static void writeImageToFile (final BufferedImage outImg, final File f) throws IOException {
