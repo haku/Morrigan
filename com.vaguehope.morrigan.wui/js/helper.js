2e4404562e55dd1830a33c0d7b9e53972a8a3b0e
haku <git@haku.me>
2016-05-20 18:17:50 +0100

    String.prototype.startsWith polyfill.

--- com.vaguehope.morrigan.wui/js/helper.js
+++ com.vaguehope.morrigan.wui/js/helper.js
@@ -10,3 +10,11 @@ ErrorHelper = {};
 jQuery.fn.setVisibility = function(visibility) {
   return this.css('visibility', visibility ? 'visible' : 'hidden');
 };
+
+// Polyfill https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/String/startsWith
+if (!String.prototype.startsWith) {
+    String.prototype.startsWith = function(searchString, position){
+      position = position || 0;
+      return this.substr(position, searchString.length) === searchString;
+  };
+}
