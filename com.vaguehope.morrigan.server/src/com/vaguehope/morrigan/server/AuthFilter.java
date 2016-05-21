e604bf62f38d633de918e73205d075ebf45e3ad8
haku <git@haku.me>
2016-05-21 10:20:46 +0100

    Autoformat AuthFilter.

--- com.vaguehope.morrigan.server/src/com/vaguehope/morrigan/server/AuthFilter.java
+++ com.vaguehope.morrigan.server/src/com/vaguehope/morrigan/server/AuthFilter.java
@@ -18,34 +18,34 @@ import com.vaguehope.morrigan.util.httpclient.Http;
 
 public class AuthFilter implements Filter {
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	
+
 	private static final Logger logger = Logger.getLogger(AuthFilter.class.getName());
-	
+
 	private final AuthChecker authChecker;
-	
+
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	
-	public AuthFilter (AuthChecker authChecker) {
+
+	public AuthFilter (final AuthChecker authChecker) {
 		this.authChecker = authChecker;
 	}
-	
+
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 	@Override
-	public void init (FilterConfig arg0) throws ServletException {
+	public void init (final FilterConfig arg0) throws ServletException {
 		// Unused.
 	}
-	
+
 	@Override
 	public void destroy () {
 		// Unused.
 	}
-	
+
 	@Override
-	public void doFilter (ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		HttpServletRequest req = (HttpServletRequest) request;
-		HttpServletResponse resp = (HttpServletResponse) response;
-		
+	public void doFilter (final ServletRequest request, final ServletResponse response, final FilterChain chain) throws IOException, ServletException {
+		final HttpServletRequest req = (HttpServletRequest) request;
+		final HttpServletResponse resp = (HttpServletResponse) response;
+
 		// Request basic auth.
 		String authHeader64 = req.getHeader(Http.HEADER_AUTHORISATION);
 		if (authHeader64 == null
@@ -55,34 +55,34 @@ public class AuthFilter implements Filter {
 			send401(resp);
 			return;
 		}
-		
+
 		// Verify password.
 		authHeader64 = authHeader64.substring(Http.HEADER_AUTHORISATION_PREFIX.length());
-		String authHeader = B64Code.decode(authHeader64, null);
-		int x = authHeader.indexOf(":");
-		String user = authHeader.substring(0, x);
-		String pass = authHeader.substring(x + 1);
+		final String authHeader = B64Code.decode(authHeader64, null);
+		final int x = authHeader.indexOf(":");
+		final String user = authHeader.substring(0, x);
+		final String pass = authHeader.substring(x + 1);
 		if (user == null || pass == null || user.isEmpty() || pass.isEmpty() || !checkUser(user, pass)) {
 			logger.fine("Auth failed: user=" + user + " pass=" + pass);
 			send401(resp);
 			return;
 		}
-		
+
 		chain.doFilter(request, response);
 	}
-	
+
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	
-	private boolean checkUser (@SuppressWarnings("unused") String user, String pass) {
+
+	private boolean checkUser (@SuppressWarnings("unused") final String user, final String pass) {
 		return this.authChecker.verifyAuth(pass);
 	}
-	
+
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-	
-	private static void send401 (HttpServletResponse resp) throws IOException {
+
+	private static void send401 (final HttpServletResponse resp) throws IOException {
 		resp.setHeader(Http.WWW_AUTHENTICATE, Http.BASIC_REALM);
 		resp.sendError(javax.servlet.http.HttpServletResponse.SC_UNAUTHORIZED);
 	}
-	
+
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 }
