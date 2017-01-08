c03dbca7e6771d141b5461ed0f96053b033559f3
haku <git@haku.me>
2017-01-08 21:16:38 +0000

    Make AudioOnlyM4aTranscode able to use stream-copy.

--- com.vaguehope.morrigan.player/src/com/vaguehope/morrigan/player/transcode/AudioOnlyM4aTranscode.java
+++ com.vaguehope.morrigan.player/src/com/vaguehope/morrigan/player/transcode/AudioOnlyM4aTranscode.java
@@ -1,7 +1,9 @@
 package com.vaguehope.morrigan.player.transcode;
 
 import java.io.File;
-
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import com.vaguehope.morrigan.model.media.IMediaItem;
 import com.vaguehope.morrigan.util.MimeType;
 import com.vaguehope.morrigan.util.StringHelper;
@@ -13,25 +15,50 @@ public class AudioOnlyM4aTranscode extends TranscodeProfile {
 	}
 
 	@Override
-	public String[] transcodeCmd (final File outputFile) {
+	public String[] transcodeCmd (final File outputFile) throws IOException {
 		final String ext = "." + getMimeType().getExt();
 		if (!StringHelper.endsWithIgnoreCase(outputFile.getName(), ext)) {
 			throw new IllegalArgumentException("Output file must end with " + ext + ": " + outputFile.getName());
 		}
 
-		return new String[]{
-				"ffmpeg",
-				"-hide_banner",
-				"-nostats",
-				"-y", // Overwrite output files.
-				// "-seekable", "1", // Only needed when input is HTTP and not a local file.
-				"-fflags", "+genpts",
-				"-threads", "0",
-				"-i", getItem().getFile().getAbsolutePath(),
-				"-vn",
-				"-c:a", "libfdk_aac",
-				"-vbr", "5", // https://trac.ffmpeg.org/wiki/Encode/AAC#fdk_vbr
-				outputFile.getAbsolutePath()};
+		final List<String> cmd = new ArrayList<String>();
+
+		cmd.add("ffmpeg");
+		cmd.add("-hide_banner");
+		cmd.add("-nostats");
+		cmd.add("-y"); // Overwrite output files.
+		// "-seekable", "1", // Only needed when input is HTTP and not a local file.
+
+		cmd.add("-fflags");
+		cmd.add("+genpts");
+
+		cmd.add("-threads");
+		cmd.add("0");
+
+		cmd.add("-i");
+		cmd.add(getItem().getFile().getAbsolutePath());
+
+		cmd.add("-vn");
+
+		if (Ffprobe.streamCodecs(getItem().getFile()).contains("aac")) {
+			cmd.add("-acodec");
+			cmd.add("copy");
+		}
+		else {
+			cmd.add("-c:a");
+			cmd.add("libfdk_aac");
+
+			// https://trac.ffmpeg.org/wiki/Encode/AAC#fdk_vbr
+			cmd.add("-vbr");
+			cmd.add("5");
+		}
+
+		cmd.add("-movflags");
+		cmd.add("+faststart");
+
+		cmd.add(outputFile.getAbsolutePath());
+
+		return cmd.toArray(new String[cmd.size()]);
 	}
 
 }
