93e3e5b689ea4d17c9f0ec3a4f6aa4e530d3af48
haku <git@haku.me>
2017-07-14 22:28:30 +0100

    Name task threads.

--- com.vaguehope.morrigan.tasks/src/com/vaguehope/morrigan/tasks/TasksActivator.java
+++ com.vaguehope.morrigan.tasks/src/com/vaguehope/morrigan/tasks/TasksActivator.java
@@ -6,6 +6,8 @@ import java.util.concurrent.Executors;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
+import com.vaguehope.morrigan.util.DaemonThreadFactory;
+
 public class TasksActivator implements BundleActivator {
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
@@ -14,13 +16,13 @@ public class TasksActivator implements BundleActivator {
 //	- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 	@Override
-	public void start (BundleContext context) throws Exception {
-		this.executor = Executors.newCachedThreadPool();
+	public void start (final BundleContext context) throws Exception {
+		this.executor = Executors.newCachedThreadPool(new DaemonThreadFactory("tsk"));
 		context.registerService(AsyncTasksRegister.class, new AsyncTasksRegisterImpl(this.executor), null);
 	}
 
 	@Override
-	public void stop (BundleContext context) throws Exception {
+	public void stop (final BundleContext context) throws Exception {
 		this.executor.shutdown(); // TODO more controlled wait for tasks to be cancelled.
 		this.executor = null;
 	}
