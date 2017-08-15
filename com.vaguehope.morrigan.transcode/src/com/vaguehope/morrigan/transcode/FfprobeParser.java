594150a71f8ac18d2e594989801e00bcbf4332d1
haku <git@haku.me>
2017-08-15 10:04:07 +0100

    Support parsing .tags.DURATION from ffprobe output.

--- com.vaguehope.morrigan.transcode/src/com/vaguehope/morrigan/transcode/FfprobeParser.java
+++ com.vaguehope.morrigan.transcode/src/com/vaguehope/morrigan/transcode/FfprobeParser.java
@@ -3,12 +3,19 @@ package com.vaguehope.morrigan.transcode;
 import java.util.LinkedHashSet;
 import java.util.Locale;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.vaguehope.morrigan.util.Listener;
 import com.vaguehope.morrigan.util.MnLogger;
 import com.vaguehope.morrigan.util.StringHelper;
 
 public class FfprobeParser implements Listener<String> {
+
+	/**
+	 * 00:03:57.196000000
+	 */
+	private static final Pattern DURATION_PATTERN = Pattern.compile("^([0-9]+):([0-9]+):([0-9]+).([0-9]+)$");
 
 	private static final MnLogger LOG = MnLogger.make(FfprobeParser.class);
 
@@ -27,6 +34,18 @@ public class FfprobeParser implements Listener<String> {
 		else if (parts[0].endsWith(".profile")) {
 			this.profiles.add(StringHelper.removeEndQuotes(parts[1]).toLowerCase(Locale.ENGLISH));
 		}
+		else if (parts[0].endsWith(".tags.DURATION")) { // streams.stream.0.tags.DURATION="00:03:57.196000000"
+			try {
+				final String val = StringHelper.removeEndQuotes(parts[1]);
+				if (!"N/A".equalsIgnoreCase(val)) {
+					final long millis = parseDurationStringToMillis(val);
+					if (this.durationMillis == null || millis > this.durationMillis) this.durationMillis = millis;
+				}
+			}
+			catch (final NumberFormatException e) {
+				LOG.w("Failed to parse {}: {}", line, e.toString());
+			}
+		}
 		else if (parts[0].endsWith(".duration")) {
 			try {
 				final String val = StringHelper.removeEndQuotes(parts[1]);
@@ -40,6 +59,19 @@ public class FfprobeParser implements Listener<String> {
 				LOG.w("Failed to parse {}: {}", line, e.toString());
 			}
 		}
+	}
+
+	private static long parseDurationStringToMillis (final String val) {
+		final Matcher m = DURATION_PATTERN.matcher(val);
+		if (m.matches()) {
+			final int hours = Integer.parseInt(m.group(1));
+			final int minutes = Integer.parseInt(m.group(2));
+			final int seconds = Integer.parseInt(m.group(3));
+			final int totalSeconds = (hours * 60 * 60) + (minutes * 60) + seconds;
+			return totalSeconds * 1000L;
+
+		}
+		return -1;
 	}
 
 	public FfprobeInfo build () {
