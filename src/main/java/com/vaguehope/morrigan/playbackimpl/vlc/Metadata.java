25c7b172d7359b1ca73309ef54d59d23950963db
haku <git@haku.me>
2022-02-25 22:07:25 +0000

    Fix hanging when file metadata can not be read.

--- src/main/java/com/vaguehope/morrigan/playbackimpl/vlc/Metadata.java
+++ src/main/java/com/vaguehope/morrigan/playbackimpl/vlc/Metadata.java
@@ -1,18 +1,22 @@
 package com.vaguehope.morrigan.playbackimpl.vlc;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 
 import uk.co.caprica.vlcj.factory.MediaPlayerFactory;
 import uk.co.caprica.vlcj.media.Media;
 import uk.co.caprica.vlcj.media.MediaEventAdapter;
 import uk.co.caprica.vlcj.media.MediaEventListener;
+import uk.co.caprica.vlcj.media.MediaParsedStatus;
 
 class Metadata {
 
+	private static final int READ_TIMEOUT_SECONDS = 5;
+
 	public static long getDurationMilliseconds(final MediaPlayerFactory factory, final String mrl) throws InterruptedException {
 		final Media media = factory.media().newMedia(mrl);
-		final AtomicLong ret = new AtomicLong();
+		final AtomicLong ret = new AtomicLong(0L);
 		try {
 			final CountDownLatch latch = new CountDownLatch(1);
 			final MediaEventListener listener = new MediaEventAdapter() {
@@ -21,17 +25,28 @@ class Metadata {
 					ret.set(newDuration);
 					latch.countDown();
 				}
+
+				@Override
+				public void mediaParsedChanged(final Media m, final MediaParsedStatus newStatus) {
+					switch (newStatus) {
+					case DONE:
+					case FAILED:
+					case SKIPPED:
+					case TIMEOUT:
+						latch.countDown();
+						break;
+					default:
+						break;
+					}
+				}
 			};
 
 			try {
 				media.events().addMediaEventListener(listener);
 				if (media.parsing().parse()) {
-					latch.await();
-					return ret.get();
+					latch.await(READ_TIMEOUT_SECONDS, TimeUnit.SECONDS);
 				}
-				else {
-					return 0;
-				}
+				return ret.get();
 			}
 			finally {
 				media.events().removeMediaEventListener(listener);
