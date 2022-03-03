8797c28b36830832f3f0bed242544656112b12c3
haku <git@haku.me>
2022-03-03 18:57:01 +0000

    Change ProcessHelper to only destroy() proc if it does not exit in a reasonable time.

--- src/main/java/com/vaguehope/morrigan/util/ExceptionHelper.java
+++ src/main/java/com/vaguehope/morrigan/util/ExceptionHelper.java
@@ -25,4 +25,17 @@ public class ExceptionHelper {
 		}
 	}
 
+	public static String causeTrace(final Throwable t, final String sep) {
+		final StringBuilder sb = new StringBuilder();
+		boolean first = true;
+		Throwable c = t;
+		while (c != null) {
+			if (!first) sb.append(sep).append("caused by: ");
+			sb.append(String.valueOf(c));
+			c = c.getCause();
+			first = false;
+		}
+		return sb.toString();
+	}
+
 }
--- src/main/java/com/vaguehope/morrigan/util/Touch.java
+++ src/main/java/com/vaguehope/morrigan/util/Touch.java
@@ -3,17 +3,29 @@ package com.vaguehope.morrigan.util;
 import java.io.File;
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Touch {
+
+	private static final Logger LOG = LoggerFactory.getLogger(Touch.class);
 
 	private static Boolean isAvailable = null;
 
 	private static boolean isAvailable () {
 		if (isAvailable == null) {
 			try {
-				isAvailable = ProcessHelper.runAndWait("touch", "--version").size() > 0;
+				if (ProcessHelper.runAndWait("touch", "--version").size() > 0) {
+					isAvailable = true;
+				}
+				else {
+					isAvailable = false;
+					LOG.warn("touch --version returned no output.");
+				}
 			}
 			catch (final IOException e) {
 				isAvailable = false;
+				LOG.warn("touch command not available: {}", ExceptionHelper.causeTrace(e, " --> "));
 			}
 		}
 		return isAvailable.booleanValue();
