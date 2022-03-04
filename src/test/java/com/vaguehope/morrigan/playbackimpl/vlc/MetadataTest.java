7bf936b7d4a3fe1754bb5b88a404d44ca2e7067d
haku <git@haku.me>
2022-03-04 23:27:56 +0000

    @Ignore test with external dep.

--- src/test/java/com/vaguehope/morrigan/playbackimpl/vlc/MetadataTest.java
+++ src/test/java/com/vaguehope/morrigan/playbackimpl/vlc/MetadataTest.java
@@ -8,6 +8,7 @@ import java.io.FileNotFoundException;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.vaguehope.morrigan.Args;
@@ -28,6 +29,7 @@ public class MetadataTest {
 		this.factory.dispose();
 	}
 
+	@Ignore
 	@Test
 	public void itReadsFileDuration() throws Exception {
 		final File file = new File(System.getProperty("user.home") + "/media/testing/test.mp3");
