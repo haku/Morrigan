2ad49af46169a94d1a59908a6d2f882a8a24f76c
haku <git@haku.me>
2025-01-10 21:15:08 +0000

    Remove unused params from PlayerEventCache.

--- src/main/java/com/vaguehope/morrigan/dlna/players/PlayerEventCache.java
+++ src/main/java/com/vaguehope/morrigan/dlna/players/PlayerEventCache.java
@@ -13,35 +13,26 @@ public class PlayerEventCache implements PlayerEventListener {
 
 	private static final Logger LOG = LoggerFactory.getLogger(PlayerEventCache.class);
 
-	private volatile PlaybackOrder playbackOrder;
-	private volatile Transcode transcode;
-	private volatile PlayItem currentItem;
 	private volatile PlayState playState;
 	private volatile long position = -1;
 	private volatile int duration = -1;
 
 	@Override
-	public void playOrderChanged (final PlaybackOrder newPlaybackOrder) {
-		this.playbackOrder = newPlaybackOrder;
-	}
+	public void playOrderChanged(final PlaybackOrder newPlaybackOrder) {}
 
 	@Override
-	public void transcodeChanged (final Transcode newTranscode) {
-		this.transcode = newTranscode;
-	}
+	public void transcodeChanged(final Transcode newTranscode) {}
 
 	@Override
-	public void currentItemChanged (final PlayItem newItem) {
-		this.currentItem = newItem;
-	}
+	public void currentItemChanged(final PlayItem newItem) {}
 
 	@Override
-	public void playStateChanged (final PlayState newPlayState) {
+	public void playStateChanged(final PlayState newPlayState) {
 		this.playState = newPlayState;
 	}
 
 	@Override
-	public void positionChanged (final long newPosition, final int newDuration) {
+	public void positionChanged(final long newPosition, final int newDuration) {
 		this.position = newPosition;
 		this.duration = newDuration;
 	}
@@ -50,32 +41,20 @@ public class PlayerEventCache implements PlayerEventListener {
 	public void afterSeek() {}
 
 	@Override
-	public void onException (final Exception e) {
+	public void onException(final Exception e) {
 		// TODO propagate to the UI!
 		LOG.warn("Unhandled excpetion.", e);
 	}
 
-	public PlaybackOrder getPlaybackOrder () {
-		return this.playbackOrder;
-	}
-
-	public Transcode getTranscode () {
-		return this.transcode;
-	}
-
-	public PlayItem getCurrentItem () {
-		return this.currentItem;
-	}
-
-	public PlayState getPlayState () {
+	public PlayState getPlayState() {
 		return this.playState;
 	}
 
-	public long getPosition () {
+	public long getPosition() {
 		return this.position;
 	}
 
-	public int getDuration () {
+	public int getDuration() {
 		return this.duration;
 	}
 
