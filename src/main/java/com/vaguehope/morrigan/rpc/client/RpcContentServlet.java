ed5f29faeb4b13989b1c18697c538f5ff2b18556
haku <git@haku.me>
2025-01-14 16:09:55 +0000

    Kinda handle GRPC errors while proxying streaming media.

--- src/main/java/com/vaguehope/morrigan/rpc/client/RpcContentServlet.java
+++ src/main/java/com/vaguehope/morrigan/rpc/client/RpcContentServlet.java
@@ -26,6 +26,8 @@ import com.vaguehope.dlnatoad.rpc.MediaToadProto.ReadMediaRequest;
 import com.vaguehope.dlnatoad.rpc.MediaToadProto.ReadMediaRequest.Builder;
 import com.vaguehope.morrigan.player.contentproxy.ContentServer;
 
+import io.grpc.StatusRuntimeException;
+
 public class RpcContentServlet implements ContentServer {
 
 	private static final Logger LOG = LoggerFactory.getLogger(RpcContentServlet.class);
@@ -79,36 +81,43 @@ public class RpcContentServlet implements ContentServer {
 		final Builder rpcReq = ReadMediaRequest.newBuilder().setId(itemId);
 		if (rpcRanges != null && rpcRanges.size() > 0) rpcReq.addAllRange(rpcRanges);
 
-		final Iterator<ReadMediaReply> replies = stub
-				.withDeadlineAfter(15, TimeUnit.MINUTES)
-				.withOnReadyThreshold(1 * 1024 * 1024)  // default is 32kib.
-				.readMedia(rpcReq.build());
-		final ReadMediaReply first = replies.next();
+		try {
+			final Iterator<ReadMediaReply> replies = stub
+					.withDeadlineAfter(15, TimeUnit.MINUTES)
+					.withOnReadyThreshold(1 * 1024 * 1024)  // default is 32kib.
+					.readMedia(rpcReq.build());
+			final ReadMediaReply first = replies.next();
 
-		resp.setContentType(first.getMimeType());
+			resp.setContentType(first.getMimeType());
 
-		if (httpRanges != null && httpRanges.size() > 0) {
-			if (!first.hasRangeIndex()) {
-				resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "range_index missing from ReadMediaReply.");
-				LOG.warn("range_index missing from ReadMediaReply from {} for: {}", listId, itemId);
-				return;
+			if (httpRanges != null && httpRanges.size() > 0) {
+				if (!first.hasRangeIndex()) {
+					resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "range_index missing from ReadMediaReply.");
+					LOG.warn("range_index missing from ReadMediaReply from {} for: {}", listId, itemId);
+					return;
+				}
+
+				resp.setStatus(HttpServletResponse.SC_PARTIAL_CONTENT);
+				final InclusiveByteRange range = httpRanges.get(first.getRangeIndex());
+				resp.setContentLengthLong(range.getSize());
+				resp.setHeader(HttpHeader.ACCEPT_RANGES.asString(), "bytes");
+				resp.setHeader(HttpHeader.CONTENT_RANGE.asString(), range.toHeaderRangeString(first.getTotalFileLength()));
+			}
+			else {
+				resp.setContentLengthLong(first.getTotalFileLength());
 			}
 
-			resp.setStatus(HttpServletResponse.SC_PARTIAL_CONTENT);
-			final InclusiveByteRange range = httpRanges.get(first.getRangeIndex());
-			resp.setContentLengthLong(range.getSize());
-			resp.setHeader(HttpHeader.ACCEPT_RANGES.asString(), "bytes");
-			resp.setHeader(HttpHeader.CONTENT_RANGE.asString(), range.toHeaderRangeString(first.getTotalFileLength()));
+			@SuppressWarnings("resource")
+			final ServletOutputStream outputStream = resp.getOutputStream();
+			first.getContent().writeTo(outputStream);
+			while (replies.hasNext()) {
+				replies.next().getContent().writeTo(outputStream);
+			}
 		}
-		else {
-			resp.setContentLengthLong(first.getTotalFileLength());
-		}
-
-		@SuppressWarnings("resource")
-		final ServletOutputStream outputStream = resp.getOutputStream();
-		first.getContent().writeTo(outputStream);
-		while (replies.hasNext()) {
-			replies.next().getContent().writeTo(outputStream);
+		catch (final StatusRuntimeException e) {
+			if (!resp.isCommitted()) {
+				resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "RPC error: " + e.getStatus());
+			}
 		}
 	}
 
