haku/Morrigan

commit 761be744da9660c3fe5c10cfdc095fbc8015fde7

Make recording playback started/completed more consistent.

src/main/java/com/vaguehope/morrigan/dlna/players/GoalSeekingDlnaPlayer.java
@@ -541,7 +541,7 @@ public class GoalSeekingDlnaPlayer extends AbstractDlnaPlayer {
 		setCurrentPlayingParams(playingParams);
 		saveState();
 
-		this.eventQueue.add(new DlnaToPlay(item, playingParams, this));
+		this.eventQueue.add(new DlnaToPlay(item, playingParams, this.playbackRecorder));
 		this.eventQueue.add(PlayState.PLAYING);
 		setStateToReportExternally(PlayState.LOADING);
 

src/main/java/com/vaguehope/morrigan/dlna/players/AbstractDlnaPlayer.java
@@ -30,7 +30,6 @@ public abstract class AbstractDlnaPlayer extends AbstractPlayer {
 
 	protected final AvTransportActions avTransport;
 	protected final RenderingControlActions renderingControl;
-	protected final ScheduledExecutorService scheduledExecutor;
 	private final DlnaPlayingParamsFactory dlnaPlayingParamsFactory;
 
 	private final String uid;
@@ -46,12 +45,12 @@ public abstract class AbstractDlnaPlayer extends AbstractPlayer {
 			final ControlPoint controlPoint,
 			final RemoteService avTransportSvc,
 			final DlnaPlayingParamsFactory dlnaPlayingParamsFactory,
-			final ScheduledExecutorService scheduledExecutor,
+			final ScheduledExecutorService schEx,
 			final PlayerStateStorage playerStateStorage,
 			final Config config,
 			final AvTransportActions avTransportActions,
 			final RenderingControlActions renderingControlActions) {
-		super(UpnpHelper.idFromRemoteService(avTransportSvc), avTransportSvc.getDevice().getDetails().getFriendlyName(), register, playerStateStorage, config);
+		super(UpnpHelper.idFromRemoteService(avTransportSvc), avTransportSvc.getDevice().getDetails().getFriendlyName(), register, schEx, playerStateStorage, config);
 		this.controlPoint = controlPoint;
 		this.avTransportSvc = avTransportSvc;
 
@@ -76,7 +75,6 @@ public abstract class AbstractDlnaPlayer extends AbstractPlayer {
 		}
 
 		this.dlnaPlayingParamsFactory = dlnaPlayingParamsFactory;
-		this.scheduledExecutor = scheduledExecutor;
 		this.uid = UpnpHelper.remoteServiceUid(avTransportSvc);
 		addEventListener(this.playerEventCache);
 	}
@@ -156,14 +154,6 @@ public abstract class AbstractDlnaPlayer extends AbstractPlayer {
 
 	protected void clearRestorePositionState () {
 		this.restorePositionState = null;
-	}
-
-	protected void recordTrackStarted (final PlayItem item) {
-		this.scheduledExecutor.execute(new RecordTrackStarted(item));
-	}
-
-	protected void recordTrackCompleted (final PlayItem item) {
-		this.scheduledExecutor.execute(new RecordTrackCompleted(item));
 	}
 
 	public static PlayState transportIntoToPlayState (final TransportInfo ti) {

src/main/java/com/vaguehope/morrigan/dlna/players/DlnaPlayer.java
@@ -81,12 +81,13 @@ public class DlnaPlayer extends AbstractDlnaPlayer {
 		final WatcherTask oldWatcher = this.watcher.getAndSet(null);
 		if (oldWatcher != null) oldWatcher.cancel();
 
-		final WatcherTask task = WatcherTask.schedule(this.scheduledExecutor,
+		final WatcherTask task = WatcherTask.schedule(this.schEx,
 				uri, this.currentUri,
 				item.getTrack().getDuration(),
 				this.avTransport,
 				getListeners(),
-				new OnTrackStarted(this, item), new OnTrackComplete(this, item));
+				this.playbackRecorder,
+				item);
 		if (!this.watcher.compareAndSet(null, task)) {
 			task.cancel();
 			LOG.info("Failed to configure watcher as another got there first.");

src/main/java/com/vaguehope/morrigan/dlna/players/DlnaToPlay.java
@@ -6,29 +6,25 @@ import org.jupnp.util.MimeType;
 
 import com.vaguehope.morrigan.dlna.players.DlnaPlayingParamsFactory.DlnaPlayingParams;
 import com.vaguehope.morrigan.player.PlayItem;
+import com.vaguehope.morrigan.player.PlaybackRecorder;
 import com.vaguehope.morrigan.util.StringHelper;
 
 class DlnaToPlay {
 
 	private final PlayItem item;
 	private final DlnaPlayingParams playingParams;
+	private final PlaybackRecorder playbackRecorder;
 
-	private final Runnable onStartOfTrack;
-	private final Runnable onEndOfTrack;
-
-	public DlnaToPlay (final PlayItem item, final DlnaPlayingParams playingParams, final AbstractDlnaPlayer dlnaPlayer) {
+	public DlnaToPlay (final PlayItem item, final DlnaPlayingParams playingParams, final PlaybackRecorder playbackRecorder) {
 		if (item == null) throw new IllegalArgumentException();
 		if (StringHelper.blank(playingParams.id)) throw new IllegalArgumentException();
 		if (StringHelper.blank(playingParams.uri)) throw new IllegalArgumentException();
 		if (playingParams.mimeType == null) throw new IllegalArgumentException();
 		if (playingParams.durationSeconds < 1) throw new IllegalArgumentException("Can not play tracks without a known duration.");
-		if (dlnaPlayer == null) throw new IllegalArgumentException();
 
 		this.item = item;
 		this.playingParams = playingParams;
-
-		this.onStartOfTrack = new OnTrackStarted(dlnaPlayer, item);
-		this.onEndOfTrack = new OnTrackComplete(dlnaPlayer, item);
+		this.playbackRecorder = playbackRecorder;
 	}
 
 	public PlayItem getItem () {
@@ -74,13 +70,13 @@ class DlnaToPlay {
 		return this.trackStarted.get();
 	}
 
-	public void recordStartOfTrack () {
-		if (this.trackStarted.compareAndSet(false, true)) this.onStartOfTrack.run();
+	public void recordStartOfTrack() {
+		if (this.trackStarted.compareAndSet(false, true)) this.playbackRecorder.recordStarted(this.item);
 	}
 
-	public void recordEndOfTrack () {
+	public void recordEndOfTrack() {
 		recordStartOfTrack();
-		if (this.trackEnded.compareAndSet(false, true)) this.onEndOfTrack.run();
+		if (this.trackEnded.compareAndSet(false, true)) this.playbackRecorder.recordCompleted(this.item);
 	}
 
 }

src/main/java/com/vaguehope/morrigan/dlna/players/OnTrackComplete.java
@@ -1,21 +1,0 @@
-package com.vaguehope.morrigan.dlna.players;
-
-import com.vaguehope.morrigan.player.PlayItem;
-
-final class OnTrackComplete implements Runnable {
-
-	private final AbstractDlnaPlayer dlnaPlayer;
-	private final PlayItem item;
-
-	public OnTrackComplete (final AbstractDlnaPlayer dlnaPlayer, final PlayItem item) {
-		this.dlnaPlayer = dlnaPlayer;
-		this.item = item;
-	}
-
-	@Override
-	public void run () {
-		this.dlnaPlayer.recordTrackCompleted(this.item);
-		this.dlnaPlayer.nextTrack();
-	}
-
-}

src/main/java/com/vaguehope/morrigan/dlna/players/OnTrackStarted.java
@@ -1,20 +1,0 @@
-package com.vaguehope.morrigan.dlna.players;
-
-import com.vaguehope.morrigan.player.PlayItem;
-
-public class OnTrackStarted implements Runnable {
-
-	private final AbstractDlnaPlayer dlnaPlayer;
-	private final PlayItem item;
-
-	public OnTrackStarted (final AbstractDlnaPlayer dlnaPlayer, final PlayItem item) {
-		this.dlnaPlayer = dlnaPlayer;
-		this.item = item;
-	}
-
-	@Override
-	public void run () {
-		this.dlnaPlayer.recordTrackStarted(this.item);
-	}
-
-}

src/main/java/com/vaguehope/morrigan/dlna/players/RecordTrackCompleted.java
@@ -1,29 +1,0 @@
-package com.vaguehope.morrigan.dlna.players;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.vaguehope.morrigan.player.PlayItem;
-import com.vaguehope.morrigan.util.ErrorHelper;
-
-final class RecordTrackCompleted implements Runnable {
-
-	private static final Logger LOG = LoggerFactory.getLogger(RecordTrackCompleted.class);
-
-	private final PlayItem item;
-
-	public RecordTrackCompleted (final PlayItem item) {
-		this.item = item;
-	}
-
-	@Override
-	public void run () {
-		try {
-			this.item.getList().incTrackEndCnt(this.item.getTrack());
-		}
-		catch (final Exception e) { // NOSONAR no other way to report errors.
-			LOG.info("Failed to increment track end count: " + ErrorHelper.getCauseTrace(e));
-		}
-	}
-
-}

src/main/java/com/vaguehope/morrigan/dlna/players/RecordTrackStarted.java
@@ -1,29 +1,0 @@
-package com.vaguehope.morrigan.dlna.players;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.vaguehope.morrigan.player.PlayItem;
-import com.vaguehope.morrigan.util.ErrorHelper;
-
-final class RecordTrackStarted implements Runnable {
-
-	private static final Logger LOG = LoggerFactory.getLogger(RecordTrackStarted.class);
-
-	private final PlayItem item;
-
-	public RecordTrackStarted (final PlayItem item) {
-		this.item = item;
-	}
-
-	@Override
-	public void run () {
-		try {
-			this.item.getList().incTrackStartCnt(this.item.getTrack());
-		}
-		catch (final Exception e) { // NOSONAR no other way to report errors.
-			LOG.info("Failed to increment track start count: " + ErrorHelper.getCauseTrace(e));
-		}
-	}
-
-}

commit 74916f9408a88f1c6a426c306b1f07c32670ce46

Fix DLNA player not advancing to next track that i broke in 761be744.

src/main/java/com/vaguehope/morrigan/dlna/players/GoalSeekingDlnaPlayer.java
@@ -312,6 +312,8 @@ public class GoalSeekingDlnaPlayer extends AbstractDlnaPlayer {
 				this.goalSeekToSeconds = null;
 				this.lastObservedPositionSeconds = Timestamped.old(0L); // Old skips WAIT_FOR_STOP_EVENT_TIMEOUT_SECONDS delay.
 
+				nextTrack();
+
 				return PlayState.STOPPED; // Made a change, so return.
 			}
 